26879f7fd6e4c64529cd4d6158ecc9ce63e18ea0
Hui Wan <hui.wan@mpimet.mpg.de>
2011-04-25 08:17:19 +0000

    In branch "icon_restart_hui": Copy mo_kind.f90 from trunk. It seems that when neither "PUBLIC" nor "PRIVATE" is present in the module, NAG compiles everything as public, while GCC considers everything as private and issues error messages.

--- src/shared/mo_kind.f90
+++ src/shared/mo_kind.f90
@@ -1,86 +1,98 @@
+!>
+!!  Module determines kinds for different precisions.
+!!
+!!  Module determines kinds for different precisions
+!!  Number model from which the SELECTED_*\\_KIND are requested: <br>
+!!
+!! @f{tabular}{{r@{\hspace*{3em}}c@{\hspace*{3em}}c}
+!!                     &4 byte REAL     &8 byte REAL        \\\
+!!        CRAY:        &-               &precision =   13   \\\
+!!                     &                &exponent  = 2465   \\\
+!!        IEEE:        &precision = 6   &precision =   15   \\\
+!!                     &exponent  = 37  &exponent  =  307
+!! @f}
+!! \\medskip
+!!
+!!  Most likely this are the only possible models.
+!!
+!! @par Revision History
+!!  Working precision and comments are added by Luis Kornblueh (2001)
+!!  Modification by Luis Kornblueh (2010-02-16):
+!!  Working precision selection is parameterized by values, which can be as
+!!  well used for selection of MPI data types associated with the respective
+!!  Fortran datatypes.
+!!
+!! @par Copyright
+!! 2002-2010 by DWD and MPI-M
+!! This software is provided for non-commercial use only.
+!! See the LICENSE and the WARRANTY conditions.
+!!
+!! @par License
+!! The use of ICON is hereby granted free of charge for an unlimited time,
+!! provided the following rules are accepted and applied:
+!! <ol>
+!! <li> You may use or modify this code for your own non commercial and non
+!!    violent purposes.
+!! <li> The code may not be re-distributed without the consent of the authors.
+!! <li> The copyright notice and statement of authorship must appear in all
+!!    copies.
+!! <li> You accept the warranty conditions (see WARRANTY).
+!! <li> In case you intend to use the code commercially, we oblige you to sign
+!!    an according license agreement with DWD and MPI-M.
+!! </ol>
+!!
+!! @par Warranty
+!! This code has been tested up to a certain level. Defects and weaknesses,
+!! which may be included in the code, do not establish any warranties by the
+!! authors.
+!! The authors do not make any warranty, express or implied, or assume any
+!! liability or responsibility for the use, acquisition or application of this
+!! software.
+!!
+!!
 MODULE mo_kind
-
-  ! L. Kornblueh, MPI, August 2001, added working precision and comments 
 
   IMPLICIT NONE
 
-  ! Number model from which the SELECTED_*_KIND are requested:
+  PRIVATE
+
+  CHARACTER(len=*), PARAMETER, PRIVATE :: version = '$Id$'
+
+  !--------------------------------------------------------------------
   !
-  !                   4 byte REAL      8 byte REAL
-  !          CRAY:        -            precision =   13
-  !                                    exponent  = 2465
-  !          IEEE:    precision =  6   precision =   15  
-  !                   exponent  = 37   exponent  =  307 
+  ! Floating point section
+  ! ----------------------
   !
-  ! Most likely this are the only possible models.
-
-  ! Floating point section: 
-
-  INTEGER, PARAMETER :: ps = 6
-  INTEGER, PARAMETER :: rs = 37
-
-  INTEGER, PARAMETER :: pd = 12
+  INTEGER, PARAMETER :: ps =   6
+  INTEGER, PARAMETER :: rs =  37
+  !
+  INTEGER, PARAMETER :: pd =  12
   INTEGER, PARAMETER :: rd = 307
-
-  INTEGER, PARAMETER :: pi4 = 9
-  INTEGER, PARAMETER :: pi8 = 14
-
-  INTEGER, PARAMETER :: sp = SELECTED_REAL_KIND(ps,rs)  
-  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(pd,rd)
-
-  ! Floating point working precision
-
-  INTEGER, PARAMETER :: wp = dp   
-
+  !
+  INTEGER, PARAMETER :: sp = SELECTED_REAL_KIND(ps,rs) !< single precission
+  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(pd,rd) !< double precission
+  !
+  INTEGER, PARAMETER :: wp = dp                        !< selected working precission
+  !
   ! Integer section
-
-  INTEGER, PARAMETER :: i4 = SELECTED_INT_KIND(pi4)
-  INTEGER, PARAMETER :: i8 = SELECTED_INT_KIND(pi8)
-
-  ! Working precision for index variables
+  ! ---------------
   !
-  ! predefined preprocessor macros:
+  INTEGER, PARAMETER :: pi4 =  9
+  INTEGER, PARAMETER :: pi8 = 14  ! could be larger, but SX cannot do some operations otherwise
   !
-  ! xlf         __64BIT__   checked with P6 and AIX
-  ! gfortran    __LP64__    checked with Darwin and Linux
-  ! Intel, PGI  __x86_64__  checked with Linux
-  ! Sun         __x86_64    checked with Linux 
-
-#if defined (__64BIT__) || defined (__LP64__) || defined (__x86_64__) || defined (__x86_64)
-  INTEGER, PARAMETER :: widx = i8
-#else
-  INTEGER, PARAMETER :: widx = i4
-#endif
-
-CONTAINS
-
-  SUBROUTINE print_kinds  
-
-    PRINT *, 'single precision : ', sp
-    PRINT *, 'double precision : ', dp
-    PRINT *, 'working precision: ', wp
-    PRINT *, '4 byte integer   : ', i4
-    PRINT *, '8 byte integer   : ', i8
-    PRINT *, 'index integer    : ', widx
-
-#if defined (__64BIT__) 
-    ! xlf
-    PRINT *, '__64BIT__' 
-#endif
-#if defined (__LP64__)
-    ! gfortran
-    PRINT *, '__LP64__'
-#endif
-#if defined (__x86_64__)
-    ! Intel, PGI
-    PRINT *, '__x86_64__'
-#endif
-#if defined (__x86_64)
-    ! Sun
-    PRINT *, '__x86_64'
-#endif
-  ! NAG has no predefined macro but speed is no issue anyhow
-
-  END SUBROUTINE print_kinds
+  INTEGER, PARAMETER :: i4 = SELECTED_INT_KIND(pi4)   !< at least 4 byte integer
+  INTEGER, PARAMETER :: i8 = SELECTED_INT_KIND(pi8)   !< at least 8 byte integer
+  !
+  !
+  ! The following variable is made available internally only. configure needs to detect
+  ! the addressing mode and according to this mo_kind has to be updated by an preprocessor
+  ! directive and #include <config.h>. This needs some changes.
+  !
+  INTEGER, PARAMETER :: wi = i4                       !< selected working precission
+  !
+  PUBLIC :: sp, dp, wp, i4, i8
+  !
+  !--------------------------------------------------------------------
 
 END MODULE mo_kind
+
