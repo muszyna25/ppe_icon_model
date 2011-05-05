ce3a60805c508e355e1c0c43e5f3b506120d9d28
Hui Wan <hui.wan@mpimet.mpg.de>
2011-05-05 06:52:52 +0000

    In branch "icon_restart_hui": Include Luis' changes in revision 4269. The vertical level type specifier (ZAXIS_*) has been removed from the GRIB2 variable descriptor, which means the memory modules need to be modified accordingly.

--- src/shared/mo_grib2.f90
+++ src/shared/mo_grib2.f90
@@ -7,6 +7,7 @@ MODULE mo_grib2
   TYPE t_grib2_global
     INTEGER :: centre
     INTEGER :: subcentre
+    INTEGER :: generating_process
   END TYPE t_grib2_global
 
   TYPE t_grib2_var
@@ -16,7 +17,6 @@ MODULE mo_grib2
     INTEGER :: bits
     INTEGER :: gridtype
     INTEGER :: subgridtype
-    INTEGER :: leveltype
   END TYPE t_grib2_var
 
   PUBLIC :: t_grib2_global
