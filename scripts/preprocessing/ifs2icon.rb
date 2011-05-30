328d90dd99c271f1a8590cd2a872c9f6d41f66fb
Hui Wan <hui.wan@mpimet.mpg.de>
2011-05-30 08:20:52 +0000

    [icon_restart_hui] svn merge -r 4515:4522 from trunk.

--- scripts/preprocessing/ifs2icon.rb
+++ scripts/preprocessing/ifs2icon.rb
@@ -415,6 +415,29 @@ class JobQueue
     @threads.size.times { @queue.enq @queue}
     @threads.each {|t| t.join}
   end
+
+  def number_of_processors
+    if RUBY_PLATFORM =~ /linux/
+      return `cat /proc/cpuinfo | grep processor | wc -l`.to_i
+    elsif RUBY_PLATFORM =~ /darwin/
+      return `sysctl -n hw.logicalcpu`.to_i
+    elsif RUBY_PLATFORM =~ /(win32|mingw|cygwin)/
+      # this works for windows 2000 or greater
+      require 'win32ole'
+      wmi = WIN32OLE.connect("winmgmts://")
+      wmi.ExecQuery("select * from Win32_ComputerSystem").each do |system|
+        begin
+          processors = system.NumberOfLogicalProcessors
+        rescue
+          processors = 0
+        end
+        return [system.NumberOfProcessors, processors].max
+      end
+      elseif RUBY_PLATFORM =~ /java/
+        return Runtime.getRuntime().availableProcessors().to_i
+    end
+    raise "can't determine 'number_of_processors' for '#{RUBY_PLATFORM}'"
+  end
 end
 # example script
 # if $0 == __FILE__
