3444e048b5f90f888781a3e65e13bf25d2b00319
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2011-07-06 16:19:52 +0000

    [icon-nml-restruct] Compile fix.

--- src/namelists/mo_parallel_configuration.f90
+++ src/namelists/mo_parallel_configuration.f90
@@ -35,6 +35,12 @@ MODULE mo_parallel_configuration
 
    USE mo_kind,               ONLY: wp
    USE mo_exception,          ONLY: message, message_text, finish
+#ifndef NOMPI
+   USE mo_mpi,                ONLY: MPI_COMM_NULL, MPI_COMM_SELF, MPI_UNDEFINED, &
+      &   p_comm_work, p_comm_work_test   ! Communicator spanningwork group and test PE
+#else
+  USE mo_mpi,                ONLY:  p_comm_work, p_comm_work_test
+#endif
 
   IMPLICIT NONE
 
@@ -145,8 +151,8 @@ MODULE mo_parallel_configuration
   INTEGER :: nh_stepping_threads 
 
   ! MPI communicators
-  INTEGER :: p_comm_work        ! Communicator for work group
-  INTEGER :: p_comm_work_test   ! Communicator spanning work group and test PE
+!   INTEGER :: p_comm_work        ! Communicator for work group
+!   INTEGER :: p_comm_work_test   ! Communicator spanning work group and test PE
 
 
 
@@ -164,7 +170,7 @@ CONTAINS
 
     ! check n_ghost_rows
     IF (n_ghost_rows<1) THEN
-      CALL finish('mo_parallel_ctl:parallel_nml_setup', &
+      CALL finish(method_name, &
           & 'n_ghost_rows<1 in parallel_ctl namelist is not allowed')
     END IF
 
@@ -176,11 +182,11 @@ CONTAINS
 #ifdef HAVE_METIS
     ! ok
 #else
-      CALL finish('mo_parallel_ctl:parallel_nml_setup', &
+      CALL finish(method_name, &
         & 'division_method=div_metis=2 in parallel_ctl namelist is not allowed')
 #endif
     CASE DEFAULT
-      CALL finish('mo_parallel_ctl:parallel_nml_setup', &
+      CALL finish(method_name, &
         & 'value of division_method in parallel_ctl namelist is not allowed')
     END SELECT
 
@@ -189,16 +195,16 @@ CONTAINS
     ! Unconditionally set p_test_run to .FALSE. and num_io_procs to 0,
     ! all other variables are already set correctly
     IF (p_test_run) THEN
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
        & 'p_test_run has no effect if the model is compiled with the NOMPI compiler directive')
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
        & '--> p_test_run set to .FALSE.')
       p_test_run = .FALSE.
     END IF
     IF (num_io_procs /= 0) THEN
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
        & 'num_io_procs has no effect if the model is compiled with the NOMPI compiler directive')
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
        & '--> num_io_procs set to 0')
       num_io_procs = 0
     END IF
@@ -206,190 +212,216 @@ CONTAINS
     ! A run on 1 PE is never a verification run,
     ! correct this if the user should set it differently
     IF (p_test_run .AND. p_nprocs == 1) THEN
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
           & 'p_test_run has no effect if p_nprocs=1')
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
+      CALL message(method_name, &
           & '--> p_test_run set to .FALSE.')
       p_test_run = .FALSE.
     ENDIF
     ! for safety only
     IF(num_io_procs < 0) num_io_procs = 0
 #endif
+  
+  ! check p_test_run and num_io_procs
+#ifdef NOMPI
+  ! Unconditionally set p_test_run to .FALSE. and num_io_procs to 0,
+  ! all other variables are already set correctly
+  IF (p_test_run) THEN
+    CALL message(method_name, &
+       & 'p_test_run has no effect if the model is compiled with the NOMPI compiler directive')
+    CALL message(method_name, &
+       & '--> p_test_run set to .FALSE.')
+    p_test_run = .FALSE.
+  END IF
+  IF (num_io_procs /= 0) THEN
+    CALL message(method_name, &
+       & 'num_io_procs has no effect if the model is compiled with the NOMPI compiler directive')
+    CALL message(method_name, &
+       & '--> num_io_procs set to 0')
+    num_io_procs = 0
+  END IF
+#else
+  ! A run on 1 PE is never a verification run,
+  ! correct this if the user should set it differently
+  IF (p_test_run .AND. p_nprocs == 1) THEN
+    CALL message(method_name, &
+         & 'p_test_run has no effect if p_nprocs=1')
+    CALL message(method_name, &
+         & '--> p_test_run set to .FALSE.')
+     p_test_run = .FALSE.
+  ENDIF
+  ! for safety only
+  IF(num_io_procs < 0) num_io_procs = 0
+#endif
 
   ! check l_test_openmp
 #ifndef _OPENMP
-    IF (l_test_openmp) THEN
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
-          & 'l_test_openmp has no effect if the model is compiled without OpenMP support')
-      CALL message('mo_parallel_ctl:parallel_nml_setup', &
-          & '--> l_test_openmp set to .FALSE.')
-      l_test_openmp = .FALSE.
-    END IF
+  IF (l_test_openmp) THEN
+    CALL message(method_name, &
+       & 'l_test_openmp has no effect if the model is compiled without OpenMP support')
+    CALL message(method_name, &
+       & '--> l_test_openmp set to .FALSE.')
+    l_test_openmp = .FALSE.
+  END IF
 #endif
-
-  ! no checks for l_log_checks
-
-  ! no checks for l_fast_sum
-
-!-----------------------------------------------------------------------
-!-----------------------------------------------------------------------
 
   ! Set dependent control variables according
   ! to the (modified) NAMELIST varaibles
   ! -----------------------------------------
 
 #ifndef NOMPI
-    ! Set up processor numbers
+  ! Set up processor numbers
 
-    IF(p_test_run) THEN
-      num_test_procs = 1
-    ELSE
-      num_test_procs = 0
-    ENDIF
+  IF(p_test_run) THEN
+    num_test_procs = 1
+  ELSE
+    num_test_procs = 0
+  ENDIF
 
-    num_work_procs = p_nprocs - num_test_procs - num_io_procs
+  num_work_procs = p_nprocs - num_test_procs - num_io_procs
 
-    ! Check if there are sufficient PEs at all
+  ! Check if there are sufficient PEs at all
 
-    IF(num_work_procs < 1) THEN
-      CALL finish('mo_parallel_ctl:parallel_nml_setup', &
-        & 'not enough processors for given values of p_test_run/num_io_procs')
-    ELSE IF (p_test_run .AND. num_work_procs == 1) THEN
-      CALL finish('mo_parallel_ctl:parallel_nml_setup', &
-        & 'running p_test_run with only 1 work processor does not make sense')
-    ENDIF
+  IF(num_work_procs < 1) THEN
+    CALL finish(method_name, &
+     & 'not enough processors for given values of p_test_run/num_io_procs')
+  ELSE IF (p_test_run .AND. num_work_procs == 1) THEN
+    CALL finish(method_name, &
+     & 'running p_test_run with only 1 work processor does not make sense')
+  ENDIF
 
-    WRITE(message_text,'(3(a,i0))') 'Number of procs for test: ',num_test_procs, &
-      & ', work: ',num_work_procs,', I/O: ',num_io_procs
+  WRITE(message_text,'(3(a,i0))') 'Number of procs for test: ',num_test_procs, &
+    & ', work: ',num_work_procs,', I/O: ',num_io_procs
 
-    CALL message('mo_parallel_ctl:parallel_nml_setup', message_text)
+  CALL message(method_name, message_text)
 
-    ! Set up p_test_pe, p_work_pe0, p_io_pe0 which are identical on all PEs
+  ! Set up p_test_pe, p_work_pe0, p_io_pe0 which are identical on all PEs
 
-    IF(p_test_run) THEN
-      p_test_pe = 0
-    ELSE
-      p_test_pe = -1
-    ENDIF
+  IF(p_test_run) THEN
+    p_test_pe = 0
+  ELSE
+    p_test_pe = -1
+  ENDIF
 
-    p_work_pe0 = num_test_procs
-    p_io_pe0   = num_test_procs + num_work_procs
+  p_work_pe0 = num_test_procs
+  p_io_pe0   = num_test_procs + num_work_procs
 
-    ! if OpenMP is used, the test PE uses only 1 thread in order to check
-    ! the correctness of the OpenMP implementation
-    ! Currently the I/O PEs are also single threaded!
+  ! if OpenMP is used, the test PE uses only 1 thread in order to check
+  ! the correctness of the OpenMP implementation
+  ! Currently the I/O PEs are also single threaded!
 #ifdef _OPENMP
-    IF (l_test_openmp .AND. p_pe == p_test_pe) CALL OMP_SET_NUM_THREADS(1)
-    IF (p_pe >= p_io_pe0) CALL OMP_SET_NUM_THREADS(1)
+  IF (l_test_openmp .AND. p_pe == p_test_pe) CALL OMP_SET_NUM_THREADS(1)
+  IF (p_pe >= p_io_pe0) CALL OMP_SET_NUM_THREADS(1)
 #endif
 
-    ! Set up p_n_work and p_pe_work which are NOT identical on all PEs
+  ! Set up p_n_work and p_pe_work which are NOT identical on all PEs
+
+  IF(p_pe < p_work_pe0) THEN
+    ! Test PE (if present)
+    p_n_work  = 1          ! 1 PE in verification work group
+    p_pe_work = 0          ! PE number within work group
+  ELSE IF(p_pe < p_io_pe0) THEN
+    ! Work PE
+    p_n_work  = num_work_procs
+    p_pe_work = p_pe - num_test_procs
+  ELSE
+    ! I/O PE (if present)
+    p_n_work  = num_io_procs
+    p_pe_work = p_pe - num_test_procs - num_work_procs
+  ENDIF
+
+  ! Set communicators
+  ! =================
+
+  ! Split communicator p_all_comm between test/work/io
+  ! to get p_comm_work which is the communicator for
+  ! usage WITHIN every group of the 3 different type
+
+  IF(p_pe < p_work_pe0) THEN
+    my_color = 1 ! Test PE
+  ELSE IF(p_pe < p_io_pe0) THEN
+    my_color = 2 ! Work PE
+  ELSE
+    my_color = 3 ! I/O PE
+  ENDIF
+
+  CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work, p_error)
+
+  ! Set p_comm_work_test, the communicator spanning work group and test PE
+
+  IF(p_test_run) THEN
+    IF(p_pe < p_io_pe0) THEN
+      my_color = 1
+    ELSE
+      my_color = MPI_UNDEFINED ! p_comm_work_test must never be used on I/O PEs
+    ENDIF
+
+    CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work_test, p_error)
+  ELSE
+    ! If not a test run, p_comm_work_test must not be used at all
+    p_comm_work_test = MPI_COMM_NULL
+  ENDIF
+
+  ! Set p_comm_work_io, the communicator spanning work group and I/O PEs
+
+  IF(num_io_procs > 0) THEN
     IF(p_pe < p_work_pe0) THEN
-      ! Test PE (if present)
-      p_n_work  = 1          ! 1 PE in verification work group
-      p_pe_work = 0          ! PE number within work group
-    ELSE IF(p_pe < p_io_pe0) THEN
-      ! Work PE
-      p_n_work  = num_work_procs
-      p_pe_work = p_pe - num_test_procs
+      my_color = MPI_UNDEFINED ! p_comm_work_io must never be used on test PE
     ELSE
-      ! I/O PE (if present)
-      p_n_work  = num_io_procs
-      p_pe_work = p_pe - num_test_procs - num_work_procs
+      my_color = 1
     ENDIF
 
-    ! Set communicators
-    ! =================
+    CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work_io, p_error)
+  ELSE
+    ! If no I/O PEs are present, p_comm_work_io must not be used at all
+    p_comm_work_io = MPI_COMM_NULL
+  ENDIF
 
-    ! Split communicator p_all_comm between test/work/io
-    ! to get p_comm_work which is the communicator for
-    ! usage WITHIN every group of the 3 different type
+  ! Set p_comm_input_bcast, the communicator for broadcasting the NetCDF input
 
-    IF(p_pe < p_work_pe0) THEN
-      my_color = 1 ! Test PE
-    ELSE IF(p_pe < p_io_pe0) THEN
-      my_color = 2 ! Work PE
+  IF(lrestore_states) THEN
+    ! NetCDF input is only read by the test pe and MUST NOT be broadcast
+    IF(p_pe == p_test_pe) THEN
+      p_comm_input_bcast = MPI_COMM_SELF ! i.e. effectively no broadcast
     ELSE
-      my_color = 3 ! I/O PE
+      p_comm_input_bcast = MPI_COMM_NULL ! Must not be used!
     ENDIF
-
-    CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work, p_error)
-
-    ! Set p_comm_work_test, the communicator spanning work group and test PE
-
-    IF(p_test_run) THEN
-      IF(p_pe < p_io_pe0) THEN
-        my_color = 1
+  ELSE
+    IF(p_pe < p_io_pe0) THEN
+      IF(p_test_run) THEN
+        ! Test PE reads and broadcasts to workers
+        p_comm_input_bcast = p_comm_work_test
       ELSE
-        my_color = MPI_UNDEFINED ! p_comm_work_test must never be used on I/O PEs
-      ENDIF
-
-      CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work_test, p_error)
-    ELSE
-      ! If not a test run, p_comm_work_test must not be used at all
-      p_comm_work_test = MPI_COMM_NULL
-    ENDIF
-
-    ! Set p_comm_work_io, the communicator spanning work group and I/O PEs
-
-    IF(num_io_procs > 0) THEN
-      IF(p_pe < p_work_pe0) THEN
-        my_color = MPI_UNDEFINED ! p_comm_work_io must never be used on test PE
-      ELSE
-        my_color = 1
-      ENDIF
-
-      CALL MPI_Comm_split(p_all_comm, my_color, p_pe, p_comm_work_io, p_error)
-    ELSE
-      ! If no I/O PEs are present, p_comm_work_io must not be used at all
-      p_comm_work_io = MPI_COMM_NULL
-    ENDIF
-
-    ! Set p_comm_input_bcast, the communicator for broadcasting the NetCDF input
-
-    IF(lrestore_states) THEN
-      ! NetCDF input is only read by the test pe and MUST NOT be broadcast
-      IF(p_pe == p_test_pe) THEN
-        p_comm_input_bcast = MPI_COMM_SELF ! i.e. effectively no broadcast
-      ELSE
-        p_comm_input_bcast = MPI_COMM_NULL ! Must not be used!
+        ! PE 0 reads and broadcasts
+        p_comm_input_bcast = p_comm_work
       ENDIF
     ELSE
-      IF(p_pe < p_io_pe0) THEN
-        IF(p_test_run) THEN
-          ! Test PE reads and broadcasts to workers
-          p_comm_input_bcast = p_comm_work_test
-        ELSE
-          ! PE 0 reads and broadcasts
-          p_comm_input_bcast = p_comm_work
-        ENDIF
-      ELSE
-        ! I/O PEs never participate in reading
-        p_comm_input_bcast = MPI_COMM_NULL
-      ENDIF
+      ! I/O PEs never participate in reading
+      p_comm_input_bcast = MPI_COMM_NULL
     ENDIF
+  ENDIF
 
-    ! Create Intercommunicator work PEs - I/O PEs
+  ! Create Intercommunicator work PEs - I/O PEs
 
-    ! From MPI-Report:
-    ! Advice to users: We recommend using a dedicated peer communicator, such as a
-    ! duplicate of MPI_COMM_WORLD, to avoid trouble with peer communicators.
+  ! From MPI-Report:
+  ! Advice to users: We recommend using a dedicated peer communicator, such as a
+  ! duplicate of MPI_COMM_WORLD, to avoid trouble with peer communicators.
 
-    ! No idea what these troubles may be in reality, but let us follow the advice
+  ! No idea what these troubles may be in reality, but let us follow the advice
 
-    CALL MPI_Comm_dup(p_all_comm, peer_comm, p_error)
+  CALL MPI_Comm_dup(p_all_comm, peer_comm, p_error)
 
-    IF(p_pe /= p_test_pe .AND. num_io_procs>0) THEN
+  IF(p_pe /= p_test_pe .AND. num_io_procs>0) THEN
 
-      IF(p_pe < p_io_pe0) THEN
-        CALL MPI_Intercomm_create(p_comm_work, 0, peer_comm, p_io_pe0,  1, p_comm_work_2_io, p_error)
-      ELSE
-        CALL MPI_Intercomm_create(p_comm_work, 0, peer_comm, p_work_pe0,1, p_comm_work_2_io, p_error)
-      ENDIF
+    IF(p_pe < p_io_pe0) THEN
+      CALL MPI_Intercomm_create(p_comm_work, 0, peer_comm, p_io_pe0,  1, p_comm_work_2_io, p_error)
     ELSE
-      ! No Intercommunicator
-      p_comm_work_2_io = MPI_COMM_NULL
+      CALL MPI_Intercomm_create(p_comm_work, 0, peer_comm, p_work_pe0,1, p_comm_work_2_io, p_error)
     ENDIF
+  ELSE
+    ! No Intercommunicator
+    p_comm_work_2_io = MPI_COMM_NULL
+  ENDIF
 #endif
 
 
