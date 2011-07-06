068c84afd876864a88f1c0d27e5eb494c2b3fe42
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2011-07-06 16:23:55 +0000

    [icon-nml-restruct] Compile fix.

--- src/namelists/mo_parallel_configuration.f90
+++ src/namelists/mo_parallel_configuration.f90
@@ -33,11 +33,12 @@
 !!
 MODULE mo_parallel_configuration
 
-   USE mo_kind,               ONLY: wp
-   USE mo_exception,          ONLY: message, message_text, finish
+  USE mo_kind,               ONLY: wp
+  USE mo_exception,          ONLY: message, message_text, finish
+  USE mo_run_nml,            ONLY: lrestore_states
 #ifndef NOMPI
-   USE mo_mpi,                ONLY: MPI_COMM_NULL, MPI_COMM_SELF, MPI_UNDEFINED, &
-      &   p_comm_work, p_comm_work_test   ! Communicator spanningwork group and test PE
+  USE mo_mpi,                ONLY: MPI_COMM_NULL, MPI_COMM_SELF, MPI_UNDEFINED, &
+     &   p_comm_work, p_comm_work_test   ! Communicator spanningwork group and test PE
 #else
   USE mo_mpi,                ONLY:  p_comm_work, p_comm_work_test
 #endif
