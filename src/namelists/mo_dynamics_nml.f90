1a59e3ba6500e1828e21b9b2cc2c3dff0f39ab91
Marco Giorgetta <marco.giorgetta@mpimet.mpg.de>
2011-07-14 16:20:45 +0000

    Use the same variable names in the "nml" and "config" modules, i.e. work without "nml_" prefix for the namelist variables in the "nml" module. Instead use local names with prefix "config_" for the variables USEd in the "nml" module from the "config" module.

--- src/namelists/mo_dynamics_nml.f90
+++ src/namelists/mo_dynamics_nml.f90
@@ -34,18 +34,25 @@
 !!
 MODULE mo_dynamics_nml
 
-  USE mo_dynamics_config
-  USE mo_kind,               ONLY: wp
-  USE mo_exception,          ONLY: message_text, finish
-  USE mo_impl_constants,     ONLY: UNKNOWN, LEAPFROG_SI, IHS_ATM_TEMP
-  USE mo_physical_constants, ONLY: grav
-  USE mo_io_units,           ONLY: nnml, nnml_output
-  USE mo_namelist,           ONLY: position_nml, positioned, open_nml, close_nml
-  USE mo_mpi,                ONLY: p_pe, p_io
+  USE mo_dynamics_config,     ONLY: config_iequations     => iequations     &
+                                  & config_itime_scheme   => itime_scheme   &
+                                  & config_idiv_method    => idiv_method    &
+                                  & config_divavg_cntrwgt => divavg_cntrwgt &
+                                  & config_ldry_dycore    => ldry_dycore    &
+                                  & config_sw_ref_height  => sw_ref_height  &
+                                  & config_lcoriolis      => lcoriolis
 
-  USE mo_master_nml,            ONLY: lrestart
-  USE mo_io_restart_namelist,   ONLY: open_tmpfile, store_and_close_namelist,   &       
-                                    & open_and_restore_namelist, close_tmpfile
+  USE mo_kind,                ONLY: wp
+  USE mo_exception,           ONLY: message_text, finish
+  USE mo_impl_constants,      ONLY: UNKNOWN, LEAPFROG_SI, IHS_ATM_TEMP
+  USE mo_physical_constants,  ONLY: grav
+  USE mo_io_units,            ONLY: nnml, nnml_output
+  USE mo_namelist,            ONLY: position_nml, positioned, open_nml, close_nml
+  USE mo_mpi,                 ONLY: p_pe, p_io
+
+  USE mo_master_nml,          ONLY: lrestart
+  USE mo_io_restart_namelist, ONLY: open_tmpfile, store_and_close_namelist,   &       
+                                  & open_and_restore_namelist, close_tmpfile
 
   IMPLICIT NONE
   PRIVATE
@@ -61,42 +68,42 @@ MODULE mo_dynamics_nml
   !---------------------------------------------------------------
   ! time stepping scheme 
 
-  INTEGER  :: nml_iequations
+  INTEGER  :: iequations
 
-  INTEGER  :: nml_itime_scheme   ! parameter used to select the time stepping scheme
-                                 ! = 1, explicit 2 time level scheme
-                                 ! = 2, semi implicit 2 time level scheme
-                                 ! = 3, explicit leapfrog
-                                 ! = 4, leapfrog with semi implicit correction
-                                 ! = 5, 4-stage Runge-Kutta method
-                                 ! = 6, SSPRK(5,4) (Runge-Kutta) method
+  INTEGER  :: itime_scheme   ! parameter used to select the time stepping scheme
+                             ! = 1, explicit 2 time level scheme
+                             ! = 2, semi implicit 2 time level scheme
+                             ! = 3, explicit leapfrog
+                             ! = 4, leapfrog with semi implicit correction
+                             ! = 5, 4-stage Runge-Kutta method
+                             ! = 6, SSPRK(5,4) (Runge-Kutta) method
 
   ! way of computing the divergence operator in the triangular model -------------
 
-  INTEGER  :: nml_idiv_method  ! 1: Hydrostatic atmospheric model: 
-                               !    Gauss integral with original normal 
-                               !    velocity components
-                               ! 1: Non-Hydrostatic atmospheric model: 
-                               !    Gauss integral with averged normal 
-                               !    velocity components
-                               ! Thus, in a linear equilateral grid, methods 1 and 2 for
-                               ! the non-hydrostatic model are the same.
-                               ! 2: divergence averaging with bilinear averaging
+  INTEGER  :: idiv_method    ! 1: Hydrostatic atmospheric model: 
+                             !    Gauss integral with original normal 
+                             !    velocity components
+                             ! 1: Non-Hydrostatic atmospheric model: 
+                             !    Gauss integral with averged normal 
+                             !    velocity components
+                             ! Thus, in a linear equilateral grid, methods 1 and 2 for
+                             ! the non-hydrostatic model are the same.
+                             ! 2: divergence averaging with bilinear averaging
 
-  REAL(wp) :: nml_divavg_cntrwgt  ! weight of central cell for divergence averaging
+  REAL(wp) :: divavg_cntrwgt ! weight of central cell for divergence averaging
 
-  LOGICAL :: nml_ldry_dycore  ! if .TRUE., ignore the effact of water vapor,
-                              ! cloud liquid and cloud ice on virtual temperature.
+  LOGICAL :: ldry_dycore     ! if .TRUE., ignore the effact of water vapor,
+                             ! cloud liquid and cloud ice on virtual temperature.
 
-  REAL(wp) :: nml_sw_ref_height  ! reference height to linearize around if using
-                                 ! lshallow_water and semi-implicit correction
+  REAL(wp) :: sw_ref_height  ! reference height to linearize around if using
+                             ! lshallow_water and semi-implicit correction
 
-  LOGICAL  :: nml_lcoriolis      ! if .TRUE.,  the Coriolis force is switched on
+  LOGICAL  :: lcoriolis      ! if .TRUE.,  the Coriolis force is switched on
 
-  NAMELIST/dynamics_nml/ nml_iequations,  nml_itime_scheme,   &
-                         nml_idiv_method, nml_divavg_cntrwgt, &
-                         nml_ldry_dycore, nml_sw_ref_height,  &
-                         nml_lcoriolis
+  NAMELIST/dynamics_nml/ iequations,  itime_scheme,   &
+                         idiv_method, divavg_cntrwgt, &
+                         ldry_dycore, sw_ref_height,  &
+                         lcoriolis
 
 CONTAINS
   !>
@@ -110,13 +117,13 @@ CONTAINS
     !------------------------------------------------------------
     ! Set up the default values
     !------------------------------------------------------------
-    nml_iequations     = IHS_ATM_TEMP
-    nml_itime_scheme   = LEAPFROG_SI
-    nml_idiv_method    = 1
-    nml_divavg_cntrwgt = 0.5_wp
-    nml_ldry_dycore    = .FALSE.
-    nml_sw_ref_height  = 0.9_wp*2.94e4_wp/grav
-    nml_lcoriolis      = .TRUE.
+    iequations     = IHS_ATM_TEMP
+    itime_scheme   = LEAPFROG_SI
+    idiv_method    = 1
+    divavg_cntrwgt = 0.5_wp
+    ldry_dycore    = .FALSE.
+    sw_ref_height  = 0.9_wp*2.94e4_wp/grav
+    lcoriolis      = .TRUE.
  
     !------------------------------------------------------------------------
     ! If this is a resumed integration, overwrite the defaults above by 
@@ -142,13 +149,13 @@ CONTAINS
     !-----------------------------------------------------
     ! Sanity check
     !-----------------------------------------------------
-    IF((nml_itime_scheme<=0).OR.(nml_itime_scheme>=unknown)) THEN
+    IF((itime_scheme<=0).OR.(itime_scheme>=unknown)) THEN
       WRITE(message_text,'(A,i2)') &
       'wrong value of itime_scheme, must be 1 ...', unknown -1
       CALL finish( TRIM(routine),TRIM(message_text))
     ENDIF
 
-    IF (nml_idiv_method > 2 .OR. nml_idiv_method < 1 )THEN
+    IF (idiv_method > 2 .OR. idiv_method < 1 )THEN
       CALL finish(TRIM(routine),'Error: idiv_method must be 1 or 2 !')
     ENDIF
 
@@ -166,13 +173,13 @@ CONTAINS
     ! 5. Fill configuration state
     !-----------------------------------------------------
 
-    iequations     = nml_iequations
-    itime_scheme   = nml_itime_scheme
-    idiv_method    = nml_idiv_method
-    divavg_cntrwgt = nml_divavg_cntrwgt
-    ldry_dycore    = nml_ldry_dycore
-    sw_ref_height  = nml_sw_ref_height
-    lcoriolis      = nml_lcoriolis
+    config_iequations     = iequations
+    config_itime_scheme   = itime_scheme
+    config_idiv_method    = idiv_method
+    config_divavg_cntrwgt = divavg_cntrwgt
+    config_ldry_dycore    = ldry_dycore
+    config_sw_ref_height  = sw_ref_height
+    config_lcoriolis      = lcoriolis
 
   END SUBROUTINE read_dynamics_namelist
 
