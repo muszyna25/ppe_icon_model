d2e2e3a41a08a272514ed06a958e54fd7ab42bed
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2011-07-19 07:59:19 +0000

    [icon-nml-restruct] Some clean-up regarding the use of mo_mpi.

--- src/drivers/mo_atmo_hydrostatic.f90
+++ src/drivers/mo_atmo_hydrostatic.f90
@@ -36,7 +36,6 @@ MODULE mo_atmo_hydrostatic
 
 USE mo_exception,           ONLY: message, finish
 USE mo_mpi,                 ONLY: p_stop, p_pe, p_io, p_nprocs, &
-& p_comm_work_test, p_comm_input_bcast, p_comm_work, &
 & my_process_is_io,  my_process_is_mpi_seq, my_process_is_mpi_test, &
 & my_process_is_stdio
 USE mo_timer,               ONLY: init_timer, print_timer
