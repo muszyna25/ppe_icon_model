edb35c6df008265232c1e28cb2fe58537e7acf9a
G��nther Z��ngl <Guenther.Zaengl@dwd.de>
2011-07-27 13:14:03 +0000

    Cleanup of USE statements

--- src/drivers/prep_icon.f90
+++ src/drivers/prep_icon.f90
@@ -37,150 +37,77 @@ USE mo_exception,           ONLY: message, finish
 !$  USE mo_exception,         ONLY: message_text     ! use only if compiled with OpenMP
 USE mo_parallel_config,     ONLY: p_test_run, l_test_openmp, num_io_procs
 USE mo_mpi,                 ONLY: start_mpi, p_stop, &
-  & my_process_is_io,  my_process_is_mpi_seq, my_process_is_mpi_test, &
-  & my_process_is_stdio, set_mpi_work_communicators, set_comm_input_bcast, null_comm_type
-USE mo_timer,               ONLY: init_timer, print_timer
-USE mo_master_control,      ONLY: is_restart_run
-USE mo_output,              ONLY: init_output_files, close_output_files, write_output
+  & my_process_is_io, my_process_is_mpi_seq, set_mpi_work_communicators
+USE mo_timer,               ONLY: init_timer
 
-
-USE mo_io_async,            ONLY: io_main_proc            ! main procedure for I/O PEs
 
 ! Control parameters: run control, dynamics, i/o
 !
-USE mo_nonhydrostatic_config,ONLY: ivctype, kstart_moist, kstart_qv, l_open_ubc
+USE mo_nonhydrostatic_config,ONLY: ivctype
 USE mo_dynamics_config,   ONLY: iequations
 USE mo_run_config,        ONLY: configure_run, &
-  & dtime,                & !    namelist parameter
-  & ltransport,           & !    :
-  & lforcing,             & !    :
   & ltimer,               & !    :
-  & iforcing,             & !    namelist parameter
-  & ldump_states,         & ! flag if states should be dumped
-  & lrestore_states,      & ! flag if states should be restored
   & nlev,nlevp1,          &
-  & num_lev,num_levp1,    &
-  & iqv, nshift,          &
-  & lvert_nest, ntracer
+  & num_lev,num_levp1, nshift
 
-USE mo_impl_constants, ONLY:&
-  & ihs_atm_temp,         & !    :
-  & ihs_atm_theta,        & !    :
-  & inh_atmosphere,       & !    :
-  & ishallow_water,       & !    :
-  & ildf_dry,             & !    :
-  & inoforcing,           & !    :
-  & iheldsuarez,          & !    :
-  & inwp
-
-
-! For the coupling
-USE mo_impl_constants, ONLY: CELLS
-USE mo_master_control, ONLY : atmo_process, is_coupled_run
-USE mo_icon_cpl_init_comp, ONLY : get_my_local_comp_id
-USE mo_icon_cpl_def_grid, ONLY : ICON_cpl_def_grid
-USE mo_icon_cpl_def_field, ONLY : ICON_cpl_def_field
-USE mo_icon_cpl_search, ONLY : ICON_cpl_search
-USE mo_model_domain_import, ONLY : get_patch_global_indexes
-
-! Test cases
-!
-USE mo_ha_testcases,     ONLY: read_ha_testcase_namelist, ctest_name
-USE mo_nh_testcases,     ONLY: read_nh_testcase_namelist ! process non-hyd. atm. test ctl. par.
+USE mo_impl_constants, ONLY:  inh_atmosphere
 
 ! Memory
 !
 USE mo_subdivision,         ONLY: decompose_atmo_domain,         &
-& copy_processor_splitting,      &
-& set_patch_communicators
-USE mo_dump_restore,        ONLY: dump_patch_state_netcdf,       &
-& restore_patches_netcdf,        &
-& restore_interpol_state_netcdf, &
-& restore_gridref_state_netcdf
+& copy_processor_splitting,  set_patch_communicators
 
-USE mo_nonhydro_state,      ONLY: p_nh_state
+
 USE mo_atmo_control,        ONLY: p_patch_global, p_patch_subdiv, p_patch
 
 ! Horizontal grid
-USE mo_grid_config,         ONLY: n_dom, n_dom_start, global_cell_type, &
-                                  dynamics_parent_grid_id
+USE mo_grid_config,         ONLY: n_dom, n_dom_start, global_cell_type
+                                  
 USE mo_model_domain_import, ONLY: import_patches, destruct_patches
 
 ! Horizontal interpolation
 !
 USE mo_intp_state,          ONLY: construct_2d_interpol_state, &
 & destruct_2d_interpol_state
-USE mo_interpolation,       ONLY: rbf_vec_interpol_cell,       &
-& edges2cells_scalar
+
 USE mo_grf_interpolation,   ONLY: construct_2d_gridref_state,  &
 & destruct_2d_gridref_state
 
 ! Vertical grid
 !
-USE mo_vertical_coord_table,ONLY: init_vertical_coord_table, &
-  &                               vct_a, vct_b, ceta, apzero
-  USE mo_nh_init_utils,       ONLY: init_hybrid_coord, init_sleve_coord
+
+USE mo_nh_init_utils,       ONLY: init_hybrid_coord, init_sleve_coord
 
 ! State variables
 !
-USE mo_nonhydro_state,      ONLY: destruct_nh_state
 
-
-! Parameterized forcing
-!
-USE mo_nwp_phy_state,       ONLY: construct_nwp_phy_state,   &
-  &                               destruct_nwp_phy_state
-USE mo_atm_phy_nwp_config, ONLY: atm_phy_nwp_config,configure_atm_phy_nwp
-USE mo_lnd_nwp_nml,         ONLY: setup_nwp_lnd
-USE mo_nwp_lnd_state,       ONLY: construct_nwp_lnd_state,   &
-& destruct_nwp_lnd_state, p_lnd_state
 
 USE mo_impl_constants,      ONLY: SUCCESS, MAX_CHAR_LENGTH
 
-! Time integration
-!
-USE mo_ha_stepping,         ONLY: initcond_ha_dyn, perform_ha_stepping
-USE mo_nh_stepping,         ONLY: prepare_nh_integration, perform_nh_stepping
-
-!  USE mo_nwp_phy_init,          ONLY: init_nwp_phy
-!!$  USE mo_gscp_cosmo,          ONLY: hydci_pp_init
-
-
-!-------------------------------------------------------------------------
-! to break circular dependency
 
 USE mo_intp_data_strc,      ONLY: p_int_state_global, p_int_state_subdiv, p_int_state
 USE mo_grf_intp_data_strc,  ONLY: p_grf_state_global, p_grf_state_subdiv, p_grf_state
 
-!-------------------------------------------------------------------------
-USE mo_io_restart,           ONLY: read_restart_info_file, read_restart_files
-USE mo_io_restart_namelist,  ONLY: read_restart_namelists
-USE mo_io_restart_attributes,ONLY: read_restart_attributes, get_restart_attribute
 
 USE mo_read_namelists,     ONLY: read_atmo_namelists
 USE mo_atm_nml_crosscheck,       ONLY: atm_crosscheck
 
 USE mo_time_config,        ONLY: time_config      ! variable
 USE mo_dynamics_config,    ONLY: configure_dynamics  ! subroutine
-!USE mo_interpol_config,    ONLY: configure_interpolation 
 USE mo_interpol_config
-USE mo_advection_config,   ONLY: configure_advection
-USE mo_diffusion_config,   ONLY: configure_diffusion
 
-USE mo_atmo_hydrostatic,    ONLY: atmo_hydrostatic 
 USE mo_atmo_nonhydrostatic, ONLY: atmo_nonhydrostatic 
 
-! USE statements added for prep_icon
-  USE mo_time_config, ONLY: time_config 
-  USE mo_prepicon_utils,      ONLY: init_prepicon, prepicon, write_prepicon_output, &
-    & compute_coord_fields, init_topo_output_files, close_prepicon_output_files,    &
-    & convert_variables, init_atmo_output_files, deallocate_prepicon
+! USE statements referring directly to prep_icon
+USE mo_time_config, ONLY: time_config 
+USE mo_prepicon_utils,      ONLY: init_prepicon, prepicon, write_prepicon_output, &
+  & compute_coord_fields, init_topo_output_files, close_prepicon_output_files,    &
+  & convert_variables, init_atmo_output_files, deallocate_prepicon
   
-  USE mo_prepicon_nml,        ONLY: i_oper_mode, prepicon_nml_setup, l_zp_out
-  USE mo_nh_vert_interp,      ONLY: vertical_interpolation, interpolate_to_p_and_z_levels
+USE mo_prepicon_nml,        ONLY: i_oper_mode, prepicon_nml_setup, l_zp_out
+USE mo_nh_vert_interp,      ONLY: vertical_interpolation, interpolate_to_p_and_z_levels
 
-  USE mo_master_nml,         ONLY: lrestart
-  USE mo_extpar_config,      ONLY: itopo
+USE mo_extpar_config,      ONLY: itopo
 
 !-------------------------------------------------------------------------
 IMPLICIT NONE
@@ -189,7 +116,7 @@ IMPLICIT NONE
   CHARACTER(*), PARAMETER :: routine = "prep_icon"
 
     
-  INTEGER :: n_io, jg, jfile, n_file, ist, n_diag
+  INTEGER :: ist
   INTEGER :: error_status
 
   !declaration of OpenMP Runtime Library Routines:
@@ -250,7 +177,7 @@ IMPLICIT NONE
     !---------------------------------------------------------------------
     CALL configure_run
 
-    ltransport = .FALSE.
+    ! This is hardcoded for prep_icon because running this program is nonsense otherwise
     itopo = 1
 
 
@@ -273,10 +200,6 @@ IMPLICIT NONE
     !-------------------------------------------------------------------
     ! 4. Import patches
     !-------------------------------------------------------------------
-    ! If we belong to the I/O PEs just call io_main_proc before reading patches.
-    ! This routine will never return
-    
-    IF (my_process_is_io()) CALL io_main_proc
     
     ! Check patch allocation status
 
@@ -328,8 +251,7 @@ IMPLICIT NONE
     !    Divide patches and interpolation states for parallel runs.
     !    This is only done if the model runs really in parallel.
     !-------------------------------------------------------------------   
-    IF (my_process_is_mpi_seq()  &
-      &  .OR. lrestore_states) THEN
+    IF (my_process_is_mpi_seq()) THEN
       
       ! This is a verification run or a run on a single processor
       ! or the divided states have been read, just set pointers
@@ -368,6 +290,7 @@ IMPLICIT NONE
         CALL init_sleve_coord(p_patch(1)%nlev)
       ENDIF
     CASE DEFAULT
+      CALL finish(TRIM(routine),'prep_icon only works for NH model')
     END SELECT
 
 
@@ -377,16 +300,6 @@ IMPLICIT NONE
     !---------------------------------------------------------------------
 
     CALL configure_dynamics ( n_dom )
-    CALL configure_diffusion( n_dom, dynamics_parent_grid_id, &
-                            & nlev, vct_a, vct_b, apzero      )
-
-    DO jg =1,n_dom
-     CALL configure_advection( jg, p_patch(jg)%nlev, p_patch(1)%nlev,      &
-       &                      iequations, iforcing, iqv, kstart_moist(jg), &
-       &                      kstart_qv(jg), lvert_nest, l_open_ubc, ntracer ) 
-    ENDDO
-
-
 
     ! Allocate prepicon data type
     ALLOCATE (prepicon(n_dom), stat=ist)
@@ -452,8 +365,7 @@ IMPLICIT NONE
       CALL destruct_2d_gridref_state( p_patch, p_grf_state )
     ENDIF
 
-    IF (my_process_is_mpi_seq()  &
-      & .OR. lrestore_states) THEN
+    IF (my_process_is_mpi_seq()) THEN
       DEALLOCATE (p_grf_state_global, STAT=error_status)
     ELSE
       DEALLOCATE (p_grf_state_subdiv, STAT=error_status)
@@ -465,8 +377,7 @@ IMPLICIT NONE
     ! Deallocate interpolation fields
 
     CALL destruct_2d_interpol_state( p_int_state )
-    IF  (my_process_is_mpi_seq()  &
-      & .OR. lrestore_states) THEN
+    IF  (my_process_is_mpi_seq()) THEN
       DEALLOCATE (p_int_state_global, STAT=error_status)
     ELSE
       DEALLOCATE (p_int_state_subdiv, STAT=error_status)
@@ -479,8 +390,7 @@ IMPLICIT NONE
 
     CALL destruct_patches( p_patch )
 
-    IF (my_process_is_mpi_seq()  &
-      & .OR. lrestore_states) THEN
+    IF (my_process_is_mpi_seq()) THEN
       DEALLOCATE( p_patch_global, STAT=error_status )
     ELSE
       DEALLOCATE( p_patch_subdiv, STAT=error_status )
