e7a7f9a7fa3d1197cf5ae0e990551d0c494f0cf7
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2011-08-31 07:33:26 +0000

    Added some debug output to the dummy model.

--- src/drivers/mo_cpl_dummy_model.f90
+++ src/drivers/mo_cpl_dummy_model.f90
@@ -397,7 +397,8 @@ CONTAINS
         & grid_id, grid_shape, p_patch(patch_no)%cells%owner_local, & ! input
         & p_pe_work,  & ! this owner id
         & error_status )                                            ! output
-
+      write(0,*) "p_pe_work:",  p_pe_work
+      write(0,*) "cells%owner_local:",  p_patch(patch_no)%cells%owner_local
       ! Define exchange fields
       !
       ! We could also get the number of defined fields from the namelist module
