2fb7ddc317f1054faa6f73c9698d50b9e2af781d
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2011-12-13 14:40:29 +0000

    Reduce IO through mor operator chainin (refs #1712)

--- scripts/preprocessing/ifs2icon.rb
+++ scripts/preprocessing/ifs2icon.rb
@@ -545,56 +545,6 @@ module Dbg
   end
 end
 
-# ==============================================================================
-# Sized Queue for limiting the number of parallel jobs
-class JobQueue
-  attr_reader :size, :queue, :threads
-
-  def initialize(size)
-    @size  = size
-    @queue = Queue.new
-  end
-
-  def push(*items)
-    items.flatten.each {|it| @queue << it}
-  end
-
-  def run
-    @threads = (1..@size).map {|i|
-      Thread.new(@queue) {|q|
-        until ( q == ( task = q.deq ) )
-          system(task)
-        end
-      }
-    }
-    @threads.size.times { @queue.enq @queue}
-    @threads.each {|t| t.join}
-  end
-
-  def number_of_processors
-    if RUBY_PLATFORM =~ /linux/
-      return `cat /proc/cpuinfo | grep processor | wc -l`.to_i
-    elsif RUBY_PLATFORM =~ /darwin/
-      return `sysctl -n hw.logicalcpu`.to_i
-    elsif RUBY_PLATFORM =~ /(win32|mingw|cygwin)/
-      # this works for windows 2000 or greater
-      require 'win32ole'
-      wmi = WIN32OLE.connect("winmgmts://")
-      wmi.ExecQuery("select * from Win32_ComputerSystem").each do |system|
-        begin
-          processors = system.NumberOfLogicalProcessors
-        rescue
-          processors = 0
-        end
-        return [system.NumberOfProcessors, processors].max
-      end
-      elseif RUBY_PLATFORM =~ /java/
-        return Runtime.getRuntime().availableProcessors().to_i
-    end
-    raise "can't determine 'number_of_processors' for '#{RUBY_PLATFORM}'"
-  end
-end
-
 # A preprocessor should basically constist fof the following parts
 # * knowledge about required in and output variables
 # * rules for computing the output from the input variables
@@ -878,10 +828,11 @@ class Ifs2Icon
           copyfile, outfile = tfile, tfile
 
           # create netcdf version of the input
-          Cdo.chainCall("-setname,#{ovar} -copy",:in => @invars[ivar],:out => copyfile,:options => "-f nc2")
+#          Cdo.chainCall("-setname,#{ovar} -copy",:in => @invars[ivar],:out => copyfile,:options => "-f nc4")
 
           # Perform conservative remapping with pregenerated weights
-          Cdo.remap([gridfile,weightfile],:in => copyfile,:out => outfile)
+#          Cdo.remap([gridfile,weightfile],:in => copyfile,:out => outfile)
+          Cdo.chainCall("-remap,#{gridfile},#{weightfile} -setname,#{ovar} -copy",:in =>@invars[ivar],:out => outfile,:options => "-f nc2")
 
           @lock.synchronize { @outvars[ovar] = outfile }
         }
