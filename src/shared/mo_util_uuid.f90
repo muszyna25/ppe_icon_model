59bf46486d4d84a99ff0e965930ec93b13c4bde3
Luis Kornblueh <luis.kornblueh@mpimet.mpg.de>
2011-12-19 11:41:09 +0000

    MAke it working with sxf90. Need to get rid of this Compiler!!!!!!

--- src/shared/mo_util_uuid.f90
+++ src/shared/mo_util_uuid.f90
@@ -1,6 +1,27 @@
+#ifdef __SX__
+MODULE my_mo_util_uuid_type
+
+  USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_CHAR
+
+  IMPLICIT NONE 
+
+  PUBLIC
+
+  TYPE, BIND(C) :: t_uuid
+    CHARACTER(C_CHAR) :: data(16)
+  END type t_uuid
+
+END MODULE my_mo_util_uuid_type
+#endif
+
 MODULE mo_util_uuid
 
+#ifdef __SX__
+  USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_CHAR, C_NULL_CHAR
+  USE my_mo_util_uuid_type, ONLY: t_uuid
+#else
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_CHAR, C_SIGNED_CHAR, C_NULL_CHAR
+#endif
 
   IMPLICIT NONE 
 
@@ -18,9 +39,11 @@ MODULE mo_util_uuid
 
   INTEGER, PARAMETER :: uuid_string_length = 36
 
+#ifndef __SX__
   TYPE, BIND(C) :: t_uuid
     INTEGER(C_SIGNED_CHAR) :: data(16)
   END type t_uuid
+#endif
 
   INTERFACE OPERATOR (==)
     MODULE PROCEDURE uuid_compare
@@ -28,24 +51,43 @@ MODULE mo_util_uuid
 
   INTERFACE
     SUBROUTINE my_uuid_get(uuid) BIND(C,NAME='uuid_get')
+#ifdef __SX__
+      USE my_mo_util_uuid_type, ONLY: t_uuid
+#else
       IMPORT :: t_uuid
+#endif
       TYPE(t_uuid),     INTENT(out) :: uuid
     END SUBROUTINE my_uuid_get
   END INTERFACE
 
   INTERFACE
     SUBROUTINE my_uuid_format(uuid_string, uuid) BIND(C,NAME='uuid_format')
+#ifdef __SX__
+      USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_CHAR
+      USE my_mo_util_uuid_type, ONLY: t_uuid
+      CHARACTER(kind=C_CHAR,len=*),    INTENT(out) :: uuid_string
+#else
       IMPORT :: C_CHAR, t_uuid
       CHARACTER(C_CHAR), DIMENSION(*), INTENT(out) :: uuid_string
+#endif
       TYPE(t_uuid),                    INTENT(in)  :: uuid
     END SUBROUTINE my_uuid_format
   END INTERFACE
 
   INTERFACE
     SUBROUTINE my_uuid_parse(uuid, uuid_string) BIND(C,NAME='uuid_parse')
+#ifdef __SX__
+      USE, INTRINSIC :: ISO_C_BINDING, ONLY: C_CHAR
+      USE my_mo_util_uuid_type, ONLY: t_uuid
+#else
       IMPORT :: C_CHAR, t_uuid
+#endif
       TYPE(t_uuid),                    INTENT(out) :: uuid
+#ifdef __SX__
+      CHARACTER(kind=C_CHAR, len=*),   INTENT(in)  :: uuid_string
+#else
       CHARACTER(C_CHAR), DIMENSION(*), INTENT(in)  :: uuid_string
+#endif
     END SUBROUTINE my_uuid_parse
   END INTERFACE
 
@@ -73,6 +115,7 @@ CONTAINS
     TYPE(t_uuid),     INTENT(in)  :: uuid1
     TYPE(t_uuid),     INTENT(in)  :: uuid2
     uuid_compare = .TRUE.
+!CDIR NOVECTOR
     IF (ANY(uuid1%data /= uuid2%data)) THEN
       uuid_compare = .FALSE.
     ENDIF
