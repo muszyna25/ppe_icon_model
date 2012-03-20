46f043c0bd4bc6485dbd2715e1c5eaaa74fdcd68
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2012-03-20 22:33:10 +0000

    paral. central_vflux_oce in mo_oce_tracer_vert, refs #2138

--- src/oce_dyn_icohom/mo_oce_tracer_transport_vert.f90
+++ src/oce_dyn_icohom/mo_oce_tracer_transport_vert.f90
@@ -640,6 +640,7 @@ CONTAINS
   !! @par Revision History
   !! Petter Korn, MPI-M
   !!
+  !! mpi parallelized, no sync
   SUBROUTINE central_vflux_oce_orig( ppatch, pvar_c, pw_c, pupflux_i )
 
     TYPE(t_patch), TARGET, INTENT(IN) :: ppatch          !< patch on which computation is performed
@@ -680,7 +681,7 @@ CONTAINS
       END DO
     END DO
   END SUBROUTINE central_vflux_oce_orig
- !-------------------------------------------------------------------------
+  !-------------------------------------------------------------------------
   !>
   !!
   !! Calculation of central vertical tracer fluxes
@@ -696,24 +697,22 @@ CONTAINS
     REAL(wp), INTENT(INOUT)  :: pupflux_i(:,:,:)   !< variable in which the upwind flux is stored
                                                    !< dim: (nproma,n_zlev+1,nblks_c)
     ! local variables
-    INTEGER  :: i_startblk, i_endblk, i_startidx, i_endidx, rl_start, rl_end
+    INTEGER  :: i_startidx_c, i_endidx_c
     INTEGER  :: jc, jk, jb               !< index of cell, vertical level and block
     INTEGER  :: jkm1                     !< jk - 1
     INTEGER  :: z_dolic
     !-------------------------------------------------------------------------
-    rl_start = 1
-    rl_end   = min_rlcell
-    i_startblk = ppatch%cells%start_blk(rl_start,1)
-    i_endblk   = ppatch%cells%end_blk(rl_end,1)
+    TYPE(t_subset_range), POINTER :: all_cells
+    !-------------------------------------------------------------------------
+    all_cells => ppatch%cells%all
 
     !fluxes at first layer
     !pupflux_i(:,1,:) =  pvar_c(:,1,:)*(pw_c(:,1,:)+pw_c(:,2,:))*0.5_wp!
     pupflux_i(:,1,:) =pvar_c(:,1,:)* pw_c(:,1,:)
 
-    DO jb = i_startblk, i_endblk
-      CALL get_indices_c(ppatch, jb, i_startblk, i_endblk,&
-                      & i_startidx, i_endidx, rl_start, rl_end)
-        DO jc = i_startidx, i_endidx
+    DO jb = all_cells%start_block, all_cells%end_block
+      CALL get_index_range(all_cells, jb, i_startidx_c, i_endidx_c)
+        DO jc = i_startidx_c, i_endidx_c
           z_dolic = v_base%dolic_c(jc,jb)
           IF(z_dolic>=MIN_DOLIC)THEN
             DO jk = 2, z_dolic
