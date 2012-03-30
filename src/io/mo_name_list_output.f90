780f6e3448c551f6473426108dd08e984c2aec34
Florian Prill <florian.prill@dwd.de>
2012-03-30 08:02:52 +0000

    Added a note on global-size arrays in new namelist output.

--- src/io/mo_name_list_output.f90
+++ src/io/mo_name_list_output.f90
@@ -144,6 +144,11 @@ MODULE mo_name_list_output
   ! TYPE t_reorder_info describes how local cells/edges/verts
   ! have to be reordered to get the global array.
   ! Below, "points" refers to either cells, edges or verts.
+  !
+  ! TODO[FP] Note that the "reorder_info" contains fields of *global*
+  !          size (reorder_index, log_dom_index). On the compute PEs
+  !          these fields could be deallocated after the call to
+  !          "transfer_reorder_info" in the setup phase!
 
   TYPE t_reorder_info
     INTEGER :: n_glb  ! Global number of points per physical patch
