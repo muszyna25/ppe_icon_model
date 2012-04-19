b3c25dfc62adabb7b3131c922f404842075d7cc5
Einar Olason <einar.olason@mpimet.mpg.de>
2012-04-19 12:50:51 +0000

    [sea ice] Minor clean-up

--- src/atm_oce_lnd_interface/mo_oce_bulk.f90
+++ src/atm_oce_lnd_interface/mo_oce_bulk.f90
@@ -84,7 +84,7 @@ USE mo_loopindices,         ONLY: get_indices_c
 USE mo_math_utilities,      ONLY: gvec2cvec, cvec2gvec
 USE mo_sea_ice,             ONLY: t_sea_ice, t_sfc_flx, t_atmos_fluxes, t_atmos_for_ocean, &
                                   calc_atm_fluxes_from_bulk, set_ice_albedo, set_ice_temp, &
-                                  ice_slow, prepareAfterRestart
+                                  ice_slow, ice_fast, prepareAfterRestart
 USE mo_oce_index,           ONLY: print_mxmn, ipl_src
 USE mo_coupling_config,     ONLY: is_coupled_run
 USE mo_icon_cpl_exchg,      ONLY: ICON_cpl_put, ICON_cpl_get
@@ -493,12 +493,12 @@ CONTAINS
         IF (iforc_type == 2 .OR. iforc_type == 5) &
           & CALL calc_atm_fluxes_from_bulk (p_patch, p_as, p_os, p_ice, Qatm)
 
-        ! This is a stripped down version of ice_fast for ice-ocean model only
-        CALL prepareAfterRestart(p_ice)
-        CALL set_ice_albedo(p_patch,p_ice)
-        CALL set_ice_temp(p_patch,p_ice,Qatm)
-
-        Qatm%counter = 1
+        CALL ice_fast(p_patch, p_ice, Qatm, Qatm)
+        ! Ice_fast and ice_slow are designed for an ice model that's split between the
+        ! atmosphere and ocean models. For ice-ocean only we need to do some minor corrections.
+        Qatm%counter = 2
+        p_ice%Qbot   (:,:,:) = 2.0_wp * p_ice%Qbot
+        p_ice%Qtop   (:,:,:) = 2.0_wp * p_ice%Qtop
         CALL ice_slow(p_patch, p_os, p_ice, Qatm, p_sfc_flx)
       ENDIF
 
@@ -673,6 +673,7 @@ CONTAINS
           Qatm%dlatdT (:,:,:) = 0.0_wp
           Qatm%dLWdT  (:,:,:) = -4.0_wp * zemiss_def*StBo * (p_ice%Tsurf(:,:,:) + tmelt)**3
 
+          ! This is a stripped down version of ice_fast, since we don't want to calculate albedo
           ! For now the ice albedo is the same as ocean albedo
           CALL prepareAfterRestart(p_ice)
           ! CALL set_ice_albedo(p_patch,p_ice)
