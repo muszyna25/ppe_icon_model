933ad83a1dc2852fca271cf7317cc157da670eb5
Walter Sauf <walter.sauf@meteogrid.de>
2012-06-19 16:33:11 +0000

    save a intermediate version of plot build scripts

--- scripts/buildbot_scripts/py/exp_plot.py
+++ scripts/buildbot_scripts/py/exp_plot.py
@@ -14,11 +14,11 @@ class EXP_plot(HtmlResource):
  function anzeigen(das,was){
  if (document.getElementById(das).style.display=='none') {
   document.getElementById(das).style.display='block'; 
-  was.src="gif/open.gif";
+  was.src="open.gif";
  }
  else {
   document.getElementById(das).style.display='none';
-   was.src='gif/closed.gif';
+   was.src='closed.gif';
   }
 }
  </script>
@@ -29,6 +29,17 @@ class EXP_plot(HtmlResource):
         global exp_plot_Info
         global l_nightly
 	self.get_info(request)
+	
+	Archive_Button_Dict = {}
+	Archive_Button_Dict['date']  = []
+	Archive_Button_Dict['rev']   = []
+	Archive_Button_Dict['comp']  = []
+	Archive_Button_Dict['build'] = []
+	Archive_Button_Dict['exp']   = []
+	Archive_Button_Dict['file']  = []
+	
+#        ExpListInfo = []
+
 	
         file = open("icon_run/svn_info")
         while 1:
@@ -48,11 +59,66 @@ class EXP_plot(HtmlResource):
                
         file.close
          
-	data = '''
+         
+# Build new emty date_Dict type
+        date_Dict = {}
+
+# Build new emty rev_Dict type and save it in the date_Dict under the correct Date
+        rev_Dict = {}
+        date_Dict[svn_Date] = rev_Dict
+	Archive_Button_Dict['date'].append(svn_Date)
+
+# Build new emty comp_Dict type and save it in the rev_Dict under the correct reverents number
+        comp_Dict = {}
+	rev_Dict[svn_Rev] = comp_Dict
+	Archive_Button_Dict['rev'].append(svn_Rev)
+	
+        p = "./public_html/archive/" + svn_Date + "/buildbot/" + svn_Rev + "/"
+	for computer in os.listdir(p):
+# Build for each computer a new emty build_Dict type and save it in the comp_Dict under the correct computer name
+          build_Dict =  {}
+          comp_Dict[computer] = build_Dict
+  	  Archive_Button_Dict['comp'].append(computer)
+          for build in os.listdir(p + "/" + computer + "/"):
+# Build for each build number a new emty exp_Dict type and save it in the build_Dict under the correct builder number
+            exp_Dict = {}
+            build_Dict[build] = exp_Dict
+    	    Archive_Button_Dict['build'].append(build)
+            for exp in os.listdir(p + "/" + computer + "/" + build + "/"):
+	      if exp != "run_info.txt": 
+# To Do
+# Hier muss noch eingebaut werden das bei 'all' alle Experimente genommen werden.
+		if exp == exp_plot_Info:
+	          file_List = []
+                  exp_Dict[exp] = file_List
+      	          Archive_Button_Dict['exp'].append(exp)
+# Build a new file list and include the all stateman into the list
+                  for fi in os.listdir(p + "/" + computer + "/" + build + "/" + exp + "/plots/"):
+# Append all existing files from the the experiment
+	            file_List.append(fi)
+       	            Archive_Button_Dict['file'].append(fi)
+
+        Archive_Button_Dict['date']  = list(sorted(set(Archive_Button_Dict['date'])))
+ 	Archive_Button_Dict['rev']   = list(sorted(set(Archive_Button_Dict['rev'])))
+	Archive_Button_Dict['comp']  = list(sorted(set(Archive_Button_Dict['comp'])))
+	Archive_Button_Dict['build'] = list(sorted(set(Archive_Button_Dict['build'])))
+	Archive_Button_Dict['exp']   = list(sorted(set(Archive_Button_Dict['exp'])))
+	Archive_Button_Dict['file']  = list(sorted(set(Archive_Button_Dict['file'])))
+	
+	if len(Archive_Button_Dict['comp']) > 1:
+	  Archive_Button_Dict['comp'].insert(0, 'all')
+#	if len(Archive_Button_Dict['build']) > 1:
+#	  Archive_Button_Dict['build'].insert(0, 'all')
+#	if len(Archive_Button_Dict['exp']) > 1:
+#	  Archive_Button_Dict['exp'].insert(0, 'all')
+#	if len(Archive_Button_Dict['file']) > 1:
+#	  Archive_Button_Dict['file'].insert(0, 'all')
+	
+        data = '''
   <div style="float:left; padding:3px; margin:5px width:350px;">
     <div id="arch_menu" >
       <h1>ICON Buildbot Archive</h1>
-      <table border=""1 cellspacing="0" cellpadding="0" align="left" width="400">
+      <table border="0" cellspacing="0" cellpadding="0" align="left" width="400">
         <colgroup>
           <col width="100">
           <col width="250">
@@ -64,12 +130,19 @@ class EXP_plot(HtmlResource):
 	    <form name=date_form>
 	    
 	'''
-
-	data += "<select  disabled=\"disabled\">\n"
+        if l_nightly == "nightly":
+	  data += "<select  disabled=\"disabled\">\n"
+	else:
+	  data += "<select>\n"
+	  
         data += "<option selected>" + tmp_Date.split('-')[0] + "</option></select>\n"
         data += "</select>\n"
 	
-	data += "<select  disabled=\"disabled\">\n"
+        if l_nightly == "nightly":
+	  data += "<select  disabled=\"disabled\">\n"
+	else:
+	  data += "<select>\n"
+
         data += "<option selected>" + tmp_Date.split('-')[1] + "</option></select>"
         data += "</select>\n"
 	
@@ -82,15 +155,27 @@ class EXP_plot(HtmlResource):
 	    <form name=date_form>
 	'''
 
-	data += "<select  disabled=\"disabled\">\n"
+        if l_nightly == "nightly":
+	  data += "<select  disabled=\"disabled\">\n"
+	else:
+	  data += "<select>\n"
+
         data += "<option selected>" + tmp_Date.split('-')[0] + "</option></select>"
         data += "</select>\n"
 	
-	data += "<select  disabled=\"disabled\">\n"
+        if l_nightly == "nightly":
+	  data += "<select  disabled=\"disabled\">\n"
+	else:
+	  data += "<select>\n"
+
         data += "<option selected>" + tmp_Date.split('-')[1] + "</option></select>"
         data += "</select>\n"
 	
-	data += "<select  disabled=\"disabled\">\n"
+        if l_nightly == "nightly":
+	  data += "<select  disabled=\"disabled\">\n"
+	else:
+	  data += "<select>\n"
+
         data += "<option selected>" + tmp_Date.split('-')[2].split(' ')[0] + "</option></select>"
         data += "</select>\n"
 	
@@ -144,7 +229,6 @@ class EXP_plot(HtmlResource):
 	      
 	data += "<option selected>" + exp_plot_Info + "</option>"
 	
-	exp_file_Info = "test_hat_jww_R2B04L31_DIV850"
 	data += '''
 	      </select>
 	    </form>
@@ -154,9 +238,16 @@ class EXP_plot(HtmlResource):
           <td><b>File</b></td>
             <td>
 	      <form name=file_form>
-		<select name=mytextarea disabled="disabled">
+		<select>
 	      '''
-	data += "<option selected>" + exp_file_Info + "</option>"
+        exp_file_Info = Archive_Button_Dict['file'][0]
+
+        for f in Archive_Button_Dict['file']:
+	  if f.strip(".png") == exp_file_Info:
+	    data += "<option selected>" + f.strip(".png") + "</option>"
+	  else:
+	    data += "<option>" + f.strip(".png") + "</option>"
+
 	data += '''
 	        </select>
 	      </form>
@@ -166,7 +257,7 @@ class EXP_plot(HtmlResource):
 	'''
         
         data += "<tr><td><b>Search</b><td>\n"
-        data += "  <input type=\"button\" name=\"Text 1\" value=\"OK\" disabled=\"disabled\">\n"
+        data += "  <input type=\"button\" name=\"Text 1\" value=\"OK\" >\n"
         data += "</td></tr>\n"
         
 	data += '''
@@ -174,7 +265,7 @@ class EXP_plot(HtmlResource):
     </div>
     <div id="arch_file_name">
       <h1>Available Plot List</h1>
-      <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
+      <table border="0" cellspacing="0" cellpadding="0" align="left" width="400">
         <tr>
           <td valign=top>
 
@@ -230,20 +321,20 @@ class EXP_plot(HtmlResource):
 
             data += "      <div id=\"file_" + str(f) + "\" class=\"Rand\" style=\"display: none;\">\n"
             data += "<!--- " + str(c) + ". File -->\n"
-            for fi in os.listdir(p + "/" + computer + "/" + build + "/" + exp_plot_Info + "/plots/"):
+            if os.path.isdir(p + "/" + computer + "/" + build + "/" + exp_plot_Info): 
+              for fi in os.listdir(p + "/" + computer + "/" + build + "/" + exp_plot_Info + "/plots/"):
 #	      if fi == exp_file_Info + ".png":
-              data += "<a href=\"#\" class=\"link\"><span style=\"color: #FF0000;\">" + fi + "</span></a><br>\n"
+                data += "<a href=\"#\" class=\"link\"><span style=\"color: #FF0000;\">" + fi.strip(".png") + "</span></a><br>\n"
+            else:
+              data += "<a href=\"#\" class=\"link\"><span style=\"color: #FF0000;\"> No File exist !!! </span></a><br>\n"
+            
             data += "<!--- Ende für file -->\n"
             data += "      </div>\n"
            
-          data += "<!--- Ende für Builds build -->\n"
           data += "    </div>\n"
           
-        data += "<!--- Ende für Computer -->\n"
         data += "  </div>\n"
-        data += "<!--- Ende für Date -->\n"
         data += "  </div>\n"
-        data += "<!--- Ende für Rev -->\n"
         data += "</div>\n"
               
 	data += '''
@@ -252,139 +343,84 @@ class EXP_plot(HtmlResource):
       </table>
     </div>
   </div>
-
   <div style="float:left; padding:3px; margin:5px  width:200px;"> 
     <div id="arch_ref">
-      <h1>Reference Plot</h1>
-      <table border="0" cellspacing="0" cellpadding="0" align="left" width="600">
-	<tr>
-	  <td>
-            <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-              <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-              <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-              <tr>   <td>Computer:</td>  <td>TORNADO_pgi</td>        </tr>
-              <tr>   <td>Build. Nr:</td>   <td>582</td>        </tr>
-              <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-            </table>
-          </td>
-          <td>
 	  '''
-        
-	data += "<img src=\"archive/" + svn_Date + "/buildbot/"+ svn_Rev +"/TORNADO_sun/1160/" + exp_plot_Info + "/plots/" + exp_file_Info + ".png\" />\n"
+        p = "./public_html/archive/ref_" + exp_plot_Info
+        if os.path.isdir(p):
+	  ref_date = os.listdir(p)[-1]
+
+          p2 = p + "/" + ref_date + "/buildbot/"
+	  ref_rev = os.listdir(p2)[-1]
+
+          p3 = p2 + "/" + ref_rev
+	  ref_comp = os.listdir(p3)[-1]
+
+          p4 = p3 + "/" + ref_comp
+	  ref_build = os.listdir(p4)[-1]
+	  
+          data += "<h1>Reference Plot (" + ref_date + ")</h1>"
+          data += "<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"600\">\n"
+	  data += "  <tr>\n    <td>"
+          data += "      <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"400\">\n"
+          data += "        <tr><td><b>Rev. Nr:</b></td><td>" + ref_rev + "</td>\n"
+          data += "            <td><b>Computer:</b></td><td>" + ref_comp +"</td>\n"
+          data += "            <td><b>Build. Nr:</b></td><td>" + ref_build + "</td>\n"
+          data += "            <td> <input type=\"button\" name=\"Text 1\" value=\"Replace\" ></td>\n"
+          data += "</tr>\n      </table>\n"
+          data += "    </td></tr>\n    <tr><td>\n"
+	  data += "      <img src=\"archive/ref_"+exp_plot_Info+"/"+ref_date+"/buildbot/"+ref_rev+"/"+ref_comp+"/"+ref_build+"/"+exp_plot_Info+"/plots/"+exp_file_Info+"\"/>\n"
+	  data += "    </td>"
+	  data += "  </tr>"
+	  data += "</table>"
+        else:
+          data += "<h1>Reference Plot (xxxx-xx-xx)</h1>"
+          data += "<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"600\">\n"
+	  data += "  <tr>\n    <td>"
+          data += "      <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"400\">\n"
+          data += "        <tr><td><b>Rev. Nr:</b></td><td> ????</td>\n"
+          data += "            <td><b>Computer:</b></td><td> ???? </td>\n"
+          data += "            <td><b>Build. Nr:</b></td><td> ???? </td>\n"
+          data += "            <td> <input type=\"button\" name=\"Text 1\" value=\"Replace\" ></td>\n"
+          data += "</tr>\n      </table>\n"
+          data += "    </td></tr>\n    <tr><td>\n"
+#	  data += "      <img src=\"archive/ref_" + exp_plot_Info + "/" + ref_date + "/buildbot/" + ref_rev + "/" + ref_comp + "/" + ref_build + "/" + exp_plot_Info + "/plots/" + exp_file_Info + "\" />\n"
+	  data += "    </td>"
+	  data += "  </tr>"
+	  data += "</table>"
+	  
 	data += '''
-          </td>
-        </tr>
-      </table>
-    
     </div>
     <div id="arch_plots">
       <h1>Plot Area</h1>
-      <table border="0" cellspacing="0" cellpadding="0" align="left" width="400">
-        <!--- Plot line one --->
-          <tr>
-	    <td>
-	      <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-	        <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-		<tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-		<tr>   <td>Computer:</td>  <td>TORNADO_pgi</td>        </tr>
-		<tr>   <td>Build. Nr:</td>   <td>582</td>        </tr>
-		<tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-	      </table>
-            </td>
-            <td>
 	  '''
+
+        p = "./public_html/archive/" + svn_Date + "/buildbot/" + svn_Rev + "/"
+        data += "  <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"400\">\n"
         
-	data += "<img src=\"a/" + svn_Date + "/buildbot/"+ svn_Rev +"/TORNADO_sun/1160/" + exp_plot_Info + "/plots/" + exp_file_Info + ".png\" />\n"
+        for comp in Archive_Button_Dict['comp']:
+#        for computer in os.listdir(p):
+	  if os.path.isdir(p + "/" + comp): 
+            for build in os.listdir(p + "/" + comp + "/"):
+              if os.path.isdir(p + "/" + comp + "/"+ build + "/" + exp_plot_Info): 
+	        data += "<tr>"
+	        data += "  <td>"
+	        data += "    <table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"left\" width=\"400\">\n"
+#	        data += "      <tr><td>Date:</td><td>" + svn_Date + "</td></tr>\n"
+#	        data += "      <tr><td>Rev. Nr:</td><td>" + svn_Rev + "</td></tr>\n"
+	        data += "      <tr><td><b>Computer:</b></td><td>" + comp + "</td><td><b>Build. Nr:</b></td><td>" + build + "</td></tr>\n"
+#	        data += "      <tr><td>Exp:</td><td>" + exp_plot_Info + "</td></tr>\n"
+	        data += "    </table>\n"
+                data += "  </td></tr>\n"
+                data += "  <tr><td>\n"        
+	        data += "    <img src=\"archive/" + svn_Date + "/buildbot/"+ svn_Rev + "/" + comp +"/"+ build + "/" + exp_plot_Info + "/plots/" + exp_file_Info + "\" />\n"
+	        data += "  </td>\n"
+	        data += "</tr>\n"
+        data += "</table>\n"
+
 	data += '''
-	    </td>
-	  </tr>
-
-
-	  <!--- Plot line 2 --->
-	  <tr>
-	    <td>
-              <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-		<tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-	        <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-                <tr>   <td>Computer:</td>  <td>BLIZZ_yMyO</td>        </tr>
-                <tr>   <td>Build. Nr:</td>   <td>557</td>        </tr>
-                <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-              </table>
-           </td>
-           <td>
-	      <img src="archive/2012-04-04/buildbot/8609/TORNADO_sun/1068/test_hat_jww/plots/test_hat_jww_R2B04L31_DIV850_day09.png" />
-	   </td>
-	 </tr>
-
-	    <!--- Plot line 3 --->
-
-	 <tr>
-	   <td>
-	     <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-	       <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-	       <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-               <tr>   <td>Computer:</td>  <td>BLIZZ_nMnO</td>        </tr>
-               <tr>   <td>Build. Nr:</td>   <td>566</td>        </tr>
-               <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-             </table>
-           </td>
-           <td>
-	      <img src="archive/2012-04-04/buildbot/8609/TORNADO_sun/1068/test_hat_jww/plots/test_hat_jww_R2B04L31_DIV850_day09.png" />
-	   </td>
-	 </tr>
-
-	    <!--- Plot line 4 --->
-
-	 <tr>
-	   <td>
-	     <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-	       <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-	       <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-               <tr>  <td>Computer:</td>  <td>TORNADO_nag</td>        </tr>
-               <tr>   <td>Build. Nr:</td>   <td>605</td>        </tr>
-               <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-             </table>
-          </td>
-          <td>
-	      <img src="archive/2012-04-04/buildbot/8609/TORNADO_sun/1068/test_hat_jww/plots/test_hat_jww_R2B04L31_DIV850_day09.png" />
-	  </td>
-	</tr>
-	    <!--- Plot line 5 --->
-	<tr>
-	  <td>
-	    <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-	      <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-	      <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-              <tr>   <td>Computer:</td>  <td>BLIZZ_yMnO</td>        </tr>
-              <tr>   <td>Build. Nr:</td>   <td>731</td>        </tr>
-              <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-            </table>
-          </td>
-          <td>
-	      <img src="archive/2012-04-04/buildbot/8609/TORNADO_sun/1068/test_hat_jww/plots/test_hat_jww_R2B04L31_DIV850_day09.png" />
-	  </td>
-	</tr>
-
-	    <!--- Plot line 6 --->
-	<tr>
-	  <td>
-	    <table border="0" cellspacing="0" cellpadding="0" align="left" width="200">
-	      <tr>   <td>Date:</td>      <td>2011-06-20</td>  </tr>
-	      <tr>   <td>Rev. Nr:</td>   <td>4725</td>        </tr>
-	      <tr>   <td>Computer:</td>  <td>BLIZZ_nMyO</td>        </tr>
-              <tr>   <td>Build. Nr:</td>   <td>558</td>        </tr>
-              <tr>   <td>Exp:</td>       <td>test_hat_jww</td>        </tr>
-            </table>
-          </td>
-          <td>
-              <img src="archive/2012-04-04/buildbot/8609/TORNADO_sun/1068/test_hat_jww/plots/test_hat_jww_R2B04L31_DIV850_day09.png" />
-          </td>
-	</tr>        
-    </table>
-
     </div>
   </div>
-
         '''
 
         return data
