52e97836660236cabfd2a0034b739f46ae86d3e5
Stephan Lorenz <stephan.lorenz@mpimet.mpg.de>
2012-06-27 15:29:49 +0000

    [ocean] bugfix

--- src/oce_dyn_icohom/mo_oce_tracer.f90
+++ src/oce_dyn_icohom/mo_oce_tracer.f90
@@ -83,6 +83,8 @@ PRIVATE
 
 ! !VERSION CONTROL:
 CHARACTER(len=*), PARAMETER :: version = '$Id$'
+CHARACTER(len=12)  :: str_module = 'oceTracer   '  ! Output of module for 1 line debug
+INTEGER            :: idt_src    = 1               ! Level of detail for 1 line debug
 
 !
 ! PUBLIC INTERFACE
