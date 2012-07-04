ffac58848b26486db4fe55331262dc41a08dddbc
Florian Prill <florian.prill@dwd.de>
2012-07-04 08:58:10 +0000

    Implemented loop for rel_hum computation.

--- src/atm_phy_nwp/mo_util_phys.f90
+++ src/atm_phy_nwp/mo_util_phys.f90
@@ -1,6 +1,5 @@
 !>
-!! Implementation of physics utility routines, mostly domain
-!! independent and elemental.
+!! Implementation of physics utility routines.
 !!
 !! @par Revision History
 !!  Initial revision  :  F. Prill, DWD (2012-07-03)
@@ -32,6 +31,11 @@
 !! liability or responsibility for the use, acquisition or application of this
 !! software.
 !!
+
+!----------------------------
+#include "omp_definitions.inc"
+!----------------------------
+
 MODULE mo_util_phys
 
   USE mo_kind,      ONLY: wp
@@ -39,6 +43,12 @@ MODULE mo_util_phys
     &                                 rdv,             & !! r_d / r_v
     &                                 cpd, p0ref, rd
   USE mo_satad,                 ONLY: sat_pres_water
+  USE mo_fortran_tools,         ONLY: assign_if_present
+  USE mo_impl_constants,        ONLY: min_rlcell_int
+  USE mo_model_domain,          ONLY: t_patch
+  USE mo_nonhydro_types,        ONLY: t_nh_prog, t_nh_diag
+  USE mo_run_config,            ONLY: iqv
+  USE mo_loopindices,           ONLY: get_indices_c
 
   IMPLICIT NONE
 
@@ -51,6 +61,8 @@ MODULE mo_util_phys
 CONTAINS
 
   !> POINTWISE computation of relative humidity as r=e/e_sat
+  !!
+  !! (domain independent and elemental)
   !!
   !! @par Revision History
   !! Initial revision  :  F. Prill, DWD (2012-07-03) 
@@ -72,5 +84,76 @@ CONTAINS
     rel_hum = 100._wp * e/e_s
 
   END FUNCTION rel_hum
+
+
+  !> computation of relative humidity as r=e/e_sat
+  !!
+  !! @par Revision History
+  !! Initial revision  :  F. Prill, DWD (2012-07-04) 
+  SUBROUTINE compute_field_rel_hum(ptr_patch, p_prog, p_diag, out_var, &
+    &                              opt_slev, opt_elev, opt_rlstart, opt_rlend)
+
+    ! patch on which computation is performed:
+    TYPE(t_patch), TARGET, INTENT(in) :: ptr_patch
+    ! nonhydrostatic state
+    TYPE(t_nh_prog), INTENT(IN) :: p_prog
+    TYPE(t_nh_diag), INTENT(IN) :: p_diag
+    ! output variable, dim: (nproma,nlev,nblks_c):
+    REAL(wp),INTENT(INOUT) :: out_var(:,:,:)
+    ! optional vertical start/end level:
+    INTEGER, INTENT(in), OPTIONAL     :: opt_slev, opt_elev
+    ! start and end values of refin_ctrl flag:
+    INTEGER, INTENT(in), OPTIONAL     :: opt_rlstart, opt_rlend
+   
+    ! local variables
+    REAL(wp) :: temp, qv, p_ex
+    INTEGER  :: slev, elev, rl_start, rl_end, i_nchdom,     &
+      &         i_startblk, i_endblk, i_startidx, i_endidx, &
+      &         jc, jk, jb
+
+    ! default values
+    slev     = 1
+    elev     = UBOUND(out_var,2)
+    rl_start = 2
+    rl_end   = min_rlcell_int
+    ! check optional arguments
+    CALL assign_if_present(slev,     opt_slev)
+    CALL assign_if_present(elev,     opt_elev)
+    CALL assign_if_present(rl_start, opt_rlstart)
+    CALL assign_if_present(rl_end,   opt_rlend)
+    ! values for the blocking
+    i_nchdom   = MAX(1,ptr_patch%n_childdom)
+    i_startblk = ptr_patch%cells%start_blk(rl_start,1)
+    i_endblk   = ptr_patch%cells%end_blk(rl_end,i_nchdom)
+
+!$OMP PARALLEL    
+!$OMP DO PRIVATE(jb,i_startidx,i_endidx,jk,jc), ICON_OMP_RUNTIME_SCHEDULE
+    DO jb = i_startblk, i_endblk
+      CALL get_indices_c(ptr_patch, jb, i_startblk, i_endblk, &
+        i_startidx, i_endidx, rl_start, rl_end)
+      
+#ifdef __LOOP_EXCHANGE
+      DO jc = i_startidx, i_endidx
+        DO jk = slev, elev
+#else
+!CDIR UNROLL=3
+      DO jk = slev, elev
+        DO jc = i_startidx, i_endidx
+#endif
+
+          ! get values for temperature, etc.:
+          temp = p_diag%temp(jc,jk,jb)
+          qv   = p_prog%tracer_ptr(iqv)%p_3d(jc,jk,jb)
+          p_ex = p_prog%exner(jc,jk,jb)
+          !-- compute relative humidity as r = e/e_s:
+          out_var(jc,jk,jb) = rel_hum(temp, qv, p_ex)
+
+        END DO
+      END DO
+    END DO
+!$OMP END DO NOWAIT
+!$OMP END PARALLEL
+
+  END SUBROUTINE compute_field_rel_hum
 
 END MODULE mo_util_phys
