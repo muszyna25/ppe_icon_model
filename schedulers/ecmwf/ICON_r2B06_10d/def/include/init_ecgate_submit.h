45a9d19f21dd5b7d48520a1f528c6f02eb1aa78e
DWD at ECMWF <florian.prill@dwd.de>
2012-07-06 11:52:12 +0000

    ECsuite: some minor fixes (Martin)

---
+++ schedulers/ecmwf/ICON_r2B06_10d/def/include/init_ecgate_submit.h
@@ -0,0 +1,95 @@
+#==========================================================================
+#  Loadleveler Directives
+#==========================================================================
+#@ job_name         = ICON_%TASK%
+#@ output           = %SMSJOBOUT%
+#@ error            = %SMSJOBOUT%
+#@ notification     = error
+#@ shell            = /usr/bin/ksh
+#@ class            = normal
+#@ queue
+
+
+#==========================================================================
+#  shell settings
+#==========================================================================
+
+set -e
+set -u
+set -x
+
+SUITE=%SUITE%
+TASK=%TASK%
+SMSPASS=%SMSPASS%
+SMSNODE=%SMSNODE%
+SMSNAME=%SMSNAME%
+SMSHOME=%SMSHOME%
+SMS_PROG=%SMS_PROG%
+WSHOST=%SCHOST%
+SCHOST=%SCHOST%
+USER=%USER%
+SMSJOBOUT=%SMSJOBOUT%
+LOGDIR=%SMSOUT%
+
+#==========================================================================
+#  SMS Definitions
+#==========================================================================
+
+LOADL_STEP_ID=${LOADL_STEP_ID:=NOT_SET}
+QSUB_REQID=${QSUB_REQID:=NOT_SET}
+
+if [[ $LOADL_STEP_ID != NOT_SET ]] ; then
+   SMSRID=$(echo $LOADL_STEP_ID | cut -f2 -d.)
+   JOB_ID=$LOADL_STEP_ID
+elif [[ $QSUB_REQID != NOT_SET ]] ; then
+   SMSRID=$(echo $QSUB_REQID | cut -f1 -d.)
+   JOB_ID=$QSUB_REQID
+else
+  SMSRID=$$
+  JOB_ID=$SMSRID
+fi
+
+export SMSPASS SMSNODE SMSNAME SMS_PROG SMSHOME SMSJOBOUT
+
+export WSHOST SCHOST USER SUITE
+
+#==========================================================================
+#  Tell sms that it started
+#==========================================================================
+
+smsinit $SMSRID
+
+#==========================================================================
+
+#==========================================================================
+#  Define error handler
+#==========================================================================
+
+ERROR() {
+  set -x
+  set +e
+  wait
+  smsabort 
+  trap 0
+  date
+  times
+  echo "environment was:"
+  printenv | sort
+  exit
+}
+
+#==========================================================================
+#  Define trap handler, including "normal" exit before smscomplete
+#==========================================================================
+
+export SMS_SIGNAL_LIST="1 2 3 4 5 6 7 8 9 10 11 12 13 15 24"
+
+trap ERROR 0
+trap '{ echo "Killed by a signal"; ERROR ; }' \
+     $SMS_SIGNAL_LIST
+
+[[ -d $TMPDIR ]] && cd $TMPDIR
+
+date
+
+
