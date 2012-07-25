6a9e012ed62143328c52db23130e3f66d5c52008
Daniel Reinert <Daniel.Reinert@dwd.de>
2012-07-25 13:34:39 +0000

    minor update of dcmip_setgattr.sh

--- scripts/postprocessing/tools/dcmip_setgattr.sh
+++ scripts/postprocessing/tools/dcmip_setgattr.sh
@@ -13,12 +13,13 @@ usage()
     echo   -r      horizontal resolution
     echo   -l      levels
     echo   -g      grid
+    echo   -t      time frequency
     echo   -d      description
     echo   -f      file to change
     echo 
 }
 #----------------------------------------------------------------------------------------------
-while getopts ":c:r:l:g:d:f:h" opt
+while getopts ":c:r:l:g:t:d:f:h" opt
 do
     case $opt in
 	h) usage $0
@@ -44,6 +45,12 @@ do
 	    ;;
 	g) grid=$OPTARG
 	    if [ "$grid" == "" ]
+	    then
+		usage $0
+	    fi
+	    ;;
+	t) tfreq=$OPTARG
+	    if [ "$tfreq" == "" ]
 	    then
 		usage $0
 	    fi
@@ -93,13 +100,19 @@ then
 fi
 if [ "$grid" == "" ]
 then
-    echo ERROR: grid  missing ...
+    echo ERROR: grid missing ...
+    usage $0
+    exit 1
+fi
+if [ "$tfreq" == "" ]
+then
+    echo ERROR: time frequency missing ...
     usage $0
     exit 1
 fi
 if [ "$desc" == "" ]
 then
-    echo ERROR: description  missing ...
+    echo ERROR: description missing ...
     usage $0
     exit 1
 fi
@@ -112,16 +125,19 @@ ncatted -a test_case,global,d,, $file
 ncatted -a horizontal_resolution,global,d,, $file 
 ncatted -a levels,global,d,, $file 
 ncatted -a grid,global,d,, $file 
+ncatted -a time_frequency,global,d,, $file 
 ncatted -a equation,global,d,, $file 
 ncatted -a description,global,d,, $file 
 #----------------------------------------------------------------------------------------------
 # add the attributes
 ncatted -a Conventions,global,c,c,"CF-1.0" $file 
-ncatted -a model,global,c,c,"icon-mpi-dwd" $file 
+ncatted -a model,global,c,c,"icon-mpi-dwd" $file
+ncatted -a native_grid,global,c,c,"tri" $file  
 ncatted -a test_case,global,c,c,"$case" $file 
 ncatted -a horizontal_resolution,global,c,c,"$hres" $file 
 ncatted -a levels,global,c,c,"$levs" $file 
 ncatted -a grid,global,c,c,"$grid" $file 
+ncatted -a time_frequency,global,c,c,"$tfreq" $file 
 ncatted -a equation,global,c,c,"nonhydro" $file 
 ncatted -a description,global,c,c,"$desc" $file 
 #----------------------------------------------------------------------------------------------
