5edf990c5c833fe9a7c2ce052c53392fe35e9724
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2012-11-23 13:40:32 +0000

    Make ZA_* constants upcase

--- src/shared/mo_cdi_constants.f90
+++ src/shared/mo_cdi_constants.f90
@@ -24,26 +24,26 @@ MODULE mo_cdi_constants
   ! to distinguish vertical axes of the same type (e.g. ZAXIS_HEIGHT), 
   ! which may have different level heights or numbers. 
   !  
-  INTEGER, PARAMETER, PUBLIC      :: ZA_surface             =  1
+  INTEGER, PARAMETER, PUBLIC      :: ZA_SURFACE             =  1
   ! Atmosphere
-  INTEGER, PARAMETER, PUBLIC      :: ZA_hybrid              =  2
-  INTEGER, PARAMETER, PUBLIC      :: ZA_hybrid_half         =  3
-  INTEGER, PARAMETER, PUBLIC      :: ZA_depth_below_land    =  4
-  INTEGER, PARAMETER, PUBLIC      :: ZA_depth_below_land_p1 =  5
-  INTEGER, PARAMETER, PUBLIC      :: ZA_generic_snow        =  6
-  INTEGER, PARAMETER, PUBLIC      :: ZA_generic_snow_p1     =  7
-  INTEGER, PARAMETER, PUBLIC      :: ZA_pressure            =  8
-  INTEGER, PARAMETER, PUBLIC      :: ZA_height              =  9
-  INTEGER, PARAMETER, PUBLIC      :: ZA_height_2m           = 10
-  INTEGER, PARAMETER, PUBLIC      :: ZA_height_10m          = 11
-  INTEGER, PARAMETER, PUBLIC      :: ZA_altitude            = 12
-  INTEGER, PARAMETER, PUBLIC      :: ZA_meansea             = 13
-  INTEGER, PARAMETER, PUBLIC      :: ZA_isentropic          = 14
+  INTEGER, PARAMETER, PUBLIC      :: ZA_HYBRID              =  2
+  INTEGER, PARAMETER, PUBLIC      :: ZA_HYBRID_HALF         =  3
+  INTEGER, PARAMETER, PUBLIC      :: ZA_DEPTH_BELOW_LAND    =  4
+  INTEGER, PARAMETER, PUBLIC      :: ZA_DEPTH_BELOW_LAND_P1 =  5
+  INTEGER, PARAMETER, PUBLIC      :: ZA_GENERIC_SNOW        =  6
+  INTEGER, PARAMETER, PUBLIC      :: ZA_GENERIC_SNOW_P1     =  7
+  INTEGER, PARAMETER, PUBLIC      :: ZA_PRESSURE            =  8
+  INTEGER, PARAMETER, PUBLIC      :: ZA_HEIGHT              =  9
+  INTEGER, PARAMETER, PUBLIC      :: ZA_HEIGHT_2M           = 10
+  INTEGER, PARAMETER, PUBLIC      :: ZA_HEIGHT_10M          = 11
+  INTEGER, PARAMETER, PUBLIC      :: ZA_ALTITUDE            = 12
+  INTEGER, PARAMETER, PUBLIC      :: ZA_MEANSEA             = 13
+  INTEGER, PARAMETER, PUBLIC      :: ZA_ISENTROPIC          = 14
   INTEGER, PARAMETER, PUBLIC      :: ZA_TOA                 = 15
   ! Ocean
-  INTEGER, PARAMETER, PUBLIC      :: ZA_depth_below_sea     = 16
-  INTEGER, PARAMETER, PUBLIC      :: ZA_depth_below_sea_half= 17
-  INTEGER, PARAMETER, PUBLIC      :: ZA_generic_ice         = 18
+  INTEGER, PARAMETER, PUBLIC      :: ZA_DEPTH_BELOW_SEA     = 16
+  INTEGER, PARAMETER, PUBLIC      :: ZA_DEPTH_BELOW_SEA_HALF= 17
+  INTEGER, PARAMETER, PUBLIC      :: ZA_GENERIC_ICE         = 18
 
 CONTAINS
 
