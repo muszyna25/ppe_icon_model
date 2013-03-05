5b74f60adcd43dab34c2b0a89b2c50c4f193c1a7
Stephan Lorenz <stephan.lorenz@mpimet.mpg.de>
2013-03-05 09:52:45 +0000

    [ocean] reactivate debug output

--- src/atm_oce_lnd_interface/mo_oce_bulk.f90
+++ src/atm_oce_lnd_interface/mo_oce_bulk.f90
@@ -1030,14 +1030,14 @@ CONTAINS
 
       !---------DEBUG DIAGNOSTICS-------------------------------------------
 !Commented out because of NAG-compiler, PK
-!       idt_src=1  ! output print level (1-5, fix)
-!       z_c2(:,:) = p_sfc_flx%forc_tracer_relax(:,:,2)
-!       CALL dbg_print('UpdSfc: Sal.-relax'        ,z_c2                    ,str_module,idt_src)
-!       idt_src=2  ! output print level (1-5, fix)
-!       z_c2(:,:) = p_sfc_flx%forc_tracer_relax(:,:,2)-p_os%p_prog(nold(1))%tracer(:,1,:,2)
-!       CALL dbg_print('UpdSfc: Sal.-difference'   ,z_c2                    ,str_module,idt_src)
-!       z_c2(:,:) = p_sfc_flx%forc_tracer(:,:,2)
-!       CALL dbg_print('UpdSfc: S-forc-trac [Km/s]',z_c2                    ,str_module,idt_src)
+        idt_src=1  ! output print level (1-5, fix)
+        z_c2(:,:) = p_sfc_flx%forc_tracer_relax(:,:,2)
+        CALL dbg_print('UpdSfc: Sal.-relax'        ,z_c2                    ,str_module,idt_src)
+        idt_src=2  ! output print level (1-5, fix)
+        z_c2(:,:) = p_sfc_flx%forc_tracer_relax(:,:,2)-p_os%p_prog(nold(1))%tracer(:,1,:,2)
+        CALL dbg_print('UpdSfc: Sal.-difference'   ,z_c2                    ,str_module,idt_src)
+        z_c2(:,:) = p_sfc_flx%forc_tracer(:,:,2)
+        CALL dbg_print('UpdSfc: S-forc-trac [Km/s]',z_c2                    ,str_module,idt_src)
       !---------------------------------------------------------------------
 
     ENDIF  !  irelax_2d_S >=1  salinity relaxation
