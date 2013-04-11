2b1aa1fc516a505dcf19ce451fa1c2f343dd4174
Stephan Lorenz <stephan.lorenz@mpimet.mpg.de>
2013-04-11 09:59:38 +0000

    [ocean]: update MOC plot

--- scripts/postprocessing/tools/mocTimeseries.sh
+++ scripts/postprocessing/tools/mocTimeseries.sh
@@ -16,42 +16,48 @@ if [[ ! -z "$2" ]]; then
 fi
 
 # calculate the moc timeseries in 1000 depth
-# cat, yearmean, mulc, netcdf of MOC to scr_moc.ym.nc
+# cat, yearmean, mulc, netcdf of MOC to moc.ym.nc
 files=$(echo $mocPattern | wc -w)
-if [[ $files -eq 1 ]]; then
-  ln -s $mocPattern scr_moc.ym.nc
-else
-  cdo -f nc -r -mulc,1.e-9 -yearmean -cat "${mocPattern}"  scr_moc.ym.nc
+if [[ $files -gt 1 ]]; then
+  cdo -f nc -r -mulc,1.e-9 -yearmean -cat "${mocPattern}"  moc.ym.nc
+  mocPattern=moc.ym.nc
 fi
-#  select AMOC at 1000m depth and write ascii data for gnuplot
-#if [[ "20" = $(cdo nlevel $(ls -1 $mocPattern | head -n 1)) ]]; then
-cdo outputkey,date,value -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 -sellevel,1000 -setgrid,r1x180 scr_moc.ym.nc > moc.dat
-#else
-#cdo outputkey,date,value -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 -intlevel,1000 -sellevel,900/1100 -setgrid,r1x180 scr_moc.ym.nc > moc.dat
-#fi
-rm scr_moc.ym.nc
+
+# "moc.dat" - plot only:
+if [[ "$mocPattern" != "moc.dat" ]]; then
+
+  #  select AMOC at 1000m depth and write ascii data for gnuplot
+  if [[ "20" = $(cdo nlevel -selvar,$mocVar $(ls -1 $mocPattern | head -n 1)) ]]; then
+    cdo outputkey,date,value -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
+         -sellevel,1000 -setgrid,r1x180 moc.ym.nc > moc.dat
+  else
+    cdo outputkey,date,value -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
+         -intlevel,1000 -sellevel,900/1100 -setgrid,r1x180 moc.ym.nc > moc.dat
+  fi
+fi
 
 gnuplot -p <<EOF
-#set term postscript color
+#set terminal postscript color
 #set output 'moc-tims.ps'
-#set term png
+#set terminal png size 1000,600
 #set output 'moc-tims.png'
+
 
 set style data lines
 set grid
-set title font "Helvetica-Bold,18"
+set title font "Helvetica-Bold,16"
 set title  "N-Atlantic Meridional Overturning Circulation (MOC)"
 #set title  "N-Atlantic Meridional Overturning Circulation (MOC)" font "Helvetica-Bold,17"
-set font "Times-Roman,15"
-set xlabel "[simulation years]"
+set xlabel "simulation years" font "Times-Bold,14
+set ylabel "[SV]" font "Times-Bold,14
 set key left
 
 #set yrange [8.1:27.8]
 set ytics 2
 set mytics 4
 
-set size 0.95,0.95
-set origin 0.05,0.05
+set size 0.98,0.95
+set origin 0.02,0.02
 set timefmt x "%Y-%m-%d"
 set xdata time
 set format x "%Y"
@@ -60,3 +66,5 @@ set format x "%Y"
 #plot 'moc.dat' using 1:2 w l t
 plot 'moc.dat' using 1:2 w l t 'AMOC, 1000m, 40N-60N' lw 3 lt -1
 EOF
+
+#gwenview moc-tims.png
