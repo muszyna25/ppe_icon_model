d9f010e442c161b7efb42bfaceb3c46811a543ce
Anurag Dipankar <anurag.dipankar@mpimet.mpg.de>
2013-06-02 21:21:09 +0000

    modules for large-scale forcing on torus

---
+++ src/atm_phy_les/mo_ls_forcing.f90
@@ -0,0 +1,391 @@
+!>
+!! mo_ls_forcing
+!!
+!! This module initializes and applies the large-scale forcing for idealized simulations
+!! This module assumes that the model grid is FLAT
+!!
+!! @author Anurag Dipankar, MPI-M
+!!
+!! @par Revision History
+!! Initial release by Anurag Dipankar, MPI-M (2013-May-30)
+!!
+!! @par Copyright
+!! 2002-2013 by DWD and MPI-M
+!! This software is provided for non-commercial use only.
+!! See the LICENSE and the WARRANTY conditions.
+!!
+!! @par License
+!! The use of ICON is hereby granted free of charge for an unlimited time,
+!! provided the following rules are accepted and applied:
+!! <ol>
+!! <li> You may use or modify this code for your own non commercial and non
+!!    violent purposes.
+!! <li> The code may not be re-distributed without the consent of the authors.
+!! <li> The copyright notice and statement of authorship must appear in all
+!!    copies.
+!! <li> You accept the warranty conditions (see WARRANTY).
+!! <li> In case you intend to use the code commercially, we oblige you to sign
+!!    an according license agreement with DWD and MPI-M.
+!! </ol>
+!!
+!! @par Warranty
+!! This code has been tested up to a certain level. Defects and weaknesses,
+!! which may be included in the code, do not establish any warranties by the
+!! authors.
+!! The authors do not make any warranty, express or implied, or assume any
+!! liability or responsibility for the use, acquisition or application of this
+!! software.
+!!
+
+!----------------------------
+#include "omp_definitions.inc"
+!----------------------------
+
+MODULE mo_ls_forcing
+
+  USE mo_kind,                ONLY: wp
+  USE mo_io_units,            ONLY: filename_max, find_next_free_unit
+  USE mo_exception,           ONLY: message, finish, message_text
+  USE mo_impl_constants,      ONLY: success, max_char_length
+  USE mo_nonhydro_types,      ONLY: t_nh_prog, t_nh_diag, t_nh_metrics
+  USE mo_model_domain,        ONLY: t_patch
+  USE mo_intp_data_strc,      ONLY: t_int_state
+  USE mo_parallel_config,     ONLY: nproma
+  USE mo_loopindices,         ONLY: get_indices_e, get_indices_c, get_indices_v
+  USE mo_vert_utilities,      ONLY: vert_intp_full2half_cell_3d, vert_intp_linear_1d, &
+                                    global_sum_vert
+  USE mo_ls_forcing_nml
+  USE mo_physical_constants,  ONLY: rd
+
+  IMPLICIT NONE
+
+  PRIVATE
+
+  CHARACTER(len=*), PARAMETER :: version = '$Id$'
+
+  PUBLIC :: init_ls_forcing, apply_ls_forcing
+
+  !Anurag Dipankar, MPIM (2013-May): variables for large-scale (LS) 
+  !forcing to be used in periodic domain or other relveant cases.
+    
+  !Constant in time and horizontal space
+  REAL(wp), ALLOCATABLE ::  &
+    w_ls(:),            & !subsidence          [m/s]
+    u_geo  (:),         & !u-geostrophic wind  [m/s]
+    v_geo  (:),         & !v-geostrophic wind  [m/s]
+    ddt_temp_hadv_ls(:),& !LS horizontal advective tendency for temp [k/s]
+    ddt_temp_rad_ls(:), & !LS (not large-scale actually!) radiative tendency for temp [k/s]
+    ddt_qv_hadv_ls(:)     !LS horizontal advective tendency for qv [1/s]
+
+  CONTAINS
+
+
+  !>
+  !! init_ls_forcing
+  !!------------------------------------------------------------------------
+  !! Initialize large-scale forcings from input file
+  !!------------------------------------------------------------------------
+  !! @par Revision History
+  !! Initial release by Anurag Dipankar, MPI-M (2013-May-30)
+  SUBROUTINE init_ls_forcing(p_metrics)
+
+    TYPE(t_nh_metrics),INTENT(in),   TARGET :: p_metrics 
+
+    CHARACTER(len=max_char_length), PARAMETER :: routine = 'mo_ls_forcing:init_ls_forcing'
+
+    REAL(wp), ALLOCATABLE, DIMENSION(:) :: zz, zw, zu, zv, z_dt_temp_adv, &
+                                           z_dt_temp_rad, z_dt_qv_adv
+    INTEGER   :: iunit, ist, nk, jk, nlev
+    
+    !Open formatted file to read ls forcing data
+    iunit = find_next_free_unit(10,20)
+    OPEN (unit=iunit,file='ls_forcing.dat',access='SEQUENTIAL', &
+          form='FORMATTED', action='READ', status='OLD', IOSTAT=ist)
+
+    IF(ist/=success)THEN
+      CALL finish (TRIM(routine), 'open ls_forcing.dat failed')
+    ENDIF  
+
+    !Read the input file till end. The order of file assumed is:
+    !Z(m) - w_ls - u_geo - v_geo - ddt_temp_hadv_ls - ddt_temp_rad_ls - ddt_qv_hadv_ls    
+    
+    READ(iunit,*,IOSTAT=ist)nk
+    IF(ist/=success)CALL finish(TRIM(routine), 'problem reading first line in ls_forcing.dat')
+   
+    ALLOCATE( zz(nk), zw(nk), zu(nk), zv(nk), z_dt_temp_adv(nk), &
+              z_dt_temp_rad(nk), z_dt_qv_adv(nk) )
+
+    DO jk = nk , 1, -1
+      READ(iunit,*,IOSTAT=ist)zz(jk),zw(jk),zu(jk),zv(jk),z_dt_temp_adv(jk), &
+                              z_dt_temp_rad(jk),z_dt_qv_adv(jk)
+      IF(ist/=success)THEN
+          CALL finish (TRIM(routine), 'something wrong in forcing.dat')
+      END IF
+    END DO
+    
+    !Check if the file is written in descending order
+    IF(zz(1) < zz(nk))CALL finish (TRIM(routine), 'Writing LS forcing data in descending order!')
+
+    CLOSE(iunit)
+
+    nlev = SIZE(p_metrics%z_mc,2)
+    ALLOCATE( w_ls(nlev), u_geo(nlev), v_geo(nlev), ddt_temp_hadv_ls(nlev), &
+              ddt_temp_rad_ls(nlev), ddt_qv_hadv_ls(nlev) )
+
+    !Now perform interpolation to grid levels assuming:
+    !a) linear interpolation
+    !b) Beyond the last Z level the values are linearly extrapolated 
+    !c) Assuming model grid is flat-NOT on sphere
+
+    CALL vert_intp_linear_1d(zz,zw,p_metrics%z_mc(2,:,2),w_ls)
+    CALL vert_intp_linear_1d(zz,zu,p_metrics%z_mc(2,:,2),u_geo)
+    CALL vert_intp_linear_1d(zz,zv,p_metrics%z_mc(2,:,2),v_geo)
+    CALL vert_intp_linear_1d(zz,z_dt_temp_adv,p_metrics%z_mc(2,:,2),ddt_temp_hadv_ls)
+    CALL vert_intp_linear_1d(zz,z_dt_temp_rad,p_metrics%z_mc(2,:,2),ddt_temp_rad_ls)
+    CALL vert_intp_linear_1d(zz,z_dt_qv_adv,p_metrics%z_mc(2,:,2),ddt_qv_hadv_ls)
+
+    DEALLOCATE( zz, zw, zu, zv, z_dt_temp_adv, z_dt_temp_rad, z_dt_qv_adv )
+
+   
+  END SUBROUTINE init_ls_forcing
+
+  !>
+  !! apply_ls_forcing
+  !!------------------------------------------------------------------------
+  !! Apply large-scale forcing: called in the end from mo_nh_interface_nwp
+  !! It uses the most updated u,v to calculate the large-scale subsidence induced
+  !! advective tendencies. All other tendencies don't need any computation.
+  !! All tendencies are then accumulated in the slow physics tendency terms
+  !!   
+  !!------------------------------------------------------------------------
+  !! @par Revision History
+  !! Initial release by Anurag Dipankar, MPI-M (2013-May-30)
+  SUBROUTINE apply_ls_forcing(p_patch, p_metrics, p_prog, p_diag,      & !in
+                              qv, ql, qv_sfc, t_sfc, rl_start, rl_end, & !in
+                              ddt_u_ls, ddt_v_ls, ddt_temp_ls,         & !out
+                              ddt_qv_ls, ddt_ql_ls, ddt_qi_ls)           !out
+
+    TYPE(t_patch),INTENT(in),        TARGET :: p_patch
+    TYPE(t_nh_metrics),INTENT(in),   TARGET :: p_metrics 
+    TYPE(t_nh_prog),INTENT(in),      TARGET :: p_prog
+    TYPE(t_nh_diag),INTENT(in),      TARGET :: p_diag
+    INTEGER,        INTENT(in)              :: rl_start
+    INTEGER,        INTENT(in)              :: rl_end
+    REAL(wp),       INTENT(in)              :: qv(:,:,:)
+    REAL(wp),       INTENT(in)              :: ql(:,:,:)
+    REAL(wp),       INTENT(in)              :: t_sfc(:,:)
+    REAL(wp),       INTENT(in)              :: qv_sfc(:,:)
+    REAL(wp),       INTENT(out)             :: ddt_u_ls(:)
+    REAL(wp),       INTENT(out)             :: ddt_v_ls(:)
+    REAL(wp),       INTENT(out)             :: ddt_temp_ls(:)
+    REAL(wp),       INTENT(out)             :: ddt_qv_ls(:)
+    REAL(wp),       INTENT(out)             :: ddt_ql_ls(:)
+    REAL(wp),       INTENT(out)             :: ddt_qi_ls(:)
+    
+    CHARACTER(len=max_char_length), PARAMETER :: routine = 'mo_ls_forcing:apply_ls_forcing'
+    REAL(wp) :: varin(nproma,p_patch%nlev,p_patch%nblks_c)
+    REAL(wp) :: varout(nproma,p_patch%nlev+1,p_patch%nblks_c)
+    REAL(wp) :: rhos(nproma,p_patch%nblks_c)
+    REAL(wp) :: inv_n_gb_cells
+    REAL(wp),DIMENSION(p_patch%nlev+1) :: u_loc, v_loc, temp_loc, qv_loc, ql_loc
+    REAL(wp),DIMENSION(p_patch%nlev+1) :: u_gb, v_gb, temp_gb, qv_gb, ql_gb
+    REAL(wp), DIMENSION(p_patch%nlev)  :: rho_loc, rho_gb, exner_loc, inv_dz, exner_gb, &
+                                          uf_loc, vf_loc, uf_gb, vf_gb
+
+    INTEGER  :: i_nchdom, i_startblk, i_endblk, jk, nlev, nlevp1
+
+    !0) Initialize all passed ddt's to 0
+!$OMP PARALLEL WORKSHARE
+    ddt_u_ls  = 0._wp; ddt_v_ls = 0._wp;  ddt_temp_ls = 0._wp
+    ddt_qv_ls = 0._wp; ddt_ql_ls = 0._wp; ddt_qi_ls  = 0._wp
+!$OMP END PARALLEL WORKSHARE
+
+    i_nchdom  = MAX(1,p_patch%n_childdom)
+    nlev      = p_patch%nlev
+    nlevp1    = p_patch%nlev+1
+    inv_n_gb_cells = 1._wp / REAL(p_patch%n_patch_cells_g,wp)
+
+    i_startblk = p_patch%cells%start_blk(rl_start,1)
+    i_endblk   = p_patch%cells%end_blk(rl_end,i_nchdom)
+
+    !1) Horizontal mean of variables and their vertical advective tendency
+
+    IF(is_subsidence)THEN
+
+!$OMP PARALLEL WORKSHARE
+      !Get surface density
+      rhos(:,i_startblk:i_endblk) = p_diag%pres_sfc(:,i_startblk:i_endblk) / (rd * &
+                      t_sfc(:,i_startblk:i_endblk)*(1._wp+0.61_wp*qv_sfc(:,i_startblk:i_endblk)) )  
+
+      !rho*u
+      varin(:,:,i_startblk:i_endblk) = p_diag%u(:,:,i_startblk:i_endblk)*p_prog%rho(:,:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL vert_intp_full2half_cell_3d(p_patch, p_metrics, varin, varout, rl_start, rl_end)
+      CALL horizontal_sum(p_patch, varout, u_loc, rl_start, rl_end)
+
+!$OMP PARALLEL WORKSHARE
+      varin(:,:,i_startblk:i_endblk) = p_diag%v(:,:,i_startblk:i_endblk)*p_prog%rho(:,:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL vert_intp_full2half_cell_3d(p_patch, p_metrics, varin, varout, rl_start, rl_end)
+      CALL horizontal_sum(p_patch, varout, v_loc, rl_start, rl_end)
+
+!$OMP PARALLEL WORKSHARE
+      varin(:,:,i_startblk:i_endblk) = p_diag%temp(:,:,i_startblk:i_endblk)*p_prog%rho(:,:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL vert_intp_full2half_cell_3d(p_patch, p_metrics, varin, varout, rl_start, rl_end)
+!$OMP PARALLEL WORKSHARE
+      varout(:,nlevp1,i_startblk:i_endblk) = t_sfc(:,i_startblk:i_endblk)*rhos(:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL horizontal_sum(p_patch, varout, temp_loc, rl_start, rl_end)
+
+!$OMP PARALLEL WORKSHARE
+      varin(:,:,i_startblk:i_endblk) = qv(:,:,i_startblk:i_endblk)*p_prog%rho(:,:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL vert_intp_full2half_cell_3d(p_patch, p_metrics, varin, varout, rl_start, rl_end)
+!$OMP PARALLEL WORKSHARE
+      varout(:,nlevp1,i_startblk:i_endblk) = qv_sfc(:,i_startblk:i_endblk)*rhos(:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL horizontal_sum(p_patch, varout, qv_loc, rl_start, rl_end)
+
+!$OMP PARALLEL WORKSHARE
+      varin(:,:,i_startblk:i_endblk) = ql(:,:,i_startblk:i_endblk)*p_prog%rho(:,:,i_startblk:i_endblk)
+!$OMP END PARALLEL WORKSHARE
+      CALL vert_intp_full2half_cell_3d(p_patch, p_metrics, varin, varout, rl_start, rl_end)
+!$OMP PARALLEL WORKSHARE
+      varout(:,nlevp1,i_startblk:i_endblk) = 0._wp
+!$OMP END PARALLEL WORKSHARE
+      CALL horizontal_sum(p_patch, varout, ql_loc, rl_start, rl_end)
+
+      !rho_loc(nlev)
+      CALL horizontal_sum(p_patch, p_prog%rho(:,:,:), rho_loc, rl_start, rl_end)
+    
+      !Global mean
+       u_gb    = global_sum_vert(u_loc)    * inv_n_gb_cells
+       v_gb    = global_sum_vert(v_loc)    * inv_n_gb_cells 
+       temp_gb = global_sum_vert(temp_loc) * inv_n_gb_cells 
+       qv_gb   = global_sum_vert(qv_loc)   * inv_n_gb_cells 
+       ql_gb   = global_sum_vert(ql_loc)   * inv_n_gb_cells 
+       rho_gb  = global_sum_vert(rho_loc)  * inv_n_gb_cells
+
+      !Vertical advective forcing
+      inv_dz(:) = 1._wp / p_metrics%ddqz_z_full(2,:,2)
+     
+      ddt_u_ls    =  ddt_u_ls - w_ls*vertical_derivative(u_gb,inv_dz)/rho_gb
+      ddt_v_ls    =  ddt_v_ls - w_ls*vertical_derivative(v_gb,inv_dz)/rho_gb
+      ddt_temp_ls =  ddt_temp_ls - w_ls*vertical_derivative(temp_gb,inv_dz)/rho_gb
+      ddt_qv_ls   =  ddt_qv_ls - w_ls*vertical_derivative(qv_gb,inv_dz)/rho_gb
+      ddt_ql_ls   =  ddt_ql_ls - w_ls*vertical_derivative(ql_gb,inv_dz)/rho_gb
+
+    END IF
+
+    !2)Horizontal advective forcing: at present only for tracers
+    !  Advective tendencies for ql is always assumed 0
+    IF(is_advection)THEN
+      IF(is_theta)THEN
+        CALL horizontal_sum(p_patch, p_prog%exner(:,:,:), exner_loc, rl_start, rl_end)
+        exner_gb = global_sum_vert(exner_loc) * inv_n_gb_cells
+        ddt_temp_ls = ddt_temp_ls + ddt_temp_hadv_ls * exner_gb      
+      ELSE
+        ddt_temp_ls = ddt_temp_ls + ddt_temp_hadv_ls 
+      END IF
+      ddt_qv_ls   = ddt_qv_ls   + ddt_qv_hadv_ls
+    END IF
+
+    !3)Coriolis and geostrophic wind
+    IF(is_geowind)THEN
+      CALL horizontal_sum(p_patch, p_diag%u(:,:,:), uf_loc, rl_start, rl_end)
+      CALL horizontal_sum(p_patch, p_diag%v(:,:,:), vf_loc, rl_start, rl_end)
+      uf_gb = global_sum_vert(uf_loc) * inv_n_gb_cells
+      vf_gb = global_sum_vert(vf_loc) * inv_n_gb_cells 
+
+      !Remember model grid is flat   
+      ddt_u_ls = ddt_u_ls + p_patch%cells%f_c(2,2) * (vf_gb - v_geo)
+      ddt_v_ls = ddt_v_ls - p_patch%cells%f_c(2,2) * (uf_gb - u_geo)
+    END IF
+
+    !4)Radiative forcing
+    IF(is_rad_forcing)THEN
+      IF(is_theta .AND. is_advection)THEN
+        ddt_temp_ls = ddt_temp_ls + ddt_temp_rad_ls * exner_gb
+      ELSEIF(is_theta .AND. .NOT.is_advection)THEN
+        CALL horizontal_sum(p_patch, p_prog%exner(:,:,:), exner_loc, rl_start, rl_end)
+        exner_gb = global_sum_vert(exner_loc) * inv_n_gb_cells
+        ddt_temp_ls = ddt_temp_ls + ddt_temp_rad_ls * exner_gb
+      ELSE
+        ddt_temp_ls = ddt_temp_ls + ddt_temp_rad_ls 
+      END IF
+    END IF
+
+
+  END SUBROUTINE apply_ls_forcing
+
+
+  !>
+  !! horizontal_sum
+  !!------------------------------------------------------------------------
+  !! @par Revision History
+  !! Initial release by Anurag Dipankar, MPI-M (2013-May-30)
+  SUBROUTINE horizontal_sum(p_patch, var, var_loc, rl_start, rl_end)
+
+    TYPE(t_patch),     INTENT(in), TARGET :: p_patch
+    REAL(wp), INTENT(in)                  :: var(:,:,:)
+    INTEGER,  INTENT(in)                  :: rl_start, rl_end 
+    REAL(wp), INTENT(out)                 :: var_loc(:)                     
+
+    INTEGER :: i_startblk, i_endblk
+    INTEGER :: i_endidx, i_startidx, i_nchdom
+    INTEGER :: jk, jc, jb
+
+    i_nchdom  = MAX(1,p_patch%n_childdom)
+
+    var_loc = 0._wp
+
+    i_startblk = p_patch%cells%start_blk(rl_start,1)
+    i_endblk   = p_patch%cells%end_blk(rl_end,i_nchdom)
+
+!$OMP PARALLEL
+!$OMP DO PRIVATE(jb, jk, jc, i_startidx, i_endidx) ICON_OMP_DEFAULT_SCHEDULE
+    DO jk = 1 , SIZE(var,2)  
+      DO jb = i_startblk, i_endblk
+        CALL get_indices_c(p_patch, jb, i_startblk, i_endblk, &
+                           i_startidx, i_endidx, rl_start, rl_end)
+
+        DO jc = i_startidx , i_endidx
+          var_loc(jk) = var_loc(jk) + var(jc,jk,jb)
+        END DO
+      END DO
+    END DO 
+!$OMP END DO NOWAIT
+!$OMP END PARALLEL
+
+
+  END SUBROUTINE horizontal_sum
+
+  !>
+  !! vertical_derivative
+  !!------------------------------------------------------------------------
+  !! @par Revision History
+  !! Initial release by Anurag Dipankar, MPI-M (2013-May-30)
+  FUNCTION vertical_derivative (var, inv_dz) RESULT(dvardz)
+
+    REAL(wp), INTENT(in) :: var(:), inv_dz(:)
+                     
+    REAL(wp) :: dvardz(SIZE(inv_dz))                     
+    INTEGER  :: jk
+
+!$OMP PARALLEL
+!$OMP DO PRIVATE(jk) ICON_OMP_DEFAULT_SCHEDULE
+    DO jk = 1 , SIZE(inv_dz)
+      dvardz(jk) = ( var(jk) - var(jk+1) ) * inv_dz(jk)
+    END DO
+!$OMP END DO NOWAIT
+!$OMP END PARALLEL
+
+  END FUNCTION vertical_derivative
+
+!-------------------------------------------------------------------------------
+     
+END MODULE mo_ls_forcing
+
+
+
---
+++ src/namelists/mo_ls_forcing_nml.f90
@@ -0,0 +1,147 @@
+!>
+!! Namelist file for large-scale forcing terms
+!!        
+!! @par Revision History
+!!
+!! @par Copyright
+!! 2002-2006 by DWD and MPI-M
+!! This software is provided for non-commercial use only.
+!! See the LICENSE and the WARRANTY conditions.
+!!
+!! @par License
+!! The use of ICON is hereby granted free of charge for an unlimited time,
+!! provided the following rules are accepted and applied:
+!! <ol>
+!! <li> You may use or modify this code for your own non commercial and non
+!!    violent purposes.
+!! <li> The code may not be re-distributed without the consent of the authors.
+!! <li> The copyright notice and statement of authorship must appear in all
+!!    copies.
+!! <li> You accept the warranty conditions (see WARRANTY).
+!! <li> In case you intend to use the code commercially, we oblige you to sign
+!!    an according license agreement with DWD and MPI-M.
+!! </ol>
+!!
+!! @par Warranty
+!! This code has been tested up to a certain level. Defects and weaknesses,
+!! which may be included in the code, do not establish any warranties by the
+!! authors.
+!! The authors do not make any warranty, express or implied, or assume any
+!! liability or responsibility for the use, acquisition or application of this
+!! software.
+!!
+!!
+MODULE mo_ls_forcing_nml
+
+  USE mo_kind,                ONLY: wp
+  USE mo_mpi,                 ONLY: my_process_is_stdio 
+  USE mo_exception,           ONLY: message, finish, message_text
+  USE mo_io_units,            ONLY: nnml, nnml_output
+  USE mo_namelist,            ONLY: position_nml, positioned, open_nml, close_nml
+  USE mo_master_control,      ONLY: is_restart_run
+  USE mo_io_restart_namelist, ONLY: open_tmpfile, store_and_close_namelist,  &
+                                  & open_and_restore_namelist, close_tmpfile
+  USE mo_impl_constants,      ONLY: MAX_CHAR_LENGTH, max_dom
+  USE mo_run_config,          ONLY: ltestcase
+  USE mo_atm_phy_nwp_config,  ONLY: atm_phy_nwp_config
+
+  IMPLICIT NONE
+  PRIVATE
+  PUBLIC :: read_ls_forcing_namelist, is_ls_forcing, is_subsidence, is_advection, &
+            is_geowind, is_rad_forcing, is_theta
+
+  CHARACTER(len=*), PARAMETER :: version = '$Id$'
+
+  LOGICAL  :: is_ls_forcing  !true if any forcing is on
+  LOGICAL  :: is_subsidence  !true if subsidence is on
+  LOGICAL  :: is_advection   !true if horizontal advective forcing is on for any variable
+  LOGICAL  :: is_geowind     !true if geostophic wind is set 
+  LOGICAL  :: is_rad_forcing !true if radiative forcing is on
+  LOGICAL  :: is_theta       !true is forcings are in terms of theta
+ 
+  NAMELIST/ls_forcing_nml/ is_subsidence, is_advection, is_geowind, is_rad_forcing,  &
+                           is_theta
+
+CONTAINS
+  !-------------------------------------------------------------------------
+  !>
+  !! Read Namelist for LS forcing
+  !!
+  !! This subroutine 
+  !! - reads the Namelist 
+  !! - sets default values
+  !! - potentially overwrites the defaults by values used in a 
+  !!   previous integration (if this is a resumed run)
+  !! - reads the user's (new) specifications
+  !! - stores the Namelist for restart
+  !! - fills the configuration state 
+  !!
+  !! @par Revision History
+  !!  by Anurag Dipankar, MPIM (2013-May-31)
+  !!
+  SUBROUTINE read_ls_forcing_namelist( filename )
+
+    CHARACTER(LEN=*), INTENT(IN) :: filename 
+    INTEGER :: istat, funit, jg
+
+    CHARACTER(len=*), PARAMETER ::  &
+      &  routine = 'mo_ls_forcing_nml: read_ls_forcing_namelist'
+
+    !-----------------------
+    ! 1. default settings
+    !-----------------------
+    is_ls_forcing = .FALSE.
+    is_subsidence = .FALSE.
+    is_advection  = .FALSE.
+    is_geowind    = .FALSE.
+    is_rad_forcing = .FALSE.
+    is_theta       = .FALSE.
+
+    !------------------------------------------------------------------
+    ! 2. If this is a resumed integration, overwrite the defaults above 
+    !    by values used in the previous integration.
+    !------------------------------------------------------------------
+    IF (is_restart_run()) THEN
+      funit = open_and_restore_namelist('ls_forcing_nml')
+      READ(funit,NML=ls_forcing_nml)
+      CALL close_tmpfile(funit)
+    END IF
+
+    !------------------------------------------------------------------------
+    ! 3. Read user's (new) specifications (Done so far by all MPI processes)
+    !------------------------------------------------------------------------
+    CALL open_nml(TRIM(filename))
+    CALL position_nml ('ls_forcing_nml', status=istat)
+    SELECT CASE (istat)
+    CASE (POSITIONED)
+      READ (nnml, ls_forcing_nml)
+    END SELECT
+    CALL close_nml
+
+    !4. checks
+    !If any of the forcing is ON turn on is_ls_forcing
+    IF(is_subsidence .OR. is_advection .OR. is_geowind .OR. is_rad_forcing) &
+        is_ls_forcing = .TRUE.
+
+    IF(is_ls_forcing .AND. .NOT.ltestcase) &
+        CALL message(TRIM(routine),'ltestcase is turned ON because is_ls_forcing is ON!')
+
+    !Check for testcases with large-scale forcing
+    IF(is_rad_forcing .AND. atm_phy_nwp_config(1)%inwp_radiation>0) &
+        CALL finish(TRIM(routine),'both inwp_rad and rad_forcing are turned on!')
+
+    !-----------------------------------------------------
+    ! 5. Store the namelist for restart
+    !-----------------------------------------------------
+    IF(my_process_is_stdio())  THEN
+      funit = open_tmpfile()
+      WRITE(funit,NML=ls_forcing_nml)                    
+      CALL store_and_close_namelist(funit,'ls_forcing_nml') 
+    ENDIF
+
+    ! 6. write the contents of the namelist to an ASCII file
+    IF(my_process_is_stdio()) WRITE(nnml_output,nml=ls_forcing_nml)
+
+  END SUBROUTINE read_ls_forcing_namelist
+
+END MODULE mo_ls_forcing_nml
