96b142295e1c96eaa53675e0889531988eb09767
Luis Kornblueh <luis.kornblueh@mpimet.mpg.de>
2013-06-05 15:06:45 +0000

    Added working ssi, tsi to amip.

--- src/atm_phy_echam/mo_echam_phy_init.f90
+++ src/atm_phy_echam/mo_echam_phy_init.f90
@@ -113,6 +113,7 @@ MODULE mo_echam_phy_init
   ! for AMIP boundary conditions
   USE mo_amip_bc,              ONLY: read_amip_bc, amip_time_weights, amip_time_interpolation
   USE mo_greenhouse_gases,     ONLY: read_ghg_bc, ghg_time_interpolation, ghg_file_read
+  USE mo_solar_irradiance,     ONLY: read_ssi_bc, ssi_time_weights, ssi_time_interpolation
 
   IMPLICIT NONE
 
@@ -165,6 +166,7 @@ CONTAINS
     ! For radiation:
 
     IF (phy_config%lrad) THEN
+      ! TSI, SSI are getting overwritten in case of an AMIP simulation by time varying once (see below)
       ssi(:) = ssi_amip(:)
       tsi    = SUM(ssi(:))
       CALL setup_srtm
@@ -328,6 +330,10 @@ CONTAINS
         IF (.NOT. ghg_file_read) CALL read_ghg_bc(ighg)
         CALL ghg_time_interpolation(current_date)
       ENDIF
+      ! overwrite defined static TSI, SSI by time varying once
+      CALL read_ssi_bc(current_date%year)
+      CALL ssi_time_weights(current_date)
+      CALL ssi_time_interpolation(tsi, ssi)
       CALL read_amip_bc(current_date%year, p_patch(1))
       CALL amip_time_weights(current_date)
       DO jg= 1,ndomain
