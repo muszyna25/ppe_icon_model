82381efb69bb7032a0802492314d4631ceba6e8c
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2013-06-28 08:18:27 +0000

    Add cells for scotland-iceland path

--- scripts/postprocessing/tools/findPath.rb
+++ scripts/postprocessing/tools/findPath.rb
@@ -8,33 +8,43 @@ module IconPathsAlongCells
   # R2B04: list of cellsof which their common edge define the path
   CellPairsLists= {
     :gibraltar      => [[4543,4485]],
+
     :denmarkStrait  => [[4690,4695]],
+
     :drakePassage   => [
       [18481,18495],
       [18483,18493],
       [18642,18649],
       [18651,18643],
       [19186,19191]],
-      :indonesianThroughflow_00 => [
-        [6864,6866],
-        [6868,6869],
-        [6872,6875],
-        [12064,12067],
-        [12072,12073],
-        [12152,12154]],
-      :indonesianThroughflow_01 => [
-        [6855,6857],
-        [6858,6869],
-        [6878,6879],
-        [11265,11269],
-        [11461,11462],
-        [11470,11471],
-        [11509,11510],
-        [11518,11519],
-        [12106,12107],
-        [12109,12110],
-        [12133,12134],
-        [12142,12143]],
+
+    :indonesianThroughflow_00 => [
+      [6864,6866],
+      [6868,6869],
+      [6872,6875],
+      [12064,12067],
+      [12072,12073],
+      [12152,12154]],
+
+    :indonesianThroughflow_01 => [
+      [6855,6857],
+      [6858,6869],
+      [6878,6879],
+      [11265,11269],
+      [11461,11462],
+      [11470,11471],
+      [11509,11510],
+      [11518,11519],
+      [12106,12107],
+      [12109,12110],
+      [12133,12134],
+      [12142,12143]],
+
+    :scotland_iceland => [
+      [4238,4256],
+      [4259,4268],
+      [4270,4271],
+      [4682,4702]],
   }
 
   def IconPathsAlongCells.getEdgesAndVerts(ifile)
@@ -47,12 +57,14 @@ module IconPathsAlongCells
 
     puts '#==========================================================================='
     CellPairsLists.each {|location,cellPairs|
+      next unless :scotland_iceland == location
       puts '============================================================================='
       puts location
       puts ["cells: ", cellPairs.join(' ')].join
 
       verts, edges = [], []
       cellPairs.each {|cellpair|
+          verts = []
         cellpair.each {|cell|
           edges << cellEdges[cell,0..-1].to_a
           verts << cellVertices[cell,0..-1].to_a
