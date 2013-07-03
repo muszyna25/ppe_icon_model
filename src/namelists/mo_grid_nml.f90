dfb1d465b5f969a38424f5b5ef0c10d3a51895a4
Anurag Dipankar <anurag.dipankar@mpimet.mpg.de>
2013-07-03 16:05:45 +0000

    added is_plane_torus in grid_nml

--- src/namelists/mo_grid_nml.f90
+++ src/namelists/mo_grid_nml.f90
@@ -59,6 +59,7 @@ MODULE mo_grid_nml
     & config_start_time                   => start_time,                   &
     & config_end_time                     => end_time,                     &
     & config_lplane                       => lplane,                       &
+    & config_is_plane_torus               => is_plane_torus,               &
     & config_corio_lat                    => corio_lat,                    &
     & config_l_limited_area               => l_limited_area,               &
     & config_patch_weight                 => patch_weight,                 &
@@ -119,6 +120,7 @@ MODULE mo_grid_nml
 !    LOGICAL    :: use_dummy_cell_closure ! if true then create a dummy cell and connect it to cells and edges with no neigbor
 
     LOGICAL    :: lplane                   ! f-plane option
+    LOGICAL    :: is_plane_torus           ! f-plane with doubly periodic boundary==> like a plane torus
     REAL(wp)   :: corio_lat                ! Latitude, where the f-plane is located if lplane=.true.
   
     REAL(wp)   :: patch_weight(max_dom)    ! If patch_weight is set to a value > 0
@@ -136,11 +138,13 @@ MODULE mo_grid_nml
 
 
     NAMELIST /grid_nml/ cell_type, lfeedback, ifeedback_type,      &
-      &  lplane, corio_lat, l_limited_area, grid_rescale_factor,   &
+      &  lplane, is_plane_torus, corio_lat, l_limited_area,        &
+      &  grid_rescale_factor,                                      &
       &  patch_weight, lredgrid_phys, start_time, end_time,        &
       &  dynamics_grid_filename,  dynamics_parent_grid_id,         &
       &  radiation_grid_filename, dynamics_radiation_grid_link,    &
-      &  grid_angular_velocity, use_duplicated_connectivity!,       &
+      &  grid_angular_velocity, use_duplicated_connectivity
+
 !      &  use_dummy_cell_closure
 !       &  radiation_grid_distribution
 
@@ -170,7 +174,8 @@ MODULE mo_grid_nml
     ifeedback_type = 2
     start_time(:) = 0._wp
     end_time(:)   = 1.e30_wp
-    lplane      = .FALSE.
+    lplane         = .FALSE.
+    is_plane_torus = .FALSE.
     l_limited_area = .FALSE.
     corio_lat   = 0.0_wp
     patch_weight= 0.0_wp
@@ -233,6 +238,7 @@ MODULE mo_grid_nml
     config_start_time        = start_time
     config_end_time          = end_time
     config_lplane            = lplane
+    config_is_plane_torus    = is_plane_torus
     config_corio_lat         = corio_lat
     config_l_limited_area    = l_limited_area
     config_patch_weight      = patch_weight
