e416ab2b0af3aaad783218db71e721d77f117d6e
Daniel Reinert <Daniel.Reinert@dwd.de>
2013-09-17 11:17:56 +0000

    bugfix for GRIB2/Netcdf output of HHL/z_ifc

--- src/io/mo_name_list_output_init.f90
+++ src/io/mo_name_list_output_init.f90
@@ -2129,6 +2129,21 @@ CONTAINS
       DEALLOCATE(levels)
 
 
+      ! REFERENCE (special version for HHL)
+      !
+      of%cdiZaxisID(ZA_reference_half_hhl) = zaxisCreate(ZAXIS_REFERENCE, nlevp1)
+      ALLOCATE(lbounds(nlevp1), ubounds(nlevp1), levels(nlevp1))
+      DO k = 1, nlevp1
+        lbounds(k) = REAL(k,dp)
+        levels(k)  = REAL(k,dp)
+      END DO
+      ubounds(1:nlevp1) = 0._dp
+      CALL zaxisDefLbounds(of%cdiZaxisID(ZA_reference_half_hhl), lbounds) !necessary for GRIB2
+      CALL zaxisDefUbounds(of%cdiZaxisID(ZA_reference_half_hhl), ubounds) !necessary for GRIB2
+      CALL zaxisDefLevels(of%cdiZaxisID(ZA_reference_half_hhl), levels)  !necessary for NetCDF
+      DEALLOCATE(lbounds, ubounds, levels)
+
+
       ! HYBRID_LAYER
       !
       of%cdiZaxisID(ZA_hybrid)      = zaxisCreate(ZAXIS_HYBRID, nlev)
@@ -2161,20 +2176,6 @@ CONTAINS
       DEALLOCATE(levels)
       CALL zaxisDefVct(of%cdiZaxisID(ZA_hybrid_half), 2*nlevp1, vct(1:2*nlevp1))
 
-      ! HYBRID (special version for HHL)
-      !
-      of%cdiZaxisID(ZA_hybrid_half_hhl) = zaxisCreate(ZAXIS_HYBRID_HALF, nlevp1)
-      ALLOCATE(lbounds(nlevp1), ubounds(nlevp1), levels(nlevp1))
-      DO k = 1, nlevp1
-        lbounds(k) = REAL(k,dp)
-        levels(k)  = REAL(k,dp)
-      END DO
-      ubounds(1:nlevp1) = 0._dp
-      CALL zaxisDefLbounds(of%cdiZaxisID(ZA_hybrid_half_hhl), lbounds) !necessary for GRIB2
-      CALL zaxisDefUbounds(of%cdiZaxisID(ZA_hybrid_half_hhl), ubounds) !necessary for GRIB2
-      CALL zaxisDefLevels(of%cdiZaxisID(ZA_hybrid_half_hhl), levels)  !necessary for NetCDF
-      DEALLOCATE(lbounds, ubounds, levels)
-      CALL zaxisDefVct(of%cdiZaxisID(ZA_hybrid_half_hhl), 2*nlevp1, vct(1:2*nlevp1))
 
       !
       ! Define axis for output on mean sea level
