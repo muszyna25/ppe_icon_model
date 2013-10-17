c9b541e8616fd02944ef44d15dca2641d39ec182
Stephan Lorenz <stephan.lorenz@mpimet.mpg.de>
2013-10-17 14:30:51 +0000

    [ocean] update volume flux condition (refs #4097)

--- src/ocean/boundary/mo_oce_bulk.f90
+++ src/ocean/boundary/mo_oce_bulk.f90
@@ -495,6 +495,7 @@ CONTAINS
               Qatm%rpreci(:,:) = 0._wp
               Qatm%rprecw(:,:) = p_sfc_flx%forc_precip(:,:)
             ENDWHERE
+
           ENDIF
 
           ! TODO:
@@ -538,13 +539,21 @@ CONTAINS
         Qatm%albnirdifw = albedoW
 
         IF ( l_forc_freshw .AND. (iforc_type == 2 .OR. iforc_type == 5) ) THEN
+
           p_sfc_flx%forc_fw_bc(:,:) = p_sfc_flx%forc_runoff(:,:)                        &
             &           + p_sfc_flx%forc_fw_bc_ice(:,:) + p_sfc_flx%forc_fw_bc_oce(:,:) 
+
+          !---------DEBUG DIAGNOSTICS-------------------------------------------
           idt_src=2  ! output print level (1-5, fix)
-          CALL dbg_print('UpdSfc:OMIP/NCEP:forc_evap',p_sfc_flx%forc_evap  &
-            &   ,str_module,idt_src, in_subset=p_patch%cells%owned)
           CALL dbg_print('UpdSfc:OMIP/NCEP:forc_fw_bc',p_sfc_flx%forc_fw_bc  &
             &   ,str_module,idt_src, in_subset=p_patch%cells%owned)
+          idt_src=3  ! output print level (1-5, fix)
+          CALL dbg_print('UpdSfc:OMIP/NCEP:forc_evap',p_sfc_flx%forc_evap  &
+            &   ,str_module,idt_src, in_subset=p_patch%cells%owned)
+          CALL dbg_print('UpdSfc:OMIP/NCEP:fw_bc_oce',p_sfc_flx%forc_fw_bc_oce  &
+            &   ,str_module,idt_src, in_subset=p_patch%cells%owned)
+          !---------------------------------------------------------------------
+
         ENDIF
 
         ! #slo# 2012-12:
@@ -1136,9 +1145,7 @@ CONTAINS
             ! 
             ! If sea ice is present (and l_relaxsal_ice), salinity relaxation is proportional to open water,
             !   under sea ice, no relaxation is applied, according to the procedure in MPIOM
-            !   TODO: p_ice%conc: class 1 of sea ice is used - must be generalized
-            IF (l_relaxsal_ice .AND. i_sea_ice >=1) z_relax = (1.0_wp-p_ice%conc(jc,1,jb))*z_relax
-            !IF (i_sea_ice >= 1) z_relax = (1.0_wp-p_ice%conc(jc,1,jb))*z_relax
+            IF (l_relaxsal_ice .AND. i_sea_ice >=1) z_relax = (1.0_wp-p_ice%concsum(jc,jb))*z_relax
 
             z_forc_tracer_old              = p_sfc_flx%forc_tracer(jc,jb,2)
             p_sfc_flx%forc_tracer(jc,jb,2) = p_sfc_flx%forc_tracer(jc,jb,2) &
@@ -1189,27 +1196,16 @@ CONTAINS
 
     ENDIF
 
- !  !-------------------------------------------------------------------------
- !  ! Add freshwater forcing due to sea ice (and snow changes)
- !  !  - added as forcing to vertical Diffusion as above
-
- !  IF (i_sea_ice >= 1) THEN
-
- !    p_sfc_flx%forc_tracer(:,:,2) = p_sfc_flx%forc_tracer(:,:,2) &
- !      &                            - p_sfc_flx%forc_fw_ice_impl(:,:)*s_top(:,:)*p_patch_3d%wet_c(:,1,:)
-
- !    !---------DEBUG DIAGNOSTICS-------------------------------------------
- !    idt_src=2  ! output print level (1-5, fix)
- !    CALL dbg_print('UpdSfc: fw_ice_impl[m/s]',p_sfc_flx%forc_fw_ice_impl ,str_module,idt_src, in_subset=p_patch%cells%owned)
- !    z_c2(:,:) = p_sfc_flx%forc_tracer(:,:,2)
- !    CALL dbg_print('UpdSfc:sice:forc_trac[Km/s]',z_c2                    ,str_module,idt_src, in_subset=p_patch%cells%owned)
- !    !---------------------------------------------------------------------
-
- !  ENDIF
-
-    ! Sum of freshwater flux F = P - E + R + F_relax in [m/s] (independent of l_forc_frehsw)
+    ! Sum of freshwater volume flux F = P - E + R + F_relax in [m/s] (independent of l_forc_frehsw)
+    ! TODO - comment more precisely
+    !  - add implicit freshwater flux due to relaxation to volume forcing term
     IF (no_tracer >1) THEN
-      p_sfc_flx%forc_fw_tot(:,:) = (p_sfc_flx%forc_fw_bc(:,:) + p_sfc_flx%forc_fwrelax(:,:))
+      ! TODO - check once more - #slo# 2013-10-17
+      !p_sfc_flx%forc_fw_tot(:,:) = (p_sfc_flx%forc_fw_bc(:,:) + p_sfc_flx%forc_fwrelax(:,:))
+      p_sfc_flx%forc_fw_tot(:,:) = p_sfc_flx%forc_runoff(:,:)     + &
+        &                          p_sfc_flx%forc_fw_ice_vol(:,:) + &
+        &                          p_sfc_flx%forc_fw_bc_oce(:,:)  + &
+        &                          p_sfc_flx%forc_fwrelax(:,:)
 
       !---------DEBUG DIAGNOSTICS-------------------------------------------
       idt_src=1  ! output print level (1-5, fix)
