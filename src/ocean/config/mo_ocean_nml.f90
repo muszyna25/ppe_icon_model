2cbcccf35a5020d3247db48fd57120172cc170a7
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2014-01-24 10:53:46 +0000

    improve readablity refs #4364

--- src/ocean/config/mo_ocean_nml.f90
+++ src/ocean/config/mo_ocean_nml.f90
@@ -401,22 +401,22 @@ MODULE mo_ocean_nml
 
   !----------------------------------------------------------------------------
   ! initial conditions
-  LOGICAL            :: use_file_initialConditions = .false.
-  REAL(wp) :: initial_temperature_top              = 16.0_wp    ! reference temperature used for initialization in testcase 46
-  REAL(wp) :: initial_temperature_bottom           = 16.0_wp    ! reference temperature used for initialization in testcase 46
-  REAL(wp) :: initial_salinity_top                 = 35.0_wp    ! reference salinity used for initialization in testcase 46
-  REAL(wp) :: initial_salinity_bottom              = 35.0_wp    ! reference salinity used for initialization in testcase 46
-  !  INTEGER  :: scatter_levels(10)                = 0          ! levels for possible scattering of the constant tracer fields
-  !  REAL(wp) :: scatter_t                         = 20.0_wp    ! temperature value for scattering
-  !  REAL(wp) :: scatter_s                         = 10.0_wp    ! salinity value for scattering
-  INTEGER  :: topography_type                      = 0          ! >= 200 analytic bathymetry
-  REAL(wp) :: topography_height_reference          = 0.0_wp     ! used if topography_type >= 200
-  INTEGER  :: sea_surface_height_type              = 0          ! >= 200 sea_surface_height
-  INTEGER  :: initial_salinity_type                = 0
-  INTEGER  :: initial_temperature_type             = 0
-  CHARACTER(LEN                                    = max_char_length) :: initial_sst_type                                      = 'sst1'
-  INTEGER  :: initial_velocity_type                = 0
-  REAL(wp) :: initial_velocity_amplitude           = 0.0_wp
+  LOGICAL  :: use_file_initialConditions  = .false.
+  REAL(wp) :: initial_temperature_top     = 16.0_wp    ! reference temperature used for initialization in testcase 46
+  REAL(wp) :: initial_temperature_bottom  = 16.0_wp    ! reference temperature used for initialization in testcase 46
+  REAL(wp) :: initial_salinity_top        = 35.0_wp    ! reference salinity used for initialization in testcase 46
+  REAL(wp) :: initial_salinity_bottom     = 35.0_wp    ! reference salinity used for initialization in testcase 46
+  !  INTEGER  :: scatter_levels(10)       = 0          ! levels for possible scattering of the constant tracer fields
+  !  REAL(wp) :: scatter_t                = 20.0_wp    ! temperature value for scattering
+  !  REAL(wp) :: scatter_s                = 10.0_wp    ! salinity value for scattering
+  INTEGER  :: topography_type             = 0          ! >= 200 analytic bathymetry
+  REAL(wp) :: topography_height_reference = 0.0_wp     ! used if topography_type >= 200
+  INTEGER  :: sea_surface_height_type     = 0          ! >= 200 sea_surface_height
+  INTEGER  :: initial_salinity_type       = 0
+  INTEGER  :: initial_temperature_type    = 0
+  CHARACTER(LEN                           = max_char_length) :: initial_sst_type                                      = 'sst1'
+  INTEGER  :: initial_velocity_type       = 0
+  REAL(wp) :: initial_velocity_amplitude  = 0.0_wp
 
   ! test cases for ocean model; for the index see run scripts
   INTEGER            :: itestcase_oce  = 0
