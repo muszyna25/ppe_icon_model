00dd2d8e26984dd01cd71a61ce80bde5438c2946
Sebastian Rast <sebastian.rast@mpimet.mpg.de>
2014-01-27 15:30:23 +0000

    S. Rast: more tests...

--- run/diff_amip_test.sh
+++ run/diff_amip_test.sh
@@ -21,7 +21,9 @@ cdo -O mergetime $FILES ${EXP2}_${TYPE}.nc
 cdo -O sub ${EXP2}_${TYPE}.nc ../${EXP1}/${EXP1}_${TYPE}.nc diff_${EXP2}-${EXP1}_${TYPE}.nc
 REMAP_FILE=/pool/data/ICON/grids/private/r2b4_amip/remapdis_r2b4_amip_cell_t63grid.nc
 if [ -f ${REMAP_FILE} ]; then
-cdo remap,${REMAP_FILE} diff_${EXP2}-${EXP1}_${TYPE}.nc diff_${EXP}_${TYPE}_t63.nc
+echo diff_${EXP2}-${EXP1}_${TYPE}.nc
+echo diff_${EXP2}-${EXP1}_${TYPE}_t63.nc
+cdo remap,t63grid,${REMAP_FILE} diff_${EXP2}-${EXP1}_${TYPE}.nc diff_${EXP2}-${EXP1}_${TYPE}_t63.nc
 fi
 if [ -f test$$.dat ]; then
   rm -f test$$.dat
@@ -31,7 +33,7 @@ FILE2=${EXP1}_${TYPE}.nc
 cdo diff ${FILE1} ../${EXP1}/${FILE2} > test$$.dat
 STATUS=`echo $?`
 if [ $STATUS == 0 -a ! -s test$$.dat ]; then
-  echo 'The variables in files '${FILE1}' and '${FILE2}' are do not differ'
+  echo 'The variables in files '${FILE1}' and '${FILE2}' do not differ'
 else
   EXIT_STATUS=$((EXIT_STATUS + 1))
 fi
