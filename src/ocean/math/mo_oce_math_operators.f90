f21f97f8bc1c45ae0eb07c4d6042895646fbf54d
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2014-02-12 16:51:38 +0000

    [ocean] Created ocean/testbed and moved test_operators under mo_oce_operators_testbed. Removed unnecessary sync in calculate_thickeness.

--- src/ocean/math/mo_oce_math_operators.f90
+++ src/ocean/math/mo_oce_math_operators.f90
@@ -77,7 +77,6 @@ MODULE mo_oce_math_operators
   PUBLIC :: div_oce_3d
   PUBLIC :: rot_vertex_ocean_3d
   PUBLIC :: grad_fd_norm_oce_2d_3d
-  PUBLIC :: operator_test
   !PUBLIC :: rot_vertex_ocean
   !PUBLIC :: rot_vertex_ocean_rbf
   PUBLIC :: calculate_thickness
@@ -91,132 +90,9 @@ MODULE mo_oce_math_operators
 
 CONTAINS
 
-  !-----------------------------------------------------------------------
-  SUBROUTINE operator_test( patch_3d, p_os, p_op_coeff, vn_e, trac_c)
-    
-    TYPE(t_patch_3d ),TARGET, INTENT(in) :: patch_3d
-    TYPE(t_hydro_ocean_state), TARGET    :: p_os
-    TYPE(t_operator_coeff), INTENT(in)   :: p_op_coeff
-    REAL(wp)                             :: vn_e(nproma, n_zlev, patch_3d%p_patch_2d(1)%nblks_e)
-    REAL(wp)                             :: trac_c(nproma, n_zlev, patch_3d%p_patch_2d(1)%alloc_cell_blocks)    
-    
-    !
-    !
-    !Local variables
-    INTEGER :: start_index, end_index
-    INTEGER :: jc, je, jv, jk, jb,edge_index,edge_block
-    REAL(wp) :: curl_integral(1:n_zlev), div_integral(1:n_zlev), lhs(1:n_zlev),rhs(1:n_zlev)
-    REAL(wp) :: grad(nproma, n_zlev, patch_3d%p_patch_2d(1)%nblks_e)
-    REAL(wp) :: div (nproma, n_zlev, patch_3d%p_patch_2d(1)%alloc_cell_blocks)    
-    REAL(wp) :: curl(nproma, n_zlev, patch_3d%p_patch_2d(1)%nblks_v)
-    REAL(wp) :: curlgrad(nproma, n_zlev, patch_3d%p_patch_2d(1)%nblks_v)    
-    TYPE(t_subset_range), POINTER :: edges_in_domain, cells_in_domain, verts_in_domain
-    TYPE(t_patch), POINTER :: patch_2D
-    TYPE(t_cartesian_coordinates) :: vn_dual(nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_v)    
-    !-------------------------------------------------------------------------------
-    patch_2D        => patch_3d%p_patch_2d(1)
-    edges_in_domain => patch_2D%edges%in_domain
-    cells_in_domain => patch_2D%cells%in_domain
-    verts_in_domain => patch_2D%verts%in_domain
-    !-------------------------------------------------------------------------------
-    grad    (1:nproma,1:n_zlev,1:patch_2D%nblks_e)=0.0_wp
-    div     (1:nproma,1:n_zlev,1:patch_2D%nblks_c)=0.0_wp
-    curl    (1:nproma,1:n_zlev,1:patch_2D%nblks_v)=0.0_wp
-    curlgrad(1:nproma,1:n_zlev,1:patch_2D%nblks_v)=0.0_wp
-    
-    
-    !vn_e=0.0_wp
-    !vn_e(1:5,1,1)=10.0_wp
-    !trac_c=35.0_wp
-    CALL map_edges2vert_3d(patch_2D, vn_e, p_op_coeff%edge2vert_coeff_cc, vn_dual)
-    
-    !calculate gradient of curl and curl
-    CALL grad_fd_norm_oce_3d( trac_c, patch_3D, p_op_coeff%grad_coeff, grad)
-    CALL rot_vertex_ocean_3d( patch_3D, grad, vn_dual, p_op_coeff, curlgrad)
-    CALL rot_vertex_ocean_3d( patch_3D, vn_e, vn_dual, p_op_coeff, curl)
-    
-    !domain integrated curl
-    curl_integral=0.0_wp
-    DO jb = verts_in_domain%start_block, verts_in_domain%end_block
-      CALL get_index_range(verts_in_domain, jb, start_index, end_index)
-      DO jk = 1, n_zlev
-        DO jv = start_index, end_index
-        curl_integral(jk) = curl_integral(jk) + curl(jv,jk,jb)*patch_2D%verts%dual_area(jv,jb)
-
-        END DO
-      END DO
-    END DO
-    write(0,*)'OPERATOR-TEST:curl-of-gradient:',maxval(curlgrad),minval(curlgrad)
-    Do jk=1,n_zlev
-    write(0,*)'OPERATOR-TEST:curl-integral:',jk,curl_integral(jk)
-    END DO
-    !calculate domain integrated div
-    CALL div_oce_3d( vn_e, patch_2D,p_op_coeff%div_coeff, div)
-    div_integral=0.0_wp
-    DO jb = cells_in_domain%start_block, cells_in_domain%end_block
-      CALL get_index_range(cells_in_domain, jb, start_index, end_index)
-      DO jc = start_index, end_index
-        DO jk = 1, patch_3d%p_patch_1d(1)%dolic_c(jc,jb)
-          DO je=1,3
-          edge_index = patch_2D%cells%edge_idx(jc,jb, je)
-          edge_block = patch_2D%cells%edge_blk(jc,jb,je)
-          
-             div_integral(jk)= div_integral(jk)+(vn_e(edge_index,jk, edge_block)* &
-             &patch_2D%edges%primal_edge_length(edge_index, edge_block)/patch_2D%cells%area(jc,jb)       * &
-            & patch_2D%cells%edge_orientation(jc,jb, je))* patch_2D%cells%area(jc,jb)
-IF(jk==1)&
-&write(123,*)'details:',jk,jc,jb,je,edge_index,edge_block,vn_e(edge_index,jk, edge_block)* &
-!             &patch_2D%edges%primal_edge_length(edge_index, edge_block)        * &
-            & patch_2D%cells%edge_orientation(jc,jb, je)             
-          END DO
-          div_integral(jk)= div_integral(jk)
-IF(jk==1)&          
-&write(123,*)'details2',jk,jc,jb,div_integral(jk)         
-!          div_integral(jk) = div_integral(jk) + div(jc,jk,jb)*patch_2D%cells%area(jc,jb)                            
-        END DO
-      END DO
-    END DO
-    write(123,*)'-----------------------'
-    DO jk=1,n_zlev
-    write(0,*)'OPERATOR-TEST: div-integral:',jk,div_integral(jk)
-    END DO
-
-    !product rule for divergence
-    CALL div_oce_3d( vn_e, patch_2D,p_op_coeff%div_coeff, div)
-    CALL grad_fd_norm_oce_3d( trac_c, patch_3D, p_op_coeff%grad_coeff, grad)
-
-    lhs=0.0_wp
-    DO jb = cells_in_domain%start_block, cells_in_domain%end_block
-      CALL get_index_range(cells_in_domain, jb, start_index, end_index)
-      DO jk = 1, n_zlev
-        DO jc = start_index, end_index    
-          lhs(jk)=lhs(jk)+trac_c(jc,jk,jb)*div(jc,jk,jb)*patch_2D%cells%area(jc,jb)        
-        END DO
-      END DO
-    END DO
-    rhs=0.0_wp
-    DO jb = edges_in_domain%start_block, edges_in_domain%end_block
-      CALL get_index_range(edges_in_domain, jb, start_index, end_index)
-      DO jk = 1, n_zlev
-        DO je = start_index, end_index
-        rhs(jk)=rhs(jk)+grad(je,jk,jb)*vn_e(je,jk,jb)*patch_2D%edges%area_edge(je,jb)
-        !rhs=rhs+grad(je,jk,jb)*vn_e(je,jk,jb)&
-        !&*patch_2D%edges%primal_edge_length(je,jb)*patch_2D%edges%dual_edge_length(je,jb)
-        END DO
-      END DO
-    END DO
-    Do jk=1,n_zlev
-      write(0,*)'OPERATOR-TEST: integration-by-parts:',jk,lhs(jk),rhs(jk),lhs(jk)+rhs(jk)          
-      write(0,*)'OPERATOR-TEST: integration-by-parts2:',jk,&
-      &maxval(grad(:,jk,:)),minval(grad(:,jk,:)),maxval(vn_e(:,jk,:)),minval(vn_e(:,jk,:))          
-      
-    END DO  
-  END SUBROUTINE operator_test
-  !-------------------------------------------------------------------------------
 
   !-------------------------------------------------------------------------
   !>
-  !! mpi parallelized by LL
   SUBROUTINE map_edges2vert_3d(patch_2D, vn, edge2vert_coeff_cc, vn_dual)
     
     TYPE(t_patch), TARGET, INTENT(in)       :: patch_2D
@@ -965,9 +841,8 @@ IF(jk==1)&
     cell_thickeness => patch_3D%p_patch_1d(n_dom)%prism_thick_c
     edge_thickeness => patch_3D%p_patch_1d(n_dom)%prism_thick_e
  
-   ! sync before run
-   ! check if we need these syncs
-    CALL sync_patch_array(sync_c, patch_2D, ocean_state%p_prog(nold(1))%h)
+   ! sync already done
+   ! CALL sync_patch_array(sync_c, patch_2D, ocean_state%p_prog(nold(1))%h)
 
     !Step 1: calculate cell-located variables for 2D and 3D case
     !For 3D and for SWE thick_c contains thickness of fluid column
@@ -1086,6 +961,8 @@ IF(jk==1)&
             ENDIF
           END DO
         END DO
+
+        ! CALL sync_patch_array_mult(SYNC_E, patch_2D, 2, ocean_state%p_diag%thick_e, ocean_state%p_diag%h_e)
         CALL sync_patch_array(SYNC_E, patch_2D, ocean_state%p_diag%thick_e)
         CALL sync_patch_array(SYNC_E, patch_2D, ocean_state%p_diag%h_e)
     ENDIF 
