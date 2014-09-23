b96c185e16ca9bbbfa41caebd728e16a1d0e1527
Daniel Reinert <Daniel.Reinert@dwd.de>
2014-09-23 14:50:48 +0000

    increase length of 'zname' which holds meteogram station names

--- src/namelists/mo_mtgrm_nml.f90
+++ src/namelists/mo_mtgrm_nml.f90
@@ -38,7 +38,7 @@ MODULE mo_meteogram_nml
 
   TYPE t_list_of_stations
     REAL(wp)              :: lat, lon
-    CHARACTER (LEN=20)    :: zname
+    CHARACTER (LEN=30)    :: zname
   END TYPE t_list_of_stations
 
   LOGICAL                         :: lmeteogram_enabled(max_dom)  !> Flag. True, if meteogram output is enabled
