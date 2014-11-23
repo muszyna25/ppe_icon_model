c14173241f339a6318d318a57f16a0ea1491f514
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2014-11-23 11:10:01 +0000

    [ocean-dev] added exp.oce_testbed_coriolis

--- src/ocean/testbed/mo_ocean_testbed_operators.f90
+++ src/ocean/testbed/mo_ocean_testbed_operators.f90
@@ -38,17 +38,17 @@ MODULE mo_ocean_testbed_operators
 #ifndef __SX__
   USE mo_timer,              ONLY: timer_start, timer_stop, timer_div, timer_grad
 #endif
-  USE mo_math_utilities,     ONLY: t_cartesian_coordinates, vector_product !, gc2cc
+  USE mo_math_utilities,      ONLY: t_cartesian_coordinates, vector_product !, gc2cc
   USE mo_grid_subset,         ONLY: t_subset_range, get_index_range
   USE mo_sync,                ONLY: SYNC_C, SYNC_E, SYNC_V, sync_patch_array
   USE mo_grid_config,         ONLY: n_dom
-  USE mo_oce_math_operators
+  USE mo_ocean_math_operators
 
   USE mo_model_domain,        ONLY: t_patch, t_patch_3d
   USE mo_ext_data_types,      ONLY: t_external_data
   USE mo_datetime,            ONLY: t_datetime
-  USE mo_oce_types,           ONLY: t_hydro_ocean_state
-  USE mo_oce_physics,         ONLY: t_ho_params
+  USE mo_ocean_types,         ONLY: t_hydro_ocean_state
+  USE mo_ocean_physics,       ONLY: t_ho_params
   USE mo_sea_ice_types,       ONLY: t_sfc_flx, t_atmos_fluxes, t_atmos_for_ocean, t_sea_ice
   USE mo_operator_ocean_coeff_3d, ONLY: t_operator_coeff, no_primal_edges, no_dual_edges
 
@@ -83,7 +83,7 @@ CONTAINS
     TYPE(t_atmos_for_ocean),  INTENT(inout)          :: oceans_atmosphere
     TYPE(t_atmos_fluxes ),    INTENT(inout)          :: oceans_atmosphere_fluxes
     TYPE (t_sea_ice),         INTENT(inout)          :: ocean_ice
-    TYPE(t_operator_coeff),   INTENT(inout)          :: operators_coefficients
+    TYPE(t_operator_coeff),TARGET,   INTENT(inout)   :: operators_coefficients
 
     CHARACTER(LEN=*), PARAMETER ::  method_name = "ocean_test_operators"
 
@@ -106,7 +106,7 @@ CONTAINS
     
     TYPE(t_patch_3d ),TARGET, INTENT(in) :: patch_3d
     TYPE(t_hydro_ocean_state), TARGET    :: ocean_state
-    TYPE(t_operator_coeff), INTENT(in)   :: operators_coefficients
+    TYPE(t_operator_coeff),TARGET, INTENT(in)   :: operators_coefficients
     REAL(wp)                             :: vn_e(nproma, n_zlev, patch_3d%p_patch_2d(1)%nblks_e)
     REAL(wp)                             :: trac_c(nproma, n_zlev, patch_3d%p_patch_2d(1)%alloc_cell_blocks)    
     
@@ -230,28 +230,138 @@ IF(jk==1)&
   SUBROUTINE test_nonlinear_coriolis_3d(patch_3D, ocean_state, operators_coefficients)
     TYPE(t_patch_3d ),TARGET,INTENT(in) :: patch_3d
     TYPE(t_hydro_ocean_state), TARGET, INTENT(inout) :: ocean_state
-    TYPE(t_operator_coeff), INTENT(in)   :: operators_coefficients
+    TYPE(t_operator_coeff),TARGET, INTENT(in)   :: operators_coefficients
 
     REAL(wp) :: vort_flux(nproma,n_zlev,patch_3D%p_patch_2D(1)%nblks_e)
     REAL(wp) :: vort_v   (nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_v)
     REAL(wp), POINTER :: vn(:,:,:)
     TYPE(t_cartesian_coordinates), POINTER  :: vn_dual(:,:,:)
+    TYPE(t_patch), POINTER :: patch_2d
 
+    patch_2d   => patch_3d%p_patch_2d(1)
     vn      => ocean_state%p_prog(nold(1))%vn
     vn_dual => ocean_state%p_diag%p_vn_dual
     
     vn = 1.0_wp
     vort_v  = 0.0_wp
+    patch_2d%verts%f_v = 1.0_wp    
     
     ! vn_dual = 0.0_wp
     ! CALL rot_vertex_ocean_3d( patch_3d, vn, p_vn_dual, operators_coefficients, vort_v)
     
+    write(0,*) "-------------- original coriolis budget -----------------------"
     CALL testbed_nonlinear_coriolis_3d(patch_3d, vn, vort_v, &
        & operators_coefficients, vort_flux)
+    CALL diagnose_coriolis_energy(patch_3d, vn, vort_flux)
 
+    write(0,*) "-------------- fast coriolis budget -----------------------"
+    CALL testbed_nonlinear_coriolis_3d_fast(patch_3d, vn, vort_v, &
+       & operators_coefficients, vort_flux)
     CALL diagnose_coriolis_energy(patch_3d, vn, vort_flux)
 
   END SUBROUTINE test_nonlinear_coriolis_3d
+  !-------------------------------------------------------------------------
+  !>
+  !! Optimized version of the nonlinear_coriolis_3d
+  !! Note: intel -o3 will produce very different results when using this version,
+  !!  the model exhibits great sensitivity using the AtlanticBoxACC setup
+  !<Optimize:inUse>
+  SUBROUTINE testbed_nonlinear_coriolis_3d_fast(patch_3d, vn, vort_v, &
+    & operators_coefficients, vort_flux)
+    TYPE(t_patch_3d ),TARGET,INTENT(in) :: patch_3d
+    REAL(wp), INTENT(inout)                    :: vn(nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_e)
+    ! TYPE(t_cartesian_coordinates), INTENT(inout)  :: p_vn_dual(nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_v)
+    REAL(wp), INTENT(inout)                    :: vort_v   (nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_v)
+    TYPE(t_operator_coeff),INTENT(in)          :: operators_coefficients
+    REAL(wp), INTENT(inout)                    :: vort_flux(nproma,n_zlev,patch_3d%p_patch_2d(1)%nblks_e)
+
+    !Local variables
+    !TYPE(t_patch), POINTER         :: patch_2D
+    INTEGER :: startLevel! , endLevel     ! vertical start and end level
+    INTEGER :: je, level, blockNo
+    INTEGER :: start_edge_index, end_edge_index
+    INTEGER :: ictr, vertex_edge
+    INTEGER :: vertex1_idx, vertex1_blk, vertex2_idx, vertex2_blk
+    REAL(wp) :: this_vort_flux(n_zlev, 2) ! for each of the two vertices
+
+    TYPE(t_subset_range), POINTER :: edges_in_domain
+    TYPE(t_patch), POINTER :: patch_2d
+    !-----------------------------------------------------------------------
+    patch_2d   => patch_3d%p_patch_2d(1)
+    edges_in_domain => patch_2d%edges%in_domain
+    !-----------------------------------------------------------------------
+    startLevel    = 1
+    ! endLevel    = n_zlev
+
+
+!ICON_OMP_PARALLEL_DO PRIVATE(blockNo,level,je,start_edge_index,end_edge_index, this_vort_flux, &
+!ICON_OMP  vertex1_idx, vertex1_blk, vertex2_idx, vertex2_blk,  &
+!ICON_OMP vertex_edge, ictr) ICON_OMP_DEFAULT_SCHEDULE
+    DO blockNo = edges_in_domain%start_block, edges_in_domain%end_block
+      CALL get_index_range(edges_in_domain, blockNo, start_edge_index, end_edge_index)
+
+      ! vort_flux(:,:,blockNo) = 0.0_wp
+
+      edge_idx_loop: DO je =  start_edge_index, end_edge_index
+
+        this_vort_flux(:,:) = 0.0_wp
+
+        vertex1_idx = patch_2d%edges%vertex_idx(je,blockNo,1)
+        vertex1_blk = patch_2d%edges%vertex_blk(je,blockNo,1)
+        vertex2_idx = patch_2d%edges%vertex_idx(je,blockNo,2)
+        vertex2_blk = patch_2d%edges%vertex_blk(je,blockNo,2)
+
+        ! vertex 1
+        ictr = 0
+        DO vertex_edge=1, patch_2d%verts%num_edges(vertex1_idx,vertex1_blk)!no_dual_cell_edges
+
+          ictr =ictr+1
+
+          DO level = startLevel, patch_3d%p_patch_1d(1)%dolic_e(je,blockNo)
+
+            this_vort_flux(level, 1) =  this_vort_flux(level, 1) + &
+              & vn( patch_2d%verts%edge_idx(vertex1_idx,vertex1_blk,vertex_edge), level, &
+              &      patch_2d%verts%edge_blk(vertex1_idx,vertex1_blk,vertex_edge))  * &
+              &  operators_coefficients%edge2edge_viavert_coeff(je,level,blockNo,ictr)
+
+          ENDDO
+
+        END DO ! edges of this vertex
+
+        ! vertex 2
+        ictr = no_dual_edges
+        DO vertex_edge=1, patch_2d%verts%num_edges(vertex2_idx,vertex2_blk)!no_dual_cell_edges
+
+          ictr =ictr+1
+
+          DO level = startLevel, patch_3d%p_patch_1d(1)%dolic_e(je,blockNo)
+
+            this_vort_flux(level, 2) =  this_vort_flux(level, 2) + &
+              & vn( patch_2d%verts%edge_idx(vertex2_idx,vertex2_blk,vertex_edge), level, &
+              &      patch_2d%verts%edge_blk(vertex2_idx,vertex2_blk,vertex_edge))  * &
+              &  operators_coefficients%edge2edge_viavert_coeff(je,level,blockNo,ictr)
+
+          ENDDO
+
+        END DO ! edges of this vertex
+
+        DO level = startLevel, patch_3d%p_patch_1d(1)%dolic_e(je,blockNo)
+
+          vort_flux(je,level,blockNo) =  &
+            & this_vort_flux(level,1) * &
+            & (vort_v(vertex1_idx, level, vertex1_blk) + patch_2d%verts%f_v(vertex1_idx, vertex1_blk))  &
+            & + this_vort_flux(level,2) * &
+            & (vort_v(vertex2_idx, level, vertex2_blk) + patch_2d%verts%f_v(vertex2_idx, vertex2_blk))
+
+        ENDDO
+
+      END DO edge_idx_loop
+
+    END DO ! blockNo = edges_inDomain%start_block, edges_inDomain%end_block
+!ICON_OMP_END_PARALLEL_DO
+
+  END SUBROUTINE testbed_nonlinear_coriolis_3d_fast
+  !-------------------------------------------------------------------------
 
   !-------------------------------------------------------------------------
   !>
@@ -404,9 +514,7 @@ IF(jk==1)&
 
     DO level = startLevel, endLevel
       write(0,*)'global vorticity budget', level, &
-        & vort_flux_budget_perlevel(level)/volume(level),&
-        & vort_flux_budget_perlevel_pos(level)/volume(level),&
-        & vort_flux_budget_perlevel_neg(level)/volume(level)
+        & vort_flux_budget_perlevel(level)/volume(level)
       write(0,*)'global vorticity budget pos:neg', level,&
         & vort_flux_budget_perlevel_pos(level), vort_flux_budget_perlevel_neg(level), &
         & (vort_flux_budget_perlevel_pos(level) + vort_flux_budget_perlevel_neg(level))/volume(level)
