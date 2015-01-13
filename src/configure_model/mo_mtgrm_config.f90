4a7f38a2f66c3709b83f3007821a3738b3075aac
Daniel Reinert <Daniel.Reinert@dwd.de>
2015-01-13 09:24:04 +0000

    add comment

--- src/configure_model/mo_mtgrm_config.f90
+++ src/configure_model/mo_mtgrm_config.f90
@@ -22,6 +22,9 @@ MODULE mo_meteogram_config
 
   INTEGER, PARAMETER :: MAX_NAME_LENGTH      =   32  !<  max. name string length   
   INTEGER, PARAMETER :: MAX_NUM_STATIONS     =   60  !<  max. number of meteogram locations (global)
+                                                     ! NOTE: MAX_NUM_STATIONS=80 did not work with Intel and GCC.
+                                                     ! Reading the meteogram namelist from the restart file 
+                                                     ! resulted in END OF FILE error in mo_mtgrm_nml.
   INTEGER, PARAMETER :: FTYPE_NETCDF         =    1
 
 
