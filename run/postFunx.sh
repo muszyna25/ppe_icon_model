4d89c1cdf62d294bc8970af4d4cc6180830fb230
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2015-02-09 13:11:17 +0000

    [ocean-merge] add returnCode-aware version of cdo-diff

--- run/postFunx.sh
+++ run/postFunx.sh
@@ -1,11 +1,10 @@
 # ================================================================================
-function accumulationDiff {
-  ifile=$1
-  varName=$2
-  maskName=$3
+function diffWithExitCode {
+  fileA=$1
+  fileB=$2
   ofile=`mktemp`
 
-  cdo -s diffv -div -selname,$varName $ifile -selname,$maskName $ifile -div -selname,${varName}_acc $ifile -selname,$maskName $ifile > $ofile
+  cdo -s diffv ${fileA} ${fileB} > $ofile
 
   nDiff=$(wc -l < $ofile)
 
@@ -16,6 +15,13 @@ function accumulationDiff {
   rm -f $ofile
   return $nDiff
 }
+function accumulationDiff {
+  ifile=$1
+  varName=$2
+  maskName=$3
+
+  diffWithExitCode "-div -selname,$varName $ifile -selname,$maskName $ifile " "-div -selname,${varName}_acc $ifile -selname,$maskName $ifile"
+}
 
 function directoryDiff {
   refDir=$1
@@ -25,7 +31,7 @@ function directoryDiff {
     refFileBasename=$(basename ${refFile})
     case "${refFileBasename}" in
       *.nc*)
-        DIFF='cdo diffv'
+        DIFF='diffWithExitCode'
         ;;
       *)
         DIFF='diff'
