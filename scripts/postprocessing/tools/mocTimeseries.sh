43faee0ea03fc0fece894ac935bde7311ac63385
Stephan Lorenz <stephan.lorenz@mpimet.mpg.de>
2015-03-12 13:14:13 +0000

    [ocean-surfaceReview]: update MOC timeseries to late 2014

--- scripts/postprocessing/tools/mocTimeseries.sh
+++ scripts/postprocessing/tools/mocTimeseries.sh
@@ -11,9 +11,6 @@ if [[ ! -z "$1" ]];then
 fi
 
 mocVar=var778 #AMOC
-if [[ ! -z "$2" ]]; then
-  mocVar=$2
-fi
 
 # calculate the moc timeseries in 1000 depth
 # cat, yearmean, netcdf of MOC to moc.ym.nc
@@ -27,20 +24,26 @@ fi
 if [[ "$mocPattern" != "moc.gnuplot.dat" ]]; then
 
   #  select AMOC at 1000m depth and write ascii data for gnuplot
-  if [[ "20" = $(cdo nlevel -selvar,$mocVar $(ls -1 $mocPattern | head -n 1)) ]]; then
+  levs=$(cdo nlevel -selvar,$mocVar $(ls -1 $mocPattern | head -n 1))
+  if [[ "$levs" = "20" ]]; then
     cdo outputkey,date,value -mulc,1.e-9 -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
          -sellevel,1000 -setgrid,r1x180 $mocPattern > moc.gnuplot.dat
-  else
-    cdo outputkey,date,value -mulc,1.e-9 -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
-         -intlevel,1000 -sellevel,900/1100 -setgrid,r1x180 $mocPattern > moc.gnuplot.dat
   fi
+  if [[ "$levs" = "40" ]]; then
+    cdo outputkey,date,value -mulc,1.e-9 -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
+         -sellevel,1020 -setgrid,r1x180 $mocPattern > moc.gnuplot.dat
+  fi
+# else
+#   cdo outputkey,date,value -mulc,1.e-9 -fldmean -selname,$mocVar -sellonlatbox,0,1,40,60 \
+#        -intlevel,1000 -sellevel,900/1100 -setgrid,r1x180 $mocPattern > moc.gnuplot.dat
+# fi
 fi
 
 gnuplot -p <<EOF
 #set terminal postscript color
 #set output 'moc-tims.ps'
-#set terminal png size 1000,600
-#set output 'moc-tims.png'
+set terminal png size 1200,600
+set output 'moc-tims.png'
 
 set size 0.98,0.95
 set origin 0.02,0.02
@@ -48,8 +51,8 @@ set origin 0.02,0.02
 set style data lines
 set grid
 set title font "Times-Bold,16"
-set title  "Atlantic MOC"
-#set title  "Atlantic MOC (1000m depth, 40-60N)"
+#set title  "Atlantic MOC"
+set title  "Atlantic MOC (1000m depth, 40-60N)"
 #set title  "Atlantic MOC ($mocPattern)"
 #set title  "N-Atlantic Meridional Overturning Circulation (MOC)"
 #set title  "N-Atlantic Meridional Overturning Circulation (MOC)" font "Helvetica-Bold,17"
@@ -61,6 +64,7 @@ set format x "%Y"
 #set mxtics 5
 set xlabel "simulation years" font "Times-Bold,14
 
+#set xrange ["2005":"2010"]
 #set yrange [8.1:27.8]
 #set ytics 4
 #set mytics 4   # minor tickmarks
@@ -69,7 +73,8 @@ set ylabel "[SV]" font "Times-Bold,14
 #set key left
 #set key left font "Helvetica-Bold,10"
 set key left font "Helvetica,11"
-plot 'moc.gnuplot.dat' using 1:2 w l t "${mocPattern}, 1000m/40N-60N" lw 3 lt -1
+plot 'moc.gnuplot.dat' using 1:2 w l t "${mocPattern}" lw 3 lt -1
+#plot 'moc.gnuplot.dat' using 1:2 w l t "${mocPattern}, 1000m/40N-60N" lw 3 lt -1
 #plot 'moc.gnuplot.dat' using 1:2 w l t
 #plot 'moc.gnuplot.dat' using 1:2 w l t 'AMOC, 1000m, 40N-60N' lw 3 lt -1
 EOF
