0f2f18f69cf42c6fe73bb2b4a196ce30476b0d14
Daniel Rieger <daniel.rieger@kit.edu>
2015-03-17 08:34:06 +0000

    [ICON-KIT-OUTPUTGROUPS Branch] Added outputgroups for volcanic ash and radioactive tracers

--- src/shared/mo_var_metadata_types.f90
+++ src/shared/mo_var_metadata_types.f90
@@ -40,7 +40,7 @@ MODULE mo_var_metadata_types
   !
   ! New groups can be added by extending the VAR_GROUPS list.
   !
-  CHARACTER(len=VARNAME_LEN), PARAMETER :: var_groups(39) = &
+  CHARACTER(len=VARNAME_LEN), PARAMETER :: var_groups(41) = &
     (/ "ALL                   ",  &
     &  "ATMO_ML_VARS          ",  &
     &  "ATMO_PL_VARS          ",  &
@@ -78,6 +78,8 @@ MODULE mo_var_metadata_types
     &  "ICE_BUDGETS           ",  &
     &  "ICE_DIAG              ",  &
     &  "LATBC_PREFETCH_VARS   ",  &
+    &  "ART_AERO_VOLC         ",  &
+    &  "ART_AERO_RADIO        ",  &
     &  "ART_AERO_DUST         ",  &  ! ICON-ART fields for mineral dust aerosol
     &  "ART_AERO_SEAS         " /)   ! ICON-ART fields for sea salt aerosol
 
