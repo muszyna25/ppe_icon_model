7c0b70c3917d1860e96aa647fa589450e7fbb01b
G��nther Z��ngl <Guenther.Zaengl@dwd.de>
2015-06-01 16:10:55 +0000

    Modifications of albedo specification for testing in assimilation cycle

--- src/atm_phy_schemes/mo_albedo.f90
+++ src/atm_phy_schemes/mo_albedo.f90
@@ -565,8 +565,10 @@ CONTAINS
     REAL(wp):: zlimsnow_alb            !< upper limit snow albedo depending on snow depth and roughness length
     REAL(wp):: zsnowalb_lu             !< maximum snow albedo specified in landuse table
     REAL(wp):: t_fac                   !< factor for temperature dependency of zminsnow_alb over glaciers
-    REAL(wp):: zsalb_snow              !< snow albedo (predictor)
-    REAL(wp):: zsnow_alb               !< snow albedo (corrector)
+    REAL(wp):: zsnow_alb               !< snow albedo
+
+    ! Auxiliaries for tile-specific calculation of direct beam albedo
+    REAL(wp):: zalbvisdir_t, zalbnirdir_t, zalbdirfac, zalbdirlim 
 
     INTEGER :: jg                      !< patch ID
     INTEGER :: jb, jc, ic, jt          !< loop indices
@@ -594,9 +596,10 @@ CONTAINS
 
 
 !$OMP PARALLEL
-!$OMP DO PRIVATE(jb,jt,jc,ic,i_startidx,i_endidx,ist,snow_frac,t_fac,             &
-!$OMP            zsalb_snow,zsnow_alb,ilu,i_count_lnd,i_count_sea,i_count_flk,    &
-!$OMP            i_count_seaice,zminsnow_alb,zmaxsnow_alb,zlimsnow_alb,zsnowalb_lu) ICON_OMP_DEFAULT_SCHEDULE
+!$OMP DO PRIVATE(jb,jt,jc,ic,i_startidx,i_endidx,ist,snow_frac,t_fac,               &
+!$OMP            zsnow_alb,ilu,i_count_lnd,i_count_sea,i_count_flk,                 &
+!$OMP            i_count_seaice,zminsnow_alb,zmaxsnow_alb,zlimsnow_alb,zsnowalb_lu, &
+!$OMP            zalbvisdir_t,zalbnirdir_t,zalbdirfac,zalbdirlim) ICON_OMP_DEFAULT_SCHEDULE
     DO jb = i_startblk, i_endblk
 
 
@@ -636,7 +639,7 @@ CONTAINS
             ! maximum snow albedo specified in landuse table
             zsnowalb_lu = ABS(ext_data%atm%snowalb_lcc(ilu))
 
-            IF (ext_data%atm%alb_dif(jc,jb) > csalb_snow_min) THEN
+            IF (ext_data%atm%alb_dif(jc,jb) > csalb_snow_min .AND. ilu == ext_data%atm%i_lc_snow_ice) THEN
               ! temperature-dependent minimum snow albedo over glaciers (i.e. alb_dif = 0.7)
               ! this is needed to prevent unrealistically low albedos over Antarctica
               t_fac = MIN(1._wp,0.1_wp*(tmelt-lnd_prog%t_snow_t(jc,jb,jt)))
@@ -649,25 +652,21 @@ CONTAINS
             ! maximum snow albedo depending on landuse class if tile approach is used
             ! (without tiles, this would conflict with the snow albedo limit depending on the forest fraction)
             IF (ntiles_lnd > 1) THEN
-              zmaxsnow_alb = MIN(csalb_snow_max,SQRT(zsnowalb_lu))
+              zmaxsnow_alb = MIN(csalb_snow_max,zsnowalb_lu)
             ELSE
               zmaxsnow_alb = csalb_snow_max
             ENDIF
-            !
+
             ! Consider effects of aging on solar snow albedo
             !
-            zsalb_snow = zminsnow_alb + lnd_diag%freshsnow_t(jc,jb,jt)*(zmaxsnow_alb-zminsnow_alb)
+            zsnow_alb = zminsnow_alb + lnd_diag%freshsnow_t(jc,jb,jt)*(zmaxsnow_alb-zminsnow_alb)
 
-            IF (ntiles_lnd > 1) THEN
-              ! Apply land-cover class specific snow albedo limits
-              zsnow_alb = MIN(zsalb_snow, zsnowalb_lu)
-            ELSE
+            IF (ntiles_lnd == 1) THEN
               ! special treatment for forests
-              ! - aging of snow not considered
+              ! - no landuse-class specific limitation of snow albedo
               ! - instead, snow albedo is limited as a function of the forest fraction
-              zsnow_alb = zsalb_snow*(1._wp-ext_data%atm%for_e(jc,jb)-ext_data%atm%for_d(jc,jb)) &
-                + csalb_snow_fe * ext_data%atm%for_e(jc,jb)                       &
-                + csalb_snow_fd * ext_data%atm%for_d(jc,jb)
+              zsnow_alb = zsnow_alb*(1._wp-ext_data%atm%for_e(jc,jb)-ext_data%atm%for_d(jc,jb))       &
+                + csalb_snow_fe * ext_data%atm%for_e(jc,jb) + csalb_snow_fd * ext_data%atm%for_d(jc,jb)
             ENDIF
 
             ! snow cover fraction
@@ -688,6 +687,11 @@ CONTAINS
             ! Limit snow albedo in case of thin snow cover depending on landuse roughness length and
             ! SSO standard deviation; it is assumed that vegetation is fully covered by snow if the snow depth
             ! exceeds three times the roughness length
+            ! Note that for ntiles_lnd > 1 (tile approach), this limitation has comparatively little impact
+            ! because the landuse-class specific limits applied above are more restrictive in most situations
+            !
+            IF (ntiles_lnd > 1) zmaxsnow_alb = MIN(csalb_snow_max,SQRT(zsnowalb_lu))
+            !
             zlimsnow_alb = zmaxsnow_alb*MIN(1._wp,SQRT(0.25_wp+0.25_wp*lnd_diag%h_snow_t(jc,jb,jt)/&
               MAX(ext_data%atm%z0_lcc(ilu),1.e-3_wp*ext_data%atm%sso_stdh_raw(jc,jb)) ))
 
@@ -880,9 +884,45 @@ CONTAINS
           prm_diag%albdif   (i_startidx:i_endidx,jb) = 0._wp
           prm_diag%albvisdif(i_startidx:i_endidx,jb) = 0._wp
           prm_diag%albnirdif(i_startidx:i_endidx,jb) = 0._wp
+          prm_diag%albvisdir(i_startidx:i_endidx,jb) = 0._wp
+          prm_diag%albnirdir(i_startidx:i_endidx,jb) = 0._wp
 
           DO jt = 1, ntiles_total+ntiles_water
             DO jc = i_startidx, i_endidx
+
+              ! Tile-specific direct beam albedo
+              IF (ext_data%atm%frac_t(jc,jb,jt) > 0._wp) THEN
+                zalbvisdir_t = MIN(0.999_wp,( 1.0_wp                                                           &
+                  &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif_t(jc,jb,jt) - 1.0_wp)))   &
+                  & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif_t(jc,jb,jt) - 1.0_wp)))**2)
+
+                zalbnirdir_t = MIN(0.999_wp,( 1.0_wp                                                           &
+                  &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif_t(jc,jb,jt) - 1.0_wp)))   &
+                  & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif_t(jc,jb,jt) - 1.0_wp)))**2)
+
+                ! Limit direct beam albedo to diffuse albedo for landuse classes with "rough" vegetation
+                ! and in mountainous regions
+                ilu = ext_data%atm%lc_class_t(jc,jb,jt)
+                IF (jt <= ntiles_total) THEN
+
+                  ! Full limitation is applied for a roughness length >= 15 cm or an SSO standard deviation >= 150 m
+                  zalbdirfac = MAX(0.01_wp*(ext_data%atm%sso_stdh_raw(jc,jb) - 50._wp), &
+                                   10._wp *(ext_data%atm%z0_lcc(ilu) - 0.05_wp)         )
+                  zalbdirfac = MIN(1._wp, MAX(0._wp, zalbdirfac))
+
+                  zalbdirlim = zalbdirfac*prm_diag%albvisdif_t(jc,jb,jt) + (1._wp-zalbdirfac)*zalbvisdir_t
+                  zalbvisdir_t = MIN(zalbvisdir_t,zalbdirlim)
+                  zalbdirlim = zalbdirfac*prm_diag%albnirdif_t(jc,jb,jt) + (1._wp-zalbdirfac)*zalbnirdir_t
+                  zalbnirdir_t = MIN(zalbnirdir_t,zalbdirlim)
+
+                ENDIF
+
+              ELSE
+                zalbvisdir_t = 0._wp
+                zalbnirdir_t = 0._wp
+              ENDIF
+
+
               prm_diag%albdif(jc,jb) = prm_diag%albdif(jc,jb)         &
                 &                    + prm_diag%albdif_t(jc,jb,jt)    &
                 &                    * ext_data%atm%frac_t(jc,jb,jt)
@@ -895,6 +935,13 @@ CONTAINS
               prm_diag%albnirdif(jc,jb) = prm_diag%albnirdif(jc,jb)   &
                 &                    + prm_diag%albnirdif_t(jc,jb,jt) &
                 &                    * ext_data%atm%frac_t(jc,jb,jt)
+
+              prm_diag%albvisdir(jc,jb) = prm_diag%albvisdir(jc,jb)                  &
+                &                       + zalbvisdir_t * ext_data%atm%frac_t(jc,jb,jt)
+
+              prm_diag%albnirdir(jc,jb) = prm_diag%albnirdir(jc,jb)                  &
+                &                       + zalbnirdir_t * ext_data%atm%frac_t(jc,jb,jt)
+
             ENDDO
           ENDDO
 
@@ -926,55 +973,23 @@ CONTAINS
       !
       ! compute black sky albedo from white sky albedo and solar zenith angle formula 
       ! as in Ritter-Geleyn's fesft.
-      DO jc = i_startidx, i_endidx
-        prm_diag%albvisdir(jc,jb) = MIN(0.999_wp,( 1.0_wp                                         &
-          &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif(jc,jb) - 1.0_wp)))   &
-          & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif(jc,jb) - 1.0_wp)))**2)
+      IF (ntiles_total == 1 .OR. atm_phy_nwp_config(jg)%inwp_surface == 0) THEN
+        DO jc = i_startidx, i_endidx
+          prm_diag%albvisdir(jc,jb) = MIN(0.999_wp,( 1.0_wp                                         &
+            &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif(jc,jb) - 1.0_wp)))   &
+            & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albvisdif(jc,jb) - 1.0_wp)))**2)
 
-        prm_diag%albnirdir(jc,jb) = MIN(0.999_wp,( 1.0_wp                                         &
-          &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif(jc,jb) - 1.0_wp)))   &
-          & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif(jc,jb) - 1.0_wp)))**2)
-      ENDDO
+          prm_diag%albnirdir(jc,jb) = MIN(0.999_wp,( 1.0_wp                                         &
+            &  + 0.5_wp * (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif(jc,jb) - 1.0_wp)))   &
+            & / (1.0_wp + (prm_diag%cosmu0(jc,jb) * (1.0_wp/prm_diag%albnirdif(jc,jb) - 1.0_wp)))**2)
+        ENDDO
+      ENDIF
 
 
     ENDDO  ! jb
 !$OMP END DO NOWAIT
 !$OMP END PARALLEL
 
-
-!      IF (atm_phy_nwp_config(jg)%lseaice) THEN
-!        DO jc = i_startidx,i_endidx
-!          ! In case the sea ice model is used AND water point AND ice is present,
-!          ! compute ice albedo for water points with an empirical formula taken from GME.
-!          ! The ice albedo is the lower the warmer, and therefore wetter, the ice is.
-!          ! Use ice temperature at time level nnow (2-time level scheme in sea ice model).
-!!          IF ((.NOT. llandmask(i,j)) .AND. (h_ice(i,j,nnow) > 0.0_ireals))               &
-!!            zalso(i,j) = (1.0_wp-0.3846_wp*EXP(-0.35_wp*(tmelt-t_ice(i,j,nnow)))) &
-!!            * csalb(10)
-!          IF (( .NOT. ext_data%atm%llsm_atm_c(jc,jb)) .AND. &
-!            & (prm_diag%h_ice(i,j,nnow) > 0.0_wp))          &
-!            prm_diag%albdif(jc,jb) = (1.0_wp-0.3846_wp*EXP(-0.35_wp*(tmelt-t_ice(i,j,nnow)))) &
-!            * csalb(10)
-!        ENDDO
-!      ENDIF
-
-      !lake model not yet implemented
-!      IF (atm_phy_nwp_config(jg)%llake) THEN
-!        DO jc = i_startidx,i_endidx
-!          IF((ext_data%atm%depth_lk(jc,jb)      >  0.0_wp) .AND.    &
-!            (prm_diag%h_ice(jc,jb) >= h_Ice_min_flk) ) THEN
-!            !  In case the lake model FLake is used AND lake point AND ice is present,
-!            !  compute ice albedo for lake points with an empirical formulation 
-!            !  proposed by Mironov and Ritter (2004) for use in GME 
-!            !  [ice_albedo=function(ice_surface_temperature)].
-!            !  Use surface temperature at time level "nnow".
-!
-!            prm_diag%albdif(jc,jb) = EXP(-c_albice_MR*(tpl_T_f-t_s(i,j,nnow))/tpl_T_f)
-!            prm_diag%albdif(jc,jb) = albedo_whiteice_ref * (1._ireals-zalso(i,j)) +      &
-!              albedo_blueice_ref  * prm_diag%albdif(jc,jb)
-!          ENDIF
-!        ENDDO
-!      ENDIF
 
   END SUBROUTINE sfc_albedo_modis
 
