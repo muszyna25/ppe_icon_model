137ff0a845dcdaebb90c8f3c6775d54a424c757e
Sebastian Rast <sebastian.rast@mpimet.mpg.de>
2015-06-05 14:16:32 +0000

    [icon-aes-radiation-mag]: added further routines for lrtm driver. Next step: restore the call of srtm in psrad_interface.

--- src/atm_phy_psrad/mo_psrad_cloud_optics.f90
+++ src/atm_phy_psrad/mo_psrad_cloud_optics.f90
@@ -40,8 +40,7 @@ MODULE mo_psrad_cloud_optics
 
   IMPLICIT NONE
   PRIVATE
-  PUBLIC :: setup_cloud_optics
-!!$, cloud_optics
+  PUBLIC :: setup_cloud_optics, cloud_optics
 
   INTEGER, PARAMETER :: &
        n_mdl_bnds = 30, &!< n of bands in effective radius table
@@ -208,155 +207,155 @@ CONTAINS
   !!   Currently this model assumes four bands in the SW and maps these to the
   !!   six-band model of ECHAM5.
   !
-!!$  SUBROUTINE cloud_optics(                                               &
-!!$          & laglac       ,laland          ,kproma         ,kbdim         ,&
-!!$          & klev         ,ktype           ,nb_lw          ,nb_sw         ,&
-!!$          & icldlyr      ,zlwp            ,ziwp           ,zlwc          ,&
-!!$          & ziwc         ,zcdnc           ,tau_lw         ,tau_sw        ,&
-!!$          & omg          ,asy             ,re_droplets2d  ,re_crystals2d  )
-!!$
-!!$    INTEGER, INTENT (IN)    :: &
-!!$         kproma,               & !< actual length of grid point block
-!!$         kbdim,                & !< maximum length of grid point block
-!!$         klev,                 &
-!!$         ktype(kbdim),         & !< type of convection
-!!$         nb_sw,                & 
-!!$         nb_lw,                &
-!!$         icldlyr(kbdim,klev)
-!!$    LOGICAL, INTENT (IN)    :: & 
-!!$         laglac(kbdim),        & !< logical for glacier points
-!!$         laland(kbdim)           !< logical for land points
-!!$    REAL (wp), INTENT (IN)  :: &
-!!$         zlwp(kbdim,klev),     & !< liquid water path
-!!$         ziwp(kbdim,klev),     & !< ice water path
-!!$         zcdnc(kbdim,klev),    & !< cloud drop number concentration
-!!$         zlwc(kbdim,klev),     & !< liquid water content
-!!$         ziwc(kbdim,klev)        !< ice water content
-!!$
-!!$    REAL (wp), INTENT (OUT) ::       &
-!!$         tau_lw(kbdim,klev,nb_lw),   & !< LW optical depth
-!!$         tau_sw(kbdim,klev,nb_sw),   & !< SW optical depth
-!!$         omg   (kbdim,klev,nb_sw),   & !< cloud single scattering albedo
-!!$         asy   (kbdim,klev,nb_sw),   & !< cloud asymmetry factor
-!!$         re_droplets2d(kbdim,klev),  & !< effective radius of liquid distribution
-!!$         re_crystals2d(kbdim,klev)     !< effective radius of ice distribution
-!!$
-!!$
-!!$    INTEGER  :: iband, ii, jk, jl, ml1, ml2, mi1, mi2
-!!$    REAL(wp) :: ztol, ztoi, zol, zoi, zgl, zgi, wl1, wl2, wi1, wi2, zfact
-!!$    REAL(wp) :: zmsald, zmsaid
-!!$
-!!$    REAL(wp) :: &
-!!$         zkap(kbdim),        & !< breath parameter for scaling effective radius
-!!$         zlwpt(kbdim),       & !< liquid water path
-!!$         zinhoml(kbdim),     & !< cloud inhomogeneity factor (liquid)
-!!$         re_droplets,        & !< effective radius of liquid distribution
-!!$         re_crystals,        & !< effective radius of ice distribution
-!!$         zscratch,           & !< effective radius of ice distribution
-!!$         ztau(kbdim,klev,n_mdl_bnds), & !< SW optical depth
-!!$         zomg(kbdim,klev,n_mdl_bnds), & !< cloud single scattering albedo
-!!$         zasy(kbdim,klev,n_mdl_bnds)    !< cloud asymmetry factor
-!!$
-!!$    IF (relmin < 1.5 .OR. relmin > 2.5 ) THEN
-!!$      CALL finish('Apparently unsuccessful loading of optical tables')
-!!$    END IF
-!!$    !
-!!$    ! 1.0 Basic cloud properties
-!!$    ! --------------------------------
-!!$    IF (l_variable_inhoml) THEN
-!!$      zlwpt(1:kproma) = 0.0_wp
-!!$      DO jk = 1, klev
-!!$        DO jl = 1, kproma
-!!$          zlwpt(jl) = zlwpt(jl)+zlwp(jl,jk)
-!!$        END DO
-!!$      END DO
-!!$      WHERE (zlwpt(1:kproma) > 1.0_wp) 
-!!$        zinhoml(1:kproma) = zlwpt(1:kproma)**(-zinpar)
-!!$      ELSEWHERE
-!!$        zinhoml(1:kproma) = 1.0_wp
-!!$      END WHERE
-!!$    ELSE
-!!$      DO jl = 1, kproma
-!!$        IF(ktype(jl) .EQ. 0) THEN          !no convection; ktype=0
-!!$          zinhoml(jl) = zinhoml1
-!!$        ELSE IF(ktype(jl) .NE. 4) THEN     !convection; ktype=1,2,3
-!!$          zinhoml(jl) = zinhoml3
-!!$        ELSE                               !shallow convection and clwprat>0.; ktype=4
-!!$          zinhoml(jl) = zinhoml2
-!!$        END IF
-!!$      END DO
-!!$    END IF
-!!$
-!!$    WHERE (laland(1:kproma).AND.(.NOT.laglac(1:kproma))) 
-!!$      zkap(1:kproma)=zkap_cont ! continental breadth factor
-!!$    ELSEWHERE
-!!$      zkap(1:kproma)=zkap_mrtm ! maritime breadth factor 
-!!$    END WHERE
-!!$    !
-!!$    ! 2.0 Cloud Optical Properties by interpolating tables in effective radius
-!!$    ! --------------------------------
-!!$    zfact = 1.0e6_wp*(3.0e-9_wp/(4.0_wp*pi*rhoh2o))**(1.0_wp/3.0_wp)
-!!$    DO jk=1,klev
-!!$      DO jl=1,kproma
-!!$        IF (icldlyr(jl,jk)==1 .AND. (zlwp(jl,jk)+ziwp(jl,jk))>ccwmin) THEN
-!!$          
-!!$          re_crystals = MAX(reimin,MIN(reimax,83.8_wp*ziwc(jl,jk)**0.216_wp))
-!!$          re_droplets = MAX(relmin,MIN(relmax,zfact*zkap(jl)*(zlwc(jl,jk)    &
-!!$               &                             /zcdnc(jl,jk))**(1.0_wp/3.0_wp)))
-!!$          
-!!$          re_crystals2d(jl,jk) = re_crystals
-!!$          re_droplets2d(jl,jk) = re_droplets
-!!$
-!!$          ml1 = MAX(1,MIN(n_sizes-1,FLOOR(1.0_wp+(re_droplets-relmin)/del_rel)))
-!!$          ml2 = ml1 + 1
-!!$          wl1 = 1.0_wp - (re_droplets - (relmin + del_rel* (ml1-1)) )/del_rel
-!!$          wl2 = 1.0_wp - wl1
-!!$
-!!$          mi1 = MAX(1,MIN(n_sizes-1,FLOOR(1.0_wp+(re_crystals-reimin)/del_rei)))
-!!$          mi2 = mi1 + 1
-!!$          wi1 = 1.0_wp - (re_crystals - (reimin + del_rei * (mi1-1)) )/del_rei
-!!$          wi2 = 1.0_wp - wi1
-!!$
-!!$          DO iband = 1,n_mdl_bnds
-!!$            ztol = zlwp(jl,jk)*(wl1*z_ext_l(ml1,iband) + wl2*z_ext_l(ml2,iband))
-!!$            ztoi = ziwp(jl,jk)*(wi1*z_ext_i(mi1,iband) + wi2*z_ext_i(mi2,iband))
-!!$            zol  = 1.0_wp - (wl1*z_coa_l(ml1,iband) + wl2*z_coa_l(ml2,iband))
-!!$            zoi  = 1.0_wp - (wi1*z_coa_i(mi1,iband) + wi2*z_coa_i(mi2,iband))
-!!$            zgl  = wl1*z_asy_l(ml1,iband) + wl2*z_asy_l(ml2,iband)
-!!$            zgi  = wi1*z_asy_i(mi1,iband) + wi2*z_asy_i(mi2,iband)
-!!$
-!!$            zscratch = (ztol*zol+ztoi*zoi)
-!!$            ztau(jl,jk,iband) = ztol*zinhoml(jl) + ztoi*zinhomi
-!!$            zomg(jl,jk,iband) = zscratch/(ztol+ztoi)
-!!$            zasy(jl,jk,iband) = (ztol*zol*zgl+ztoi*zoi*zgi)/zscratch
-!!$          END DO
-!!$          !
-!!$          ! overwrite Kinne Optics with old Cloud Optics for LW Only
-!!$          !
-!!$          zmsald=0.025520637_wp+0.2854650784_wp*EXP(-0.088968393014_wp  &
-!!$               *re_droplets)
-!!$          DO ii = 1,16
-!!$            iband = ii
-!!$            IF (ii == 16) iband=30 
-!!$            zmsaid=(rebcuh(ii)+rebcug(ii)/re_crystals)
-!!$            ztau(jl,jk,iband)  = zmsald*zlwp(jl,jk)*zinhoml(jl)          &
-!!$                 &             + zmsaid*ziwp(jl,jk)*zinhomi
-!!$          END DO
-!!$        ELSE
-!!$          ztau(jl,jk,:)  = 0.0_wp
-!!$          zomg(jl,jk,:)  = 1.0_wp
-!!$          zasy(jl,jk,:)  = 0.0_wp
-!!$          re_crystals2d(jl,jk) = 0.0_wp
-!!$          re_droplets2d(jl,jk) = 0.0_wp
-!!$        END IF
-!!$        tau_lw(jl,jk,1:nb_lw-1) = ztau(jl,jk,1:15)
-!!$        tau_lw(jl,jk,nb_lw)     = ztau(jl,jk,30)
-!!$        tau_sw(jl,jk,1:nb_sw)   = ztau(jl,jk,16:29)
-!!$        omg   (jl,jk,1:nb_sw)   = zomg(jl,jk,16:29)
-!!$        asy   (jl,jk,1:nb_sw)   = zasy(jl,jk,16:29)
-!!$      END DO
-!!$    END DO
-!!$
-!!$  END SUBROUTINE cloud_optics
+  SUBROUTINE cloud_optics(                                               &
+          & laglac       ,laland          ,kproma         ,kbdim         ,&
+          & klev         ,ktype           ,nb_lw          ,nb_sw         ,&
+          & icldlyr      ,zlwp            ,ziwp           ,zlwc          ,&
+          & ziwc         ,zcdnc           ,tau_lw         ,tau_sw        ,&
+          & omg          ,asy             ,re_droplets2d  ,re_crystals2d  )
+
+    INTEGER, INTENT (IN)    :: &
+         kproma,               & !< actual length of grid point block
+         kbdim,                & !< maximum length of grid point block
+         klev,                 &
+         ktype(kbdim),         & !< type of convection
+         nb_sw,                & 
+         nb_lw,                &
+         icldlyr(kbdim,klev)
+    LOGICAL, INTENT (IN)    :: & 
+         laglac(kbdim),        & !< logical for glacier points
+         laland(kbdim)           !< logical for land points
+    REAL (wp), INTENT (IN)  :: &
+         zlwp(kbdim,klev),     & !< liquid water path
+         ziwp(kbdim,klev),     & !< ice water path
+         zcdnc(kbdim,klev),    & !< cloud drop number concentration
+         zlwc(kbdim,klev),     & !< liquid water content
+         ziwc(kbdim,klev)        !< ice water content
+
+    REAL (wp), INTENT (OUT) ::       &
+         tau_lw(kbdim,klev,nb_lw),   & !< LW optical depth
+         tau_sw(kbdim,klev,nb_sw),   & !< SW optical depth
+         omg   (kbdim,klev,nb_sw),   & !< cloud single scattering albedo
+         asy   (kbdim,klev,nb_sw),   & !< cloud asymmetry factor
+         re_droplets2d(kbdim,klev),  & !< effective radius of liquid distribution
+         re_crystals2d(kbdim,klev)     !< effective radius of ice distribution
+
+
+    INTEGER  :: iband, ii, jk, jl, ml1, ml2, mi1, mi2
+    REAL(wp) :: ztol, ztoi, zol, zoi, zgl, zgi, wl1, wl2, wi1, wi2, zfact
+    REAL(wp) :: zmsald, zmsaid
+
+    REAL(wp) :: &
+         zkap(kbdim),        & !< breath parameter for scaling effective radius
+         zlwpt(kbdim),       & !< liquid water path
+         zinhoml(kbdim),     & !< cloud inhomogeneity factor (liquid)
+         re_droplets,        & !< effective radius of liquid distribution
+         re_crystals,        & !< effective radius of ice distribution
+         zscratch,           & !< effective radius of ice distribution
+         ztau(kbdim,klev,n_mdl_bnds), & !< SW optical depth
+         zomg(kbdim,klev,n_mdl_bnds), & !< cloud single scattering albedo
+         zasy(kbdim,klev,n_mdl_bnds)    !< cloud asymmetry factor
+
+    IF (relmin < 1.5 .OR. relmin > 2.5 ) THEN
+      CALL finish('Apparently unsuccessful loading of optical tables')
+    END IF
+    !
+    ! 1.0 Basic cloud properties
+    ! --------------------------------
+    IF (l_variable_inhoml) THEN
+      zlwpt(1:kproma) = 0.0_wp
+      DO jk = 1, klev
+        DO jl = 1, kproma
+          zlwpt(jl) = zlwpt(jl)+zlwp(jl,jk)
+        END DO
+      END DO
+      WHERE (zlwpt(1:kproma) > 1.0_wp) 
+        zinhoml(1:kproma) = zlwpt(1:kproma)**(-zinpar)
+      ELSEWHERE
+        zinhoml(1:kproma) = 1.0_wp
+      END WHERE
+    ELSE
+      DO jl = 1, kproma
+        IF(ktype(jl) .EQ. 0) THEN          !no convection; ktype=0
+          zinhoml(jl) = zinhoml1
+        ELSE IF(ktype(jl) .NE. 4) THEN     !convection; ktype=1,2,3
+          zinhoml(jl) = zinhoml3
+        ELSE                               !shallow convection and clwprat>0.; ktype=4
+          zinhoml(jl) = zinhoml2
+        END IF
+      END DO
+    END IF
+
+    WHERE (laland(1:kproma).AND.(.NOT.laglac(1:kproma))) 
+      zkap(1:kproma)=zkap_cont ! continental breadth factor
+    ELSEWHERE
+      zkap(1:kproma)=zkap_mrtm ! maritime breadth factor 
+    END WHERE
+    !
+    ! 2.0 Cloud Optical Properties by interpolating tables in effective radius
+    ! --------------------------------
+    zfact = 1.0e6_wp*(3.0e-9_wp/(4.0_wp*pi*rhoh2o))**(1.0_wp/3.0_wp)
+    DO jk=1,klev
+      DO jl=1,kproma
+        IF (icldlyr(jl,jk)==1 .AND. (zlwp(jl,jk)+ziwp(jl,jk))>ccwmin) THEN
+          
+          re_crystals = MAX(reimin,MIN(reimax,83.8_wp*ziwc(jl,jk)**0.216_wp))
+          re_droplets = MAX(relmin,MIN(relmax,zfact*zkap(jl)*(zlwc(jl,jk)    &
+               &                             /zcdnc(jl,jk))**(1.0_wp/3.0_wp)))
+          
+          re_crystals2d(jl,jk) = re_crystals
+          re_droplets2d(jl,jk) = re_droplets
+
+          ml1 = MAX(1,MIN(n_sizes-1,FLOOR(1.0_wp+(re_droplets-relmin)/del_rel)))
+          ml2 = ml1 + 1
+          wl1 = 1.0_wp - (re_droplets - (relmin + del_rel* (ml1-1)) )/del_rel
+          wl2 = 1.0_wp - wl1
+
+          mi1 = MAX(1,MIN(n_sizes-1,FLOOR(1.0_wp+(re_crystals-reimin)/del_rei)))
+          mi2 = mi1 + 1
+          wi1 = 1.0_wp - (re_crystals - (reimin + del_rei * (mi1-1)) )/del_rei
+          wi2 = 1.0_wp - wi1
+
+          DO iband = 1,n_mdl_bnds
+            ztol = zlwp(jl,jk)*(wl1*z_ext_l(ml1,iband) + wl2*z_ext_l(ml2,iband))
+            ztoi = ziwp(jl,jk)*(wi1*z_ext_i(mi1,iband) + wi2*z_ext_i(mi2,iband))
+            zol  = 1.0_wp - (wl1*z_coa_l(ml1,iband) + wl2*z_coa_l(ml2,iband))
+            zoi  = 1.0_wp - (wi1*z_coa_i(mi1,iband) + wi2*z_coa_i(mi2,iband))
+            zgl  = wl1*z_asy_l(ml1,iband) + wl2*z_asy_l(ml2,iband)
+            zgi  = wi1*z_asy_i(mi1,iband) + wi2*z_asy_i(mi2,iband)
+
+            zscratch = (ztol*zol+ztoi*zoi)
+            ztau(jl,jk,iband) = ztol*zinhoml(jl) + ztoi*zinhomi
+            zomg(jl,jk,iband) = zscratch/(ztol+ztoi)
+            zasy(jl,jk,iband) = (ztol*zol*zgl+ztoi*zoi*zgi)/zscratch
+          END DO
+          !
+          ! overwrite Kinne Optics with old Cloud Optics for LW Only
+          !
+          zmsald=0.025520637_wp+0.2854650784_wp*EXP(-0.088968393014_wp  &
+               *re_droplets)
+          DO ii = 1,16
+            iband = ii
+            IF (ii == 16) iband=30 
+            zmsaid=(rebcuh(ii)+rebcug(ii)/re_crystals)
+            ztau(jl,jk,iband)  = zmsald*zlwp(jl,jk)*zinhoml(jl)          &
+                 &             + zmsaid*ziwp(jl,jk)*zinhomi
+          END DO
+        ELSE
+          ztau(jl,jk,:)  = 0.0_wp
+          zomg(jl,jk,:)  = 1.0_wp
+          zasy(jl,jk,:)  = 0.0_wp
+          re_crystals2d(jl,jk) = 0.0_wp
+          re_droplets2d(jl,jk) = 0.0_wp
+        END IF
+        tau_lw(jl,jk,1:nb_lw-1) = ztau(jl,jk,1:15)
+        tau_lw(jl,jk,nb_lw)     = ztau(jl,jk,30)
+        tau_sw(jl,jk,1:nb_sw)   = ztau(jl,jk,16:29)
+        omg   (jl,jk,1:nb_sw)   = zomg(jl,jk,16:29)
+        asy   (jl,jk,1:nb_sw)   = zasy(jl,jk,16:29)
+      END DO
+    END DO
+
+  END SUBROUTINE cloud_optics
 
 END MODULE mo_psrad_cloud_optics
