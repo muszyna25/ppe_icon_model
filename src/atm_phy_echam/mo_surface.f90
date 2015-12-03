376af36c5171221d01980455d1bdc6a3c2283c04
Marco Giorgetta <marco.giorgetta@mpimet.mpg.de>
2015-12-03 17:44:02 +0000

    [icon-aes-mag] Debug test 5: Also do not compute "AFTER CALLING land/ocean/ice model".

--- src/atm_phy_echam/mo_surface.f90
+++ src/atm_phy_echam/mo_surface.f90
@@ -493,92 +493,92 @@ CONTAINS
 #endif
     ENDIF ! lice
 
-    !===================================================================
-    ! AFTER CALLING land/ocean/ice model
-    !===================================================================
-    ! Turbulent transport of moisture and dry static energy:
-    ! Get solution of the two variables on the lowest model level.
-    !-------------------------------------------------------------------
-    ! - Over individual tiles
-    !   For echam developers: relationship to "update_surface" of echam6:
-    !   bb_btm(:,jsfc,ih) : tpfac2*land%ztklevl, tpfac2*ice%ztklevi, tpfac2*ocean%ztklevw
-    !   bb_btm(:,jsfc,iqv): tpfac2*land%zqklevl, tpfac2*ice%zqklevi, tpfac2*ocean%zqklevw
-
-    DO jsfc = 1,ksfc_type
-       bb_btm(1:kproma,jsfc,ih)  = tpfac2*(    zen_h (1:kproma,jsfc) &
-                                 &         *pcpt_tile(1:kproma,jsfc) &
-                                 &         +   zfn_h (1:kproma,jsfc) )
-
-       bb_btm(1:kproma,jsfc,iqv) = tpfac2*(    zen_qv(1:kproma,jsfc) &
-                                 &        *pqsat_tile(1:kproma,jsfc) &
-                                 &        +    zfn_qv(1:kproma,jsfc) )
-    END DO
-
-    ! - Grid box mean
-    !   For echam developers: relationship to "update_surface" of echam6:
-    !   bb(:,klev,ih) : ztdif_new
-    !   bb(:,klev,iqv): zqdif_new
-
-     se_sum(1:kproma) = 0._wp    ! sum of weighted solution
-     qv_sum(1:kproma) = 0._wp    ! sum of weighted solution
-    wgt_sum(1:kproma) = 0._wp    ! sum of weights
-
-    DO jsfc = 1,ksfc_type
-           wgt(1:kproma) = pfrc(1:kproma,jsfc)
-       wgt_sum(1:kproma) = wgt_sum(1:kproma) + wgt(1:kproma)
-        se_sum(1:kproma) = se_sum(1:kproma) + bb_btm(1:kproma,jsfc,ih ) * wgt(1:kproma)
-        qv_sum(1:kproma) = qv_sum(1:kproma) + bb_btm(1:kproma,jsfc,iqv) * wgt(1:kproma)
-    ENDDO
-
-    IF (lsfc_heat_flux) THEN
-      bb(1:kproma,klev,ih ) = se_sum(1:kproma)/wgt_sum(1:kproma)
-      bb(1:kproma,klev,iqv) = qv_sum(1:kproma)/wgt_sum(1:kproma)
-    ELSE
-      jsfc = 1
-      bb(1:kproma,klev,ih ) = bb_btm(1:kproma,jsfc,ih )
-      bb(1:kproma,klev,iqv) = bb_btm(1:kproma,jsfc,iqv)
-    END IF
-
-    !-------------------------------------------------------------------
-    ! Turbulent transport of u and v: adjust the right-hand side vector,
-    ! then perform the bottom level elimination to get the solution
-    !-------------------------------------------------------------------
-    ! Add additional terms to the r.h.s. of the velocity equations
-    ! to take into account ocean currents.
-    ! Note that in subroutine rhs_setup the constant tpfac2 has been
-    ! multiplied to the r.h.s. array bb. Thus the additional terms here
-    ! need to be scaled by the same factor.
-
-    IF (idx_wtr.LE.ksfc_type) THEN   ! Open water is considered
-      IF (idx_ice.LE.ksfc_type) THEN ! Sea ice is also considered
-        zfrc_oce(1:kproma) = pfrc(1:kproma,idx_wtr)+pfrc(1:kproma,idx_ice)
-      ELSE ! only open water
-        zfrc_oce(1:kproma) = pfrc(1:kproma,idx_wtr)
-      ENDIF
-      bb(1:kproma,klev,iu) =   bb(1:kproma,klev,iu)                   &
-                           & - pocu(1:kproma)*zfrc_oce(1:kproma)*tpfac2
-      bb(1:kproma,klev,iv) =   bb(1:kproma,klev,iv)                   &
-                           & - pocv(1:kproma)*zfrc_oce(1:kproma)*tpfac2
-    ENDIF
-
-    ! Bottom level elimination
-
-    im   = imuv
-    jk   = klev    ! Bottom level index
-    jkm1 = jk - 1
-
-    aa(1:kproma,jk,2,im) =  aa(1:kproma,jk,2,im)                      &
-                         & -aa(1:kproma,jk,1,im)*aa(1:kproma,jkm1,3,im)
-    aa(1:kproma,jk,3,im) =  aa(1:kproma,jk,3,im)/aa(1:kproma,jk,2,im)
-
-    bb(1:kproma,jk,iu) = (bb(1:kproma,jk,iu)                         &
-                       & -aa(1:kproma,jk,1,im)*bb(1:kproma,jkm1,iu)) &
-                       & /aa(1:kproma,jk,2,im)
-
-    bb(1:kproma,jk,iv) = (bb(1:kproma,jk,iv)                         &
-                       & -aa(1:kproma,jk,1,im)*bb(1:kproma,jkm1,iv)) &
-                       & /aa(1:kproma,jk,2,im)
-
+!!$    !===================================================================
+!!$    ! AFTER CALLING land/ocean/ice model
+!!$    !===================================================================
+!!$    ! Turbulent transport of moisture and dry static energy:
+!!$    ! Get solution of the two variables on the lowest model level.
+!!$    !-------------------------------------------------------------------
+!!$    ! - Over individual tiles
+!!$    !   For echam developers: relationship to "update_surface" of echam6:
+!!$    !   bb_btm(:,jsfc,ih) : tpfac2*land%ztklevl, tpfac2*ice%ztklevi, tpfac2*ocean%ztklevw
+!!$    !   bb_btm(:,jsfc,iqv): tpfac2*land%zqklevl, tpfac2*ice%zqklevi, tpfac2*ocean%zqklevw
+!!$
+!!$    DO jsfc = 1,ksfc_type
+!!$       bb_btm(1:kproma,jsfc,ih)  = tpfac2*(    zen_h (1:kproma,jsfc) &
+!!$                                 &         *pcpt_tile(1:kproma,jsfc) &
+!!$                                 &         +   zfn_h (1:kproma,jsfc) )
+!!$
+!!$       bb_btm(1:kproma,jsfc,iqv) = tpfac2*(    zen_qv(1:kproma,jsfc) &
+!!$                                 &        *pqsat_tile(1:kproma,jsfc) &
+!!$                                 &        +    zfn_qv(1:kproma,jsfc) )
+!!$    END DO
+!!$
+!!$    ! - Grid box mean
+!!$    !   For echam developers: relationship to "update_surface" of echam6:
+!!$    !   bb(:,klev,ih) : ztdif_new
+!!$    !   bb(:,klev,iqv): zqdif_new
+!!$
+!!$     se_sum(1:kproma) = 0._wp    ! sum of weighted solution
+!!$     qv_sum(1:kproma) = 0._wp    ! sum of weighted solution
+!!$    wgt_sum(1:kproma) = 0._wp    ! sum of weights
+!!$
+!!$    DO jsfc = 1,ksfc_type
+!!$           wgt(1:kproma) = pfrc(1:kproma,jsfc)
+!!$       wgt_sum(1:kproma) = wgt_sum(1:kproma) + wgt(1:kproma)
+!!$        se_sum(1:kproma) = se_sum(1:kproma) + bb_btm(1:kproma,jsfc,ih ) * wgt(1:kproma)
+!!$        qv_sum(1:kproma) = qv_sum(1:kproma) + bb_btm(1:kproma,jsfc,iqv) * wgt(1:kproma)
+!!$    ENDDO
+!!$
+!!$    IF (lsfc_heat_flux) THEN
+!!$      bb(1:kproma,klev,ih ) = se_sum(1:kproma)/wgt_sum(1:kproma)
+!!$      bb(1:kproma,klev,iqv) = qv_sum(1:kproma)/wgt_sum(1:kproma)
+!!$    ELSE
+!!$      jsfc = 1
+!!$      bb(1:kproma,klev,ih ) = bb_btm(1:kproma,jsfc,ih )
+!!$      bb(1:kproma,klev,iqv) = bb_btm(1:kproma,jsfc,iqv)
+!!$    END IF
+!!$
+!!$    !-------------------------------------------------------------------
+!!$    ! Turbulent transport of u and v: adjust the right-hand side vector,
+!!$    ! then perform the bottom level elimination to get the solution
+!!$    !-------------------------------------------------------------------
+!!$    ! Add additional terms to the r.h.s. of the velocity equations
+!!$    ! to take into account ocean currents.
+!!$    ! Note that in subroutine rhs_setup the constant tpfac2 has been
+!!$    ! multiplied to the r.h.s. array bb. Thus the additional terms here
+!!$    ! need to be scaled by the same factor.
+!!$
+!!$    IF (idx_wtr.LE.ksfc_type) THEN   ! Open water is considered
+!!$      IF (idx_ice.LE.ksfc_type) THEN ! Sea ice is also considered
+!!$        zfrc_oce(1:kproma) = pfrc(1:kproma,idx_wtr)+pfrc(1:kproma,idx_ice)
+!!$      ELSE ! only open water
+!!$        zfrc_oce(1:kproma) = pfrc(1:kproma,idx_wtr)
+!!$      ENDIF
+!!$      bb(1:kproma,klev,iu) =   bb(1:kproma,klev,iu)                   &
+!!$                           & - pocu(1:kproma)*zfrc_oce(1:kproma)*tpfac2
+!!$      bb(1:kproma,klev,iv) =   bb(1:kproma,klev,iv)                   &
+!!$                           & - pocv(1:kproma)*zfrc_oce(1:kproma)*tpfac2
+!!$    ENDIF
+!!$
+!!$    ! Bottom level elimination
+!!$
+!!$    im   = imuv
+!!$    jk   = klev    ! Bottom level index
+!!$    jkm1 = jk - 1
+!!$
+!!$    aa(1:kproma,jk,2,im) =  aa(1:kproma,jk,2,im)                      &
+!!$                         & -aa(1:kproma,jk,1,im)*aa(1:kproma,jkm1,3,im)
+!!$    aa(1:kproma,jk,3,im) =  aa(1:kproma,jk,3,im)/aa(1:kproma,jk,2,im)
+!!$
+!!$    bb(1:kproma,jk,iu) = (bb(1:kproma,jk,iu)                         &
+!!$                       & -aa(1:kproma,jk,1,im)*bb(1:kproma,jkm1,iu)) &
+!!$                       & /aa(1:kproma,jk,2,im)
+!!$
+!!$    bb(1:kproma,jk,iv) = (bb(1:kproma,jk,iv)                         &
+!!$                       & -aa(1:kproma,jk,1,im)*bb(1:kproma,jkm1,iv)) &
+!!$                       & /aa(1:kproma,jk,2,im)
+!!$
 !!$   !-------------------------------------------------------------------
 !!$   ! Various diagnostics
 !!$   !-------------------------------------------------------------------
