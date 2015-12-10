ab862917fa43a14cb26d0c168afa4dd1c2324ddf
Ren�� Redler <rene.redler@mpimet.mpg.de>
2015-12-10 15:53:18 +0000

    [icon-cimd-yac] workaround in yac for incomplete impi and mvapich on mistral

--- src/configure_model/mo_coupling_config.f90
+++ src/configure_model/mo_coupling_config.f90
@@ -27,11 +27,7 @@ CONTAINS
 
     is_coupled_run = config_coupled_mode
 
-    WRITE ( 6 , * ) " Rene is_coupled_run ", is_coupled_run
-    FLUSH ( 6 )
-
   END FUNCTION is_coupled_run
   !------------------------------------------------------------------------
-
 
 END MODULE mo_coupling_config
