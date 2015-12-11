09d5ba2af6eb9a50799dbf903495c20b5f6eb6aa
Daniel Rieger <daniel.rieger@kit.edu>
2015-12-11 09:24:14 +0000

    [icon-kit-ohchem branch] Commented a debug message

--- src/atm_phy_nwp/mo_nwp_rrtm_interface.f90
+++ src/atm_phy_nwp/mo_nwp_rrtm_interface.f90
@@ -169,8 +169,8 @@ CONTAINS
     CASE (9)
       CALL calc_o3_gems(pt_patch,datetime,pt_diag,prm_diag,ext_data)
     CASE(10)
-      CALL message('mo_nwp_rg_interface:irad_o3=10', &  
-        &          'Ozone used for radiation is calculated by ART')
+      !CALL message('mo_nwp_rg_interface:irad_o3=10', &  
+      !  &          'Ozone used for radiation is calculated by ART')
     END SELECT
 
     IF ( irad_aero == 6 ) CALL month2hour (datetime, imo1, imo2, zw )
