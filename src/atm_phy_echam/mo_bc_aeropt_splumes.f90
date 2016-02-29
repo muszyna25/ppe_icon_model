394821d183dff5c456e51c2c3e856d09ec4b25f7
Sebastian Rast <sebastian.rast@mpimet.mpg.de>
2016-02-29 16:26:44 +0000

    [icon-aes-smp-jsr]: simple plume module is completed and compiles, however, it is not yet applied and tested.

--- src/atm_phy_echam/mo_bc_aeropt_splumes.f90
+++ src/atm_phy_echam/mo_bc_aeropt_splumes.f90
@@ -21,13 +21,24 @@ MODULE mo_bc_aeropt_splumes
   USE mo_read_interface,       ONLY: openInputFile, read_1D, &
                                    & read_bcast_real_2D, read_bcast_real_3D, &
                                    & closeFile
+  USE mo_datetime,             ONLY: t_datetime
+  USE mo_physical_constants,   ONLY: rgrav
+  USE mo_echam_phy_memory,     ONLY: t_echam_phy_field
+  USE mo_model_domain,         ONLY: p_patch
+  USE mo_psrad_srtm_setup,     ONLY: &
+      &  sw_wv1 => wavenum1     ,&     !< smallest wave number in each of the sw bands
+      &  sw_wv2 => wavenum2            !< largest wave number in each of the sw bands
+  USE mo_rrtm_params,          ONLY: &
+      & jpb1                    ,&     !< index for lower sw band
+      & jpb2                           !< index for upper sw band
+
 !!$, on_cells, &
 !!$    &                                t_stream_id, read_0D_real, read_3D_time
 
   IMPLICIT NONE
 
   PRIVATE
-  PUBLIC                  :: sp_setup
+  PUBLIC                  :: sp_setup, add_aop_sp
 
   INTEGER, PARAMETER      ::     &
        nplumes   = 9            ,& !< Number of plumes
@@ -79,7 +90,6 @@ MODULE mo_bc_aeropt_splumes
     ! ---------- 
     !
     INTEGER           :: ifile_id
-    CHARACTER(len=32) :: ci_length, cj_length
 
     cfname='MACv2.0-SP_v1-beta.nc'
     ifile_id=openInputFile(cfname)
@@ -173,6 +183,334 @@ MODULE mo_bc_aeropt_splumes
     sp_initialized = .TRUE.
     RETURN
   END SUBROUTINE SP_SETUP
+  ! ------------------------------------------------------------------------------------------------------------------------
+  ! SET_TIME_WEIGHT:  The simple plume model assumes that meteorology constrains plume shape and that only source strength
+  ! influences the amplitude of a plume associated with a given source region.   This routine retrieves the temporal weights
+  ! for the plumes.  Each plume feature has its own temporal weights which varies yearly.  The annual cycle is indexed by
+  ! week in the year and superimposed on the yearly mean value of the weight. 
+  !
+  SUBROUTINE set_time_weight(year_fr)
+    !
+    ! ---------- 
+    !
+    REAL(wp), INTENT(IN) ::  &
+         year_fr           !< Fractional Year (1850.0 - 2100.99)
+
+    INTEGER          ::  &
+         idate(8)       ,& !< integer array of system clock date yyyy, mm, dd
+         iyear          ,& !< Integer year values between 1 and 156 (1850-2100) 
+         iweek          ,& !< Integer index (between 1 and ntimes); for ntimes=52 this corresponds to weeks (roughly)
+         iplume            ! plume number
+    CHARACTER(LEN=1024) :: message
+    !
+    ! ---------- 
+    !
+    iyear = FLOOR(year_fr) - 1850 + 1
+    iweek = FLOOR((year_fr - FLOOR(year_fr)) * ntimes) + 1
+
+    IF ((iweek > ntimes) .OR. (iweek < 1) .OR. (iyear > nyears) .OR. (iyear < 1)) STOP 'Time out of bounds in set_time_weight'
+    DO iplume=1,nplumes
+      time_weight(1,iplume) = year_weight(iyear,iplume) * ann_cycle(1,iweek,iplume)
+      time_weight(2,iplume) = year_weight(iyear,iplume) * ann_cycle(2,iweek,iplume)
+    END DO
+    !
+    ! check if code is being used in beta version and stop if it is after June 1, 2016
+    !
+    CALL DATE_AND_TIME(VALUES=idate)
+    IF (idate(1) >= 2016 .AND. idate(2) >= 6) THEN
+      message='System clock says the date is after June 2016 at which time this release should be depreciated.'
+      message=TRIM(ADJUSTL(message))//'REASON: input data for spimple plumes may be outdated, code may be buggy'
+      message=TRIM(ADJUSTL(message))//'Terminating; please check with developers for a finalized CMIP6 version of the code.'
+      CALL finish('set_time_weight of mo_bc_aeropt_splumes',message)
+    END IF
+    
+    RETURN
+  END SUBROUTINE set_time_weight
+  !
+  ! ------------------------------------------------------------------------------------------------------------------------
+  ! SP_AOP_PROFILE:  This subroutine calculates the simple plume aerosol and cloud active optical properites based on the
+  ! the simple plume fit to the MPI Aerosol Climatology (Version 2).  It sums over nplumes to provide a profile of aerosol
+  ! optical properties on a host models vertical grid. 
+  !
+  SUBROUTINE sp_aop_profile( &
+     & nlevels        ,ncol           ,ncol_max       ,lambda         ,oro            ,lon            , &
+     & lat            ,year_fr        ,z              ,dz             ,dNovrN         ,aod_prof       , &
+     & ssa_prof       ,asy_prof       )
+    !
+    ! ---------- 
+    !
+    INTEGER, INTENT(IN)        :: &
+       & nlevels,                 & !< number of levels
+       & ncol,                    & !< number of columns
+       & ncol_max                   !< first dimension of 2d-vars as declared in calling (sub)program
+
+    REAL(wp), INTENT(IN)       :: &
+       & lambda,                  & !< wavelength
+       & year_fr,                 & !< Fractional Year (1903.0 is the 0Z on the first of January 1903, Gregorian)
+       & oro(ncol),               & !< orographic height (m)
+       & lon(ncol),               & !< longitude 
+       & lat(ncol),               & !< latitude
+       & z (ncol_max,nlevels),    & !< height above sea-level (m)
+       & dz(ncol_max,nlevels)       !< level thickness (difference between half levels)
+
+    REAL(wp), INTENT(OUT)      ::     &
+       & dNovrN(ncol)               , & !< anthropogenic incroment to cloud drop number concentration 
+       & aod_prof(ncol_max,nlevels) , & !< profile of aerosol optical depth
+       & ssa_prof(ncol_max,nlevels) , & !< profile of single scattering albedo
+       & asy_prof(ncol_max,nlevels)     !< profile of asymmetry parameter
+
+    INTEGER                    :: iplume, icol, k
+
+    REAL(wp)                   ::  &
+       & eta(ncol_max,nlevels),    & !< normalized height (by 15 km)
+       & z_beta(ncol_max,nlevels), & !< profile for scaling column optical depth
+       & prof(ncol_max,nlevels),   & !< scaled profile (by beta function)
+       & beta_sum(ncol),           & !< vertical sum of beta function
+       & ssa(ncol),                & !< aerosol optical depth 
+       & asy(ncol),                & !< aerosol optical depth 
+       & cw_an(ncol),              & !< column weight for simple plume (anthropogenic) aod at 550 nm
+       & cw_bg(ncol),              & !< column weight for fine-mode background aod at 550 nm
+       & caod_sp(ncol),            & !< column simple plume (anthropogenic) aod at 550 nm
+       & caod_bg(ncol),            & !< column fine-mode background aod at 550 nm
+       & a_plume1,                 & !< gaussian longitude factor for feature 1
+       & a_plume2,                 & !< gaussian longitude factor for feature 2
+       & b_plume1,                 & !< gaussian latitude factor for feature 1
+       & b_plume2,                 & !< gaussian latitude factor for feature 2
+       & delta_lat,                & !< latitude offset
+       & delta_lon,                & !< longitude offset
+       & lon1,                     & !< rotated longitude for feature 1
+       & lat1,                     & !< rotated latitude for feature 2
+       & lon2,                     & !< rotated longitude for feature 1
+       & lat2,                     & !< rotated latitude for feature 2
+       & f1,                       & !< contribution from feature 1
+       & f2,                       & !< contribution from feature 2
+       & aod_550,                  & !< aerosol optical depth at 550nm
+       & aod_lmd,                  & !< aerosol optical depth at input wavelength
+       & lfactor                     !< factor to compute wavelength dependence of optical properties
+    !
+    ! ---------- 
+    !
+    ! initialize input data (by calling setup at first instance) 
+    !
+    IF (.NOT.sp_initialized) CALL sp_setup
+    !
+    ! get time weights
+    !
+    CALL set_time_weight(year_fr)
+    !
+    ! initialize variables, including output
+    !
+    DO k=1,nlevels
+      DO icol=1,ncol
+        aod_prof(icol,k) = 0.0
+        ssa_prof(icol,k) = 0.0
+        asy_prof(icol,k) = 0.0
+        z_beta(icol,k)   = MERGE(1.0_wp, 0.0_wp, z(icol,k) >= oro(icol))
+        eta(icol,k)      = MAX(0.0_wp,MIN(1.0_wp,z(icol,k)/15000.))
+      END DO
+    END DO
+    DO icol=1,ncol
+      dNovrN(icol)   = 1.0_wp
+      caod_sp(icol)  = 0.00_wp
+      caod_bg(icol)  = 0.02_wp
+    END DO
+    !
+    ! sum contribution from plumes to construct composite profiles of aerosol otpical properties
+    !
+    DO iplume=1,nplumes
+      !
+      ! calculate vertical distribution function from parameters of beta distribution
+      !
+      DO icol=1,ncol
+        beta_sum(icol) = 0._wp
+      END DO
+      DO k=1,nlevels
+        DO icol=1,ncol
+          prof(icol,k)   = (eta(icol,k)**(beta_a(iplume)-1._wp) * (1._wp-eta(icol,k))**(beta_b(iplume)-1._wp))*dz(icol,k)
+          beta_sum(icol) = beta_sum(icol) + prof(icol,k)
+        END DO
+      END DO
+      DO k=1,nlevels
+        DO icol=1,ncol
+          prof(icol,k)   = prof(icol,k) / beta_sum(icol) * z_beta(icol,k)
+        END DO
+      END DO
+      !
+      ! calculate plume weights
+      !
+      DO icol=1,ncol
+        !
+        ! get plume-center relative spatial parameters for specifying amplitude of plume at given lat and lon
+        !
+        delta_lat = lat(icol) - plume_lat(iplume)
+        delta_lon = lon(icol) - plume_lon(iplume)
+        delta_lon = MERGE ( delta_lon-SIGN(360._wp,delta_lon) , delta_lon , ABS(delta_lon) > 180._wp )
+        a_plume1  = 0.5_wp / (MERGE(sig_lon_E(1,iplume), sig_lon_W(1,iplume), delta_lon > 0)**2)
+        b_plume1  = 0.5_wp / (MERGE(sig_lat_E(1,iplume), sig_lat_W(1,iplume), delta_lon > 0)**2)
+        a_plume2  = 0.5_wp / (MERGE(sig_lon_E(2,iplume), sig_lon_W(2,iplume), delta_lon > 0)**2)
+        b_plume2  = 0.5_wp / (MERGE(sig_lat_E(2,iplume), sig_lat_W(2,iplume), delta_lon > 0)**2)
+        !
+        ! adjust for a plume specific rotation which helps match plume state to climatology.
+        !
+        lon1 =   COS(theta(1,iplume))*(delta_lon) + SIN(theta(1,iplume))*(delta_lat)
+        lat1 = - SIN(theta(1,iplume))*(delta_lon) + COS(theta(1,iplume))*(delta_lat)
+        lon2 =   COS(theta(2,iplume))*(delta_lon) + SIN(theta(2,iplume))*(delta_lat)
+        lat2 = - SIN(theta(2,iplume))*(delta_lon) + COS(theta(2,iplume))*(delta_lat)
+        !
+        ! calculate contribution to plume from its different features, to get a column weight for the anthropogenic
+        ! (cw_an) and the fine-mode background aerosol (cw_bg)
+        !
+        f1 = time_weight(1,iplume) * ftr_weight(1,iplume) * EXP(-1._wp* (a_plume1 * ((lon1)**2) + (b_plume1 * ((lat1)**2)))) 
+        f2 = time_weight(2,iplume) * ftr_weight(2,iplume) * EXP(-1._wp* (a_plume2 * ((lon2)**2) + (b_plume2 * ((lat2)**2)))) 
+
+        cw_an(icol) = f1 * aod_spmx(iplume) + f2 * aod_spmx(iplume)  
+        cw_bg(icol) = f1 * aod_fmbg(iplume) + f2 * aod_fmbg(iplume) 
+        !
+        ! calculate wavelength-dependent scattering properties
+        !
+        lfactor   = MIN(1.0_wp,700.0_wp/lambda)
+        ssa(icol) = (ssa550(iplume) * lfactor**4) / ((ssa550(iplume) * lfactor**4) + ((1-ssa550(iplume)) * lfactor))
+        asy(icol) =  asy550(iplume) * SQRT(lfactor)
+      END DO
+      !
+      ! distribute plume optical properties across its vertical profile weighting by optical depth and scaling for
+      ! wavelength using the anstrom parameter. 
+      !      
+      lfactor = EXP(-angstrom(iplume) * LOG(lambda/550.0_wp))
+      DO k=1,nlevels
+        DO icol = 1,ncol
+          aod_550          = prof(icol,k)     * cw_an(icol)
+          aod_lmd          = aod_550          * lfactor
+          caod_sp(icol)    = caod_sp(icol)    + prof(icol,k) * cw_an(icol)
+          caod_bg(icol)    = caod_bg(icol)    + prof(icol,k) * cw_bg(icol)
+          asy_prof(icol,k) = asy_prof(icol,k) + aod_lmd * ssa(icol) * asy(icol)
+          ssa_prof(icol,k) = ssa_prof(icol,k) + aod_lmd * ssa(icol)
+          aod_prof(icol,k) = aod_prof(icol,k) + aod_lmd
+        END DO
+      END DO
+    END DO
+    !
+    ! complete optical depth weighting
+    !
+    DO k=1,nlevels
+      DO icol = 1,ncol
+        asy_prof(icol,k) = MERGE(asy_prof(icol,k)/ssa_prof(icol,k), 0.0_wp, ssa_prof(icol,k) > TINY(1._wp))
+        ssa_prof(icol,k) = MERGE(ssa_prof(icol,k)/aod_prof(icol,k), 1.0_wp, aod_prof(icol,k) > TINY(1._wp))
+      END DO
+    END DO
+    !
+    ! calcuate effective radius normalization (divisor) factor
+    !
+    DO icol=1,ncol
+      dNovrN(icol) = LOG((1000.0_wp * (caod_sp(icol) + caod_bg(icol))) + 3.0_wp)/LOG((1000.0_wp * caod_bg(icol)) + 3.0_wp)
+    END DO
+
+    RETURN
+  END SUBROUTINE sp_aop_profile
+  ! ------------------------------------------------------------------------------------------------------------------------
+  ! ADD_AOP_SP:  This subroutine provides the interface to simple plume (sp) fit to the MPI Aerosol Climatology (Version 2).
+  ! It does so by collecting or deriving spatio-temporal information and calling the simple plume aerosol subroutine and
+  ! incrementing the background aerosol properties (and effective radisu) with the anthropogenic plumes.
+  !
+  SUBROUTINE add_aop_sp                                                           ( &
+     & jg             ,kproma         ,kbdim          ,klev           ,krow        ,&
+     & nb_sw          ,datetime       ,prm_field      ,aod_sw_vr      ,ssa_sw_vr   ,&
+     & asy_sw_vr      ,x_cdnc                                                     )
+    !
+    ! --- 0.1 Variables passed through argument list
+    INTEGER, INTENT(IN) ::            &
+         jg                          ,& !< domain index
+         kproma                      ,& !< number of elements in current block
+         kbdim                       ,& !< block dimension (greater than or equal to kproma)
+         klev                        ,& !< number of full levels
+         krow                        ,& !< index for current block
+         nb_sw                          !< number of bands in short wave
+
+    TYPE(t_datetime), INTENT(IN) :: datetime
+
+    TYPE(t_echam_phy_field), INTENT(IN) :: prm_field
+
+    REAL(wp), INTENT (INOUT) ::       &
+         aod_sw_vr(kbdim,klev,nb_sw) ,& !< Aerosol shortwave optical depth
+         ssa_sw_vr(kbdim,klev,nb_sw) ,& !< Aerosol single scattering albedo
+         asy_sw_vr(kbdim,klev,nb_sw) ,& !< Aerosol asymmetry parameter
+         x_cdnc(kbdim)                  !< Scale factor for Cloud Droplet Number Concentration
+  
+    !
+    ! --- 0.2 Dummy variables
+    !
+    INTEGER ::                        &
+         jk                          ,& !< index for looping over vertical dimension
+         jki                         ,& !< index for looping over vertical dimension for reversing
+         jl                          ,& !< index for looping over block
+         jwl                         ,& !< index for looping over wavelengths
+         j_sw                           !< index for looping over wavelengths
+    
+    REAL(wp) ::                       &
+         year_fr                     ,& !< time in year fraction (1989.0 is 0Z on Jan 1 1989)
+         lambda                      ,& !< wavelength at central band wavenumber [nm]
+         z_sfc(kproma)               ,& !< surface height [m]
+         lon_sp(kproma)              ,& !< longitude passed to sp
+         lat_sp(kproma)              ,& !< latitude passed to sp
+         z_fl_vr(kbdim,klev)         ,& !< level height [m], vertically reversed indexing (1=lowest level)
+         dz_vr(kbdim,klev)           ,& !< level thickness [m], vertically reversed 
+         sp_aod_vr(kbdim,klev)       ,& !< simple plume aerosol optical depth, vertically reversed 
+         sp_ssa_vr(kbdim,klev)       ,& !< simple plume single scattering albedo, vertically reversed
+         sp_asy_vr(kbdim,klev)       ,& !< simple plume asymmetry factor, vertically reversed indexing
+         sp_xcdnc(kproma)               !< drop number scale factor
+
+    year_fr=datetime%yeafrc
+    IF (datetime%year > 1850) THEN
+      ! 
+      ! --- 1.1 geographic information
+      !
+      DO jk=1,klev
+        jki=klev-jk+1
+        DO jl=1,kproma
+          dz_vr  (jl,jk) = rgrav*(prm_field%geoi(jl,jki+1,krow)-prm_field%geoi(jl,jki,krow))
+          z_fl_vr(jl,jk) = rgrav*prm_field%geom(jl,jki,krow)+prm_field%oromea(jl,krow)
+        END DO
+      END DO
+      z_sfc(1:kproma)  = prm_field%oromea(1:kproma,krow)
+      lon_sp(1:kproma) = p_patch(jg)%cells%center(1:kproma,krow)%lon
+      lat_sp(1:kproma) = p_patch(jg)%cells%center(1:kproma,krow)%lat
+      ! 
+      ! --- 1.2 Aerosol Shortwave properties
+      !
+      ! get aerosol optical properties in each band, and adjust effective radius
+      !
+      DO jwl = 1,nb_sw
+        j_sw   = jpb1 + jwl - 1
+        lambda = 1.e7_wp/ (0.5_wp * (sw_wv1(j_sw) + sw_wv2(j_sw)))
+        CALL sp_aop_profile                                                                   ( &
+           & klev               ,kproma             ,kbdim               ,lambda              , &
+           & z_sfc(:)           ,lon_sp(:)          ,lat_sp(:)           ,year_fr             , &
+           & z_fl_vr(:,:)       ,dz_vr(:,:)         ,sp_xcdnc(:)         ,sp_aod_vr(:,:)      , &
+           & sp_ssa_vr(:,:)     ,sp_asy_vr(:,:)                                               )
+
+        DO jk=1,klev
+          DO jl=1,kproma
+            asy_sw_vr(jl,jk,jwl) = asy_sw_vr(jl,jk,jwl) * ssa_sw_vr(jl,jk,jwl) * aod_sw_vr(jl,jk,jwl)    &
+                 + sp_asy_vr(jl,jk)   * sp_ssa_vr(jl,jk)    * sp_aod_vr(jl,jk)
+            ssa_sw_vr(jl,jk,jwl) = ssa_sw_vr(jl,jk,jwl) * aod_sw_vr(jl,jk,jwl)                           &
+                 + sp_ssa_vr(jl,jk)   * sp_aod_vr(jl,jk)
+            aod_sw_vr(jl,jk,jwl) = aod_sw_vr(jl,jk,jwl) + sp_aod_vr(jl,jk)
+            asy_sw_vr(jl,jk,jwl) = MERGE(asy_sw_vr(jl,jk,jwl)/ssa_sw_vr(jl,jk,jwl),asy_sw_vr(jl,jk,jwl), &
+                 ssa_sw_vr(jl,jk,jwl) > TINY(1.0_wp))
+            ssa_sw_vr(jl,jk,jwl) = MERGE(ssa_sw_vr(jl,jk,jwl)/aod_sw_vr(jl,jk,jwl),ssa_sw_vr(jl,jk,jwl), &
+                 aod_sw_vr(jl,jk,jwl) > TINY(1.0_wp))
+          END DO
+        END DO
+      END DO
+
+      DO jl=1,kproma
+        x_cdnc(jl) = sp_xcdnc(jl)
+        !*sp_xre(jl)
+      END DO
+      RETURN
+    END IF
+ 
+  END SUBROUTINE add_aop_sp
 
   SUBROUTINE read_1d_wrapper(ifile_id,                 variable_name,        &
                            & return_pointer,           file_name,            &
@@ -340,8 +678,8 @@ MODULE mo_bc_aeropt_splumes
          ELSE
            message2=TRIM(ADJUSTL(message2))//' unknown file'
          END IF
-         WRITE(0,*) TRIM(ADJUSTL(message1))
-         WRITE(0,*) TRIM(ADJUSTL(message2))
+!!$         WRITE(0,*) TRIM(ADJUSTL(message1))
+!!$         WRITE(0,*) TRIM(ADJUSTL(message2))
          CALL finish(TRIM(ADJUSTL(message1)),TRIM(ADJUSTL(message2)))
        END IF
     END IF
