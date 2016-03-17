a798e7cd580fff8acabca8f10d97d9c2647ccd5d
Karl-Hermann Wieners <karl-hermann.wieners@mpimet.mpg.de>
2016-03-17 10:59:42 +0000

    [icon-cimd-buildbotlists] Fixed path to python in scripts to use PATH setting

--- scripts/buildbot_scripts/buildbot_builders.py
+++ scripts/buildbot_scripts/buildbot_builders.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #==============================================================================
 # Simple library for the builders and experiment lists
--- scripts/buildbot_scripts/test_builders.py
+++ scripts/buildbot_scripts/test_builders.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #==============================================================================
 # driver for testing the builder classes
