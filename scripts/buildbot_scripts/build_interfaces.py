6d296a7396e2984e3989895a2c0846ba8ab66944
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2016-03-17 17:22:29 +0000

    [cimd-buildbotlists] added adhoc account number; add cpu time parameters

--- scripts/buildbot_scripts/build_interfaces.py
+++ scripts/buildbot_scripts/build_interfaces.py
@@ -18,13 +18,14 @@ def make_all_binaries(configure_flags):
   if not status == 0:
     print("Build failed")
     return status
+  set_account()
   os.chdir(paths.thisPath)
   return 0
     
 def make_ocean_binaries(configure_flags):
   ocean_only_flags=" --disable-atmo --disable-jsbach  --with-yac=no"
   os.chdir(paths.basePath)
-  ocean_folder = paths.basePath+"/ocean"
+  ocean_folder = paths.basePath+"/ocean_build"
   status = os.system("scripts/building/get_ocean "+paths.basePath+" "+ocean_folder)
   if not status == 0:
     print("get_ocean failed")
@@ -43,13 +44,21 @@ def make_ocean_binaries(configure_flags):
   os.chdir(paths.basePath)
   status = os.system("cp -r "+ocean_folder+"/build .")
   status = os.system("cp "+ocean_folder+"/config/set-up.info config")
+  set_account()
   
+  os.chdir(paths.thisPath)  
   return 0
 
-
+def set_account():
+  setup_file = open("./config/set-up.info", 'a')
+  setup_file.write("use_account_no=mh0287\n")
+  setup_file.write("use_notification=never\n")
+  setup_file.close()
+  
 # if succesful returns a list of the runscripts
 #  otherwise returns the status
 def make_runscript(experimentPathName, runflags):
+  #print("make runscript:"+experimentPathName+" with flags:"+runflags)
   os.chdir(paths.basePath)
   # seperate the the input path from the experiment name
   experimentPath, experimentName = paths.getPathAndName(experimentPathName)
