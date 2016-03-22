6adbc4a958e439e99e24a6b3ca23817391e107e2
Leonidas Linardakis <leonidas.linardakis@mpimet.mpg.de>
2016-03-22 13:31:06 +0000

    [ocean_tracerAdvection] remove option for explicit vertical diffusion from namelist

--- src/ocean/config/mo_ocean_nml.f90
+++ src/ocean/config/mo_ocean_nml.f90
@@ -213,7 +213,7 @@ MODULE mo_ocean_nml
 !   INTEGER  :: expl_vertical_velocity_diff = 1    ! 0=explicit, 1 = implicit
 !   INTEGER, PARAMETER :: explicit_diffusion = 2
 !   INTEGER, PARAMETER :: implicit_diffusion = 1
-  INTEGER  :: vertical_tracer_diffusion_type   = 1   ! not used !
+!   INTEGER  :: vertical_tracer_diffusion_type   = 1   ! not used !
   INTEGER  :: HorizontalViscosity_type  = 1          ! 0=no hor.diff; 1=constant Laplacian coefficients
                                                  ! 2=constant coefficients satisfying Munk criterion
                                                  ! 3=variable coefficients satisfying Munk criterion
@@ -300,7 +300,7 @@ MODULE mo_ocean_nml
     &                 dhdtw_abort                  , &
     &                 discretization_scheme        , &
     &                 dzlev_m                      , &
-    &                 vertical_tracer_diffusion_type , &
+!     &                 vertical_tracer_diffusion_type , &
     &                 i_bc_veloc_bot               , &
     &                 i_bc_veloc_lateral           , &
     &                 i_bc_veloc_top               , &
