f84ede8130868668912f4c598fe4e2ed5c0b685c
Marco Giorgetta <marco.giorgetta@mpimet.mpg.de>
2016-05-07 07:03:09 +0000

    [icon-aes-mag] For experiment mag101_amip: gkdrag=0.2, gkwake=0.0.

--- src/atm_phy_echam/mo_ssodrag.f90
+++ src/atm_phy_echam/mo_ssodrag.f90
@@ -93,8 +93,8 @@ CONTAINS
   ! Define the tuning parameters for SSO drag. These values depend on:
   ! (1) the resolution of the topography DATA used to compute the SSO parameters, and
   ! (2) the model resolution.
-    gkdrag  = 0.0_wp  ! drag due to blocking
-    gkwake  = 1.0_wp  ! drag due to gravity waves
+    gkdrag  = 0.2_wp  ! drag due to blocking
+    gkwake  = 0.0_wp  ! drag due to gravity waves
 
   ! Compute globally valid layer index nktopg from the vertical coordinate tables that
   ! describe the hybrid sigma coordinate as follows:
