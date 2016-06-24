d0e13fe5f5e31ba8b06d637b2125c7ace9929de5
Carolin Walter <carolin.walter@kit.edu>
2016-06-24 09:30:06 +0000

    added att bsc

--- src/art_interface/mo_art_diagnostics_interface.f90
+++ src/art_interface/mo_art_diagnostics_interface.f90
@@ -30,7 +30,7 @@ MODULE mo_art_diagnostics_interface
 #ifdef __ICON_ART
   USE mo_art_data,                      ONLY: p_art_data
   USE mo_art_config,                    ONLY: art_config
-  USE mo_art_aero_optical_props,        ONLY: art_calc_aod
+  USE mo_art_aero_optical_props,        ONLY: art_calc_aod, art_calc_bsc
   USE mo_art_diagnostics,               ONLY: art_volc_diagnostics
   USE mo_art_clipping,                  ONLY: art_clip_lt
 #endif
@@ -93,6 +93,7 @@ SUBROUTINE art_diagnostics_interface(p_patch, rho, pres, p_trac, dz, hml, jg)
         ! --------------------------------------
         IF (art_config(jg)%iart_dust > 0 .OR. art_config(jg)%iart_seasalt > 0 .OR. art_config(jg)%iart_volcano > 0) THEN
           CALL art_calc_aod(rho(:,:,jb), p_trac(:,:,jb,:), dz(:,:,jb), istart, iend, nlev, jb, jg, p_art_data(jg))
+          CALL art_calc_bsc(rho(:,:,jb), p_trac(:,:,jb,:), dz(:,:,jb), istart, iend, nlev, jb, jg, p_art_data(jg))
         ENDIF
     
         ! -------------------------------------
