5f37e16be4f76efc7f2faaad3cfc5455e150ecc2
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2016-07-13 11:32:07 +0000

    rm MTIME cpp; activate automatic mean value handling

--- src/io/shared/mo_name_list_output_init.f90
+++ src/io/shared/mo_name_list_output_init.f90
@@ -1460,11 +1460,9 @@ CONTAINS
 
               ENDDO
 
-#ifdef USE_MTIME_LOOP
               CALL process_mean_stream(p_onl,i_typ,sim_step_info, p_patch( patch_info(1)%log_patch_id ) ) ! works for amip and test_nat_rce
               !CALL process_mean_stream(p_onl,i_typ,sim_step_info, p_patch( 0 )) ! this is how it works for _nwp_R02B04N06multi2
               !CALL process_mean_stream(p_onl,i_typ,sim_step_info, p_patch( 1 )) ! initial setup
-#endif
 
               SELECT CASE(i_typ)
               CASE(level_type_ml)
