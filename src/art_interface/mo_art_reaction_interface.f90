9e8635366af23ba8ef99a9c9502833131ab60ff0
Daniel Rieger <daniel.rieger@kit.edu>
2016-09-19 10:52:43 +0000

    [icon-kit-modes] Adapted readioactive decay to updates in aerosol infrastructure

--- src/art_interface/mo_art_reaction_interface.f90
+++ src/art_interface/mo_art_reaction_interface.f90
@@ -113,9 +113,7 @@ SUBROUTINE art_reaction_interface(ext_data, p_patch,datetime,p_dtime,p_prog_list
         ! Select type of mode
         select type (fields=>this_mode%fields)
           type is (t_fields_radio)
-#if 0
-            CALL  art_decay_radioact(p_patch,p_dtime,p_tracer_now(:,:,:,fields%itracer),fields%halflife) 
-#endif
+            CALL  art_decay_radioact(p_patch,p_dtime,fields%ptr%p3d(:,:,:),fields%halflife) 
         end select                  
         this_mode => this_mode%next_mode
       END DO
