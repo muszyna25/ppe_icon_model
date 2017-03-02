03522bd90550253dba672318242f267d96cf92c4
Helmuth Haak <helmuth.haak@mpimet.mpg.de>
2017-03-02 14:39:29 +0100

    [icon-oes/icon-oes-dev_merge] GMRedi bugfix for implicit s2 term : reenable contribution from PP scheme

--- src/ocean/physics/mo_ocean_GM_Redi.f90
+++ src/ocean/physics/mo_ocean_GM_Redi.f90
@@ -403,7 +403,7 @@ CONTAINS
           DO cell_index = start_cell_index, end_cell_index
             DO level = start_level, patch_3D%p_patch_1D(1)%dolic_c(cell_index,blockNo)
               param%a_tracer_v(cell_index,level,blockNo, tracer_index) =  &
-             ! & param%a_tracer_v(cell_index,level,blockNo, tracer_index) + &
+              & param%a_tracer_v(cell_index,level,blockNo, tracer_index) + &
               & ocean_state%p_diag%vertical_mixing_coeff_GMRedi_implicit(cell_index,level,blockNo)
             END DO                  
           END DO                
