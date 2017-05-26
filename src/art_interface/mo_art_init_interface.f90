544bc41c51b3aa5b0f36dda83c95c7deac5f041f
Michael Weimer <michael.weimer@kit.edu>
2017-05-26 09:46:36 +0200

    [icon-kit/icon-kit-adapt-art-ntracer] added subroutine for getting the number of art tracers (art_init_interface.f90)

--- src/art_interface/mo_art_init_interface.f90
+++ src/art_interface/mo_art_init_interface.f90
@@ -22,16 +22,18 @@
 MODULE mo_art_init_interface
 
   USE mo_run_config,                    ONLY: lart
+  USE mo_art_config,                    ONLY: t_art_config
 #ifdef __ICON_ART
   USE mo_art_init_all_dom,              ONLY: art_init_all_dom
   USE mo_art_clean_up,                  ONLY: art_clean_up
+  USE mo_art_read_xml,                  ONLY: art_get_element_number_xml
 #endif
   
   IMPLICIT NONE
 
   PRIVATE
 
-  PUBLIC :: art_init_interface
+  PUBLIC :: art_init_interface, art_calc_number_of_art_tracers
 
 CONTAINS
 !!
@@ -57,6 +59,43 @@ SUBROUTINE art_init_interface(n_dom,defcase)
 #endif
 
 END SUBROUTINE art_init_interface
+!!
+!!-------------------------------------------------------------------------
+!!
+SUBROUTINE art_calc_number_of_art_tracers(art_config_jg)
+  IMPLICIT NONE
+  TYPE(t_art_config), INTENT(inout) :: art_config_jg
+#ifdef __ICON_ART
+  INTEGER :: number_elem
+#endif
+
+  art_config_jg%iart_ntracer = 0
+
+#ifdef __ICON_ART
+
+  IF (art_config_jg%lart_chem) THEN
+    CALL art_get_element_number_xml(number_elem,'/tracers','chemical', &
+                             &  TRIM(art_config_jg%cart_chemistry_xml))
+
+    art_config_jg%iart_ntracer = art_config_jg%iart_ntracer + number_elem
+  END IF
+
+  IF (art_config_jg%lart_aerosol) THEN
+    CALL art_get_element_number_xml(number_elem,'/tracers','aerosol', &
+                             &  TRIM(art_config_jg%cart_aerosol_xml))
+
+    art_config_jg%iart_ntracer = art_config_jg%iart_ntracer + number_elem
+  END IF
+
+  IF (art_config_jg%lart_passive) THEN
+    CALL art_get_element_number_xml(number_elem,'/tracers','passive', &
+                             &  TRIM(art_config_jg%cart_passive_xml))
+
+    art_config_jg%iart_ntracer = art_config_jg%iart_ntracer + number_elem
+  END IF
+#endif
+
+END SUBROUTINE art_calc_number_of_art_tracers
 !!
 !!-------------------------------------------------------------------------
 !!
