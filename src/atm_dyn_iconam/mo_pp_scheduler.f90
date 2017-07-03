f80b128f84cdb57c6404c0e27d300a875ea60fdb
Daniel Reinert <daniel.reinert@dwd.de>
2017-07-03 07:15:50 +0000

    [icon-nwp-dev] add missing copy of metainfo isteptype

--- src/atm_dyn_iconam/mo_pp_scheduler.f90
+++ src/atm_dyn_iconam/mo_pp_scheduler.f90
@@ -1424,6 +1424,7 @@ CONTAINS
                 &           ldims=shape3d, lrestart=.FALSE.,                &
                 &           tracer_info=info_dyn%tracer,                    &
                 &           loutput=.TRUE., new_element=new_element,        &
+                &           isteptype=info%isteptype,                       &
                 &           post_op=info%post_op, var_class=info%var_class, &
                 &           tlev_source=info%tlev_source,                   &
                 &           hor_interp=info%hor_interp,                     &
