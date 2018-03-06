db102d5f0e35ee49378a803e90c00a47b87b8370
Marco Giorgetta <marco.giorgetta@mpimet.mpg.de>
2018-03-06 14:14:34 +0100

    [icon-aes:master] [icon-aes:icon-aes-dbg2] Renovate APE script and add it to Buildbot The new exp.atm_ape_test runs now on the same grids as the AMIP experiments and uses a new ozone file, that was generated for the new grid. Further it is renovated similarly as the AMIP scripts.
    The new ozone files are derived from the original file for APE ozone.
    The processing is modified such that the meta data and units are the
    same as for the ozone files of the AMIP experiments. This means that
    the vertical axis is now named "plev" instead of "level" and the
    concentration is now mole/mole instead of ppmv (=factor 1.e-6).
    
    To make this possible the reading routine for APE ozone (io3_ape)
    is also adapted.
    
    For the new ozone files the following two steps were necessary:
    (1) Adapt Hui's original scripts to transform the original ozone
        field for APE, given on T42 Gaussian latitudes and 59 pressure
        levels in ASCII format to a longitudinally expanded field on the
        T42 Gaussian grid and vertically expanded from 0.2hPa to 0.01hPa.
        The scripts, input file and output file are stored in:
    /pool/data/ICON/grids/private/mpim/icon_preprocessing/import__ape_ozone:
    - original data : ape_ozone_T42-Lat-Alt.dat
    - fortran reader: ape_ozone_read.f     (used by NCL script)
    - driver script : ape_ozone.ksh        (is also README)
    - NCL script    : ape_ozone_latlon.ncl (does the work)
    - output file   : ape_ozone_T42_1Pa.nc (ozone on T42 grid up to 1Pa)
    
    (2) A new script /pool/data/ICON/grids/private/mpim/icon_preprocessing/scripts/19-remap_ape_ozone.sh
    interpolates the ozone field of ape_ozone_T42_1Pa.nc to all grids
    from R02B00 to R02B11, stored in /pool/data/ICON/grids/private/mpim/icon_preprocessing/source/ozone/ape/R02Bnn/bc_ozone_ape.nc
    
    Conflicts:
    	scripts/buildbot_scripts/experiment_lists/icon-dev

--- src/shr_horizontal/mo_ext_data_init.f90
+++ src/shr_horizontal/mo_ext_data_init.f90
@@ -41,7 +41,7 @@ MODULE mo_ext_data_init
     &                              MODIS, GLOBCOVER2009, GLC2000, SUCCESS, SSTICE_ANA_CLINC,        &
     &                              SSTICE_CLIM
   USE mo_math_constants,     ONLY: dbl_eps, rad2deg
-  USE mo_physical_constants, ONLY: ppmv2gg, zemiss_def
+  USE mo_physical_constants, ONLY: o3mr2gg, ppmv2gg, zemiss_def
   USE mo_run_config,         ONLY: msg_level, iforcing, check_uuid_gracefully
   USE mo_impl_constants_grf, ONLY: grf_bdywidth_c
   USE mo_lnd_nwp_config,     ONLY: ntiles_total, ntiles_lnd, ntiles_water, lsnowtile, frlnd_thrhld, &
@@ -606,9 +606,14 @@ CONTAINS
       O3 : IF (                        irad_o3 == io3_clim .OR.                      irad_o3 == io3_ape &
            & .OR. echam_rad_config(jg)%irad_o3 == io3_clim .OR. echam_rad_config(jg)%irad_o3 == io3_ape ) THEN
 
-        IF(irad_o3 == io3_ape .OR. echam_rad_config(jg)%irad_o3 == io3_ape) THEN
+        IF(iforcing == inwp .AND. irad_o3 == io3_ape) THEN
           levelname = 'level'
           cellname  = 'ncells'
+          o3name    = 'O3'
+          o3unit    = 'g/g'
+        ELSE IF(iforcing == iecham .AND. echam_rad_config(jg)%irad_o3 == io3_ape) THEN
+          levelname = 'plev'
+          cellname  = 'cell'
           o3name    = 'O3'
           o3unit    = 'g/g'
         ELSE ! o3_clim
@@ -1281,19 +1286,25 @@ CONTAINS
         CALL read_3D_extdim(stream_id, on_cells, TRIM(o3name), &
           &                 ext_data(jg)%atm_td%O3)
 
-        WRITE(message_text,'(a,f12.4,f12.4)')'MAX/MIN o3 ppmv', &
-           MAXVAL(ext_data(jg)%atm_td%O3(:,:,:,:)), MINVAL(ext_data(jg)%atm_td%O3(:,:,:,:))
-        CALL message(routine, TRIM(message_text))
-
         ! convert from ppmv to g/g only in case of APE ozone
         ! whether o3mr2gg or ppmv2gg is used to convert O3 to gg depends on the units of
         ! the incoming ozone file.  Often, the incoming units are not ppmv.
-        IF(irad_o3 == io3_ape .OR. echam_rad_config(jg)%irad_o3 == io3_ape) &
-         ! &         ext_data(jg)%atm_td%O3(:,:,:,:)= ext_data(jg)%atm_td%O3(:,:,:,:)*o3mr2gg
-          &         ext_data(jg)%atm_td%O3(:,:,:,:)= ext_data(jg)%atm_td%O3(:,:,:,:)*ppmv2gg
+        !
+        IF(iforcing == inwp .AND. irad_o3 == io3_ape) THEN
+           ! ozone input expected in units of ppmv
+           WRITE(message_text,'(a,f12.4,f12.4)')'MAX/MIN o3 ppmv', &
+                & MAXVAL(ext_data(jg)%atm_td%O3(:,:,:,:)), MINVAL(ext_data(jg)%atm_td%O3(:,:,:,:))
+           CALL message(routine, TRIM(message_text))
+           ext_data(jg)%atm_td%O3(:,:,:,:)= ext_data(jg)%atm_td%O3(:,:,:,:)*ppmv2gg
+        ELSE IF(iforcing == iecham .AND. echam_rad_config(jg)%irad_o3 == io3_ape) THEN
+           ! ozone input expected in units of mole/mole
+           WRITE(message_text,'(a,e12.4,e12.4)')'MAX/MIN o3 mole/mole', &
+                & MAXVAL(ext_data(jg)%atm_td%O3(:,:,:,:)), MINVAL(ext_data(jg)%atm_td%O3(:,:,:,:))
+           CALL message(routine, TRIM(message_text))
+           ext_data(jg)%atm_td%O3(:,:,:,:)= ext_data(jg)%atm_td%O3(:,:,:,:)*o3mr2gg
+        END IF
 
-
-        WRITE(message_text,'(a,f12.4,f12.4)')'MAX/MIN o3 g/g', &
+        WRITE(message_text,'(a,e12.4,e12.4)')'MAX/MIN o3 g/g', &
            MAXVAL(ext_data(jg)%atm_td%O3(:,:,:,:)), MINVAL(ext_data(jg)%atm_td%O3(:,:,:,:))
         CALL message(routine, TRIM(message_text))
 
