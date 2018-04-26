44236f423a71c10331bb11322a16ba5bfb79c4bc
Thomas Jahns <jahns@dkrz.de>
2018-04-26 16:22:47 +0200

    [icon-cimd-yaxt] Fix unnecessarily large string constants.

--- src/atm_dyn_iconam/mo_nh_feedback.f90
+++ src/atm_dyn_iconam/mo_nh_feedback.f90
@@ -78,7 +78,7 @@ CONTAINS
   SUBROUTINE feedback(p_patch, p_nh_state, p_int_state, p_grf_state, p_lnd_state, &
     jg, jgp)
 
-    CHARACTER(len=MAX_CHAR_LENGTH), PARAMETER ::  &
+    CHARACTER(len=*), PARAMETER ::  &
       &  routine = 'mo_nh_feedback:feedback'
 
 
@@ -151,7 +151,7 @@ CONTAINS
 
     IF (msg_level >= 10) THEN
       WRITE(message_text,'(a,i2,a,i2)') '========= Feedback:',jg,' =>',jgp
-      CALL message(TRIM(routine),message_text)
+      CALL message(routine,message_text)
     ENDIF
 
 
@@ -951,7 +951,7 @@ CONTAINS
   !!
   SUBROUTINE relax_feedback(p_patch, p_nh_state, p_int_state, p_grf_state, prm_diag, jg, jgp, dt_fbk)
 
-    CHARACTER(len=MAX_CHAR_LENGTH), PARAMETER ::  &
+    CHARACTER(len=*), PARAMETER ::  &
       &  routine = 'mo_nh_feedback:relax_feedback'
 
     TYPE(t_patch),       TARGET, INTENT(INOUT) ::  p_patch(n_dom_start:n_dom)
@@ -1044,7 +1044,7 @@ CONTAINS
     ! write(0,*) "n_dom_start,n_dom, jg, jgp=", n_dom_start, n_dom, jg, jgp
     IF (msg_level >= 10) THEN
       WRITE(message_text,'(a,i2,a,i2)') '========= Feedback:',jg,' =>',jgp
-      CALL message(TRIM(routine),message_text)
+      CALL message(routine,message_text)
     ENDIF
 
     p_parent_prog    => p_nh_state(jgp)%prog(nnew(jgp))
