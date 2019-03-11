9db85323cc2aec96011f906fd9de06393e86abdf
Daniel Rieger <daniel.rieger@dwd.de>
2019-03-11 16:08:25 +0000

    [icon-nwp/op-release-2.3.0-nwp5_latbc] Fix for line trunkation

--- src/atm_phy_nwp/mo_nwp_phy_state.f90
+++ src/atm_phy_nwp/mo_nwp_phy_state.f90
@@ -1760,7 +1760,9 @@ SUBROUTINE new_nwp_phy_diag_list( k_jg, klev, klevp1, kblks, &
 
     ! Reference pressure used for vertical distribution of aerosol optical depths
     ! &      diag%pref_aerdis(nproma,nblks_c)
-    cf_desc    = t_cf_var('pref_aerdis', '', 'Reference pressure used for vertical distribution of aerosol optical depths', datatype_flt)
+    cf_desc    = t_cf_var('pref_aerdis', '',                                                             &
+      &                   'Reference pressure used for vertical distribution of aerosol optical depths', &
+      &                   datatype_flt)
     grib2_desc = grib2_var(255, 255, 255, ibits, GRID_UNSTRUCTURED, GRID_CELL)
     CALL add_var( diag_list, 'pref_aerdis', diag%pref_aerdis,              &
       & GRID_UNSTRUCTURED_CELL, ZA_SURFACE, cf_desc, grib2_desc,          &
