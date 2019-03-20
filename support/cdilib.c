30bf65424a7b3f87578a8f3ca2289ed1e0d86ac7
Florian Prill <florian.prill@dwd.de>
2019-03-20 17:14:31 +0000

    [icon-nwp/icon-nwp-dev]  Fix in cdilib: prevent erroneous identification of two grid IDs.

--- support/cdilib.c
+++ support/cdilib.c
@@ -27117,6 +27117,7 @@ bool gridCompare(int gridID, const grid_t *grid, bool coord_compare)
                     || gridRef->vtable->compareXYAO((grid_t *)gridRef, (grid_t *)grid);
 
 		  differ |= ((gridRef->uuid[0] || grid->uuid[0]) && memcmp(gridRef->uuid, grid->uuid, CDI_UUID_SIZE));
+		  differ |= (grid->number > 0 && grid->position != gridRef->position);
 
                 }
               else
@@ -29681,6 +29682,7 @@ struct addIfNewRes cdiVlistAddGridIfNew(int vlistID, grid_t *grid, int mode)
         }
       if ( mode < 2 )
         {
+	  xassert(ngrids < MAX_GRIDS_PS);
           vlistptr->gridIDs[ngrids] = gridID;
           vlistptr->ngrids++;
         }
