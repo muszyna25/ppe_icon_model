f32ae3a23df3cf5783792c2924699e5b88a1eea1
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2019-04-30 15:51:36 +0200

    TESTS: update.

--- config/examples/tests/build.sh
+++ config/examples/tests/build.sh
@@ -41,8 +41,22 @@ for vendor in ${vendors}; do
     echo "*************************"
     log_file="${work_dir}/$(basename "${vendor_test}").log"
     echo "Running make for the second time in $(pwd)..." | tee -a "${log_file}"
-    make 2>&1 | tee -a "${log_file}"
+    make 2>&1 | tee -a "${log_file}" | tee make_2.log
+    if grep '^\(  GEN      version\.c\|perl .*pvcs.pl\)' make_2.log 2>&1 >/dev/null; then :
+      else echo -e "\nMake did not try to regenerate 'version.c'" && exit 1; fi
+    if grep ' [ ]*\(DEPGEN\|FC\|CC\|CCLD\|FCLD\|-o icon\)' make_2.log 2>&1 >/dev/null; then
+      echo -e "\nThe second call of make did some compilation/linking work" && exit 1; fi
+    echo "Running make for the third time in $(pwd)..." | tee -a "${log_file}"
+    make 2>&1 | tee -a "${log_file}" | tee make_3.log
+    if diff make_2.log make_3.log 2>&1 >/dev/null; then :
+      else echo -e "\nSecond and third calls of make gave different output" && exit 1; fi
+    rm make_2.log  make_3.log
+    echo "Running 'make distclean' in $(pwd)..." | tee -a "${log_file}"
+    make distclean 2>&1 | tee -a "${log_file}"
+    if find -mindepth 1 -print -quit 2>/dev/null | grep -q . 2>&1 >/dev/null; then
+      echo -e "\n'make distclean' did not delete all files" && exit 1; fi
     cd ..
+    find "${vendor_test_dir}" -type d -empty -delete
   done
 done
 
