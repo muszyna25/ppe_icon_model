588ecd2862d50bd4838fdaf0c1bb66c3d701ce13
Daniel Reinert <daniel.reinert@dwd.de>
2019-05-03 16:40:15 +0000

    [icon-nwp-dev] reintegrate branch icon-nwp-latbcfix
    * fix when using IFS data as lateral boundary conditions
      (allows reading of GEOSP as an alternative to GEOP_ML)
    
    commit f51fc2e36075163487078a9be8a85e54f6a3fb52
    Author: Daniel Reinert <daniel.reinert@dwd.de>
    Date:   Fri May 3 14:34:43 2019 +0000
    
        [icon-nwp-latbcfix] fix when using IFS data as lateral boundary conditions
    
        * allows reading of GEOSP as an alternative to GEOP_ML

--- src/io/atmo/mo_async_latbc.f90
+++ src/io/atmo/mo_async_latbc.f90
@@ -1489,7 +1489,7 @@ MODULE mo_async_latbc
         END IF
       END DO
       DO jp = 1, buffer%ngrp_vars
-        IF (buffer%mapped_name(jp) == buffer%geop_ml_var) THEN
+        IF (buffer%internal_name(jp) == buffer%geop_ml_var) THEN
           ! variable GEOSP is stored in cell center location
           buffer%hgrid(jp) = GRID_UNSTRUCTURED_CELL
         END IF
@@ -1554,7 +1554,7 @@ MODULE mo_async_latbc
       IF (found_unknown_grid) CALL finish(routine,'Unknown grid type found!')
 
       DO jp = 1, buffer%ngrp_vars
-        IF (buffer%mapped_name(jp) == buffer%geop_ml_var) THEN
+        IF (buffer%internal_name(jp) == buffer%geop_ml_var) THEN
           ! Memory for GEOSP variable taken as memory equivalent to 1 level of z_ifc
           ! as the variable GEOSP doesn't exist in metadata
           mem_size_ = mem_size_ + INT(1*n_own_cells,mpi_address_kind)
