7ec47a5ea2aae8be53d04a315d59006bce6ab448
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2019-10-10 18:21:16 +0200

    TESTS: update.

--- config/examples/tests/time_build.sh
+++ config/examples/tests/time_build.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-
 set -eu
 set -o pipefail
 
-curr_dir=$(pwd)
 root_dir=$(cd "$(dirname "$0")"; pwd)
-work_dir=$(basename "$0" '.sh')
+work_dir="${root_dir}/$(basename "$0" '.sh')"
 rm -rf "${work_dir}" && mkdir -p "${work_dir}" && cd "${work_dir}"
-work_dir=$(pwd)
 
-cfg='mpim-mpipc45-spack/gcc.external'
+###############################################################################
+
+config='mpim-mpipc45-spack/gcc.external'
+
 time_cmd=$(which time)
 
-"${root_dir}/../${cfg}"
+eval "${root_dir}/../${config}"
 make -j8 depend
+
+###############################################################################
+
 for i in {1..5}; do
   make mostlyclean
   "$time_cmd" -f '%e' -o time.txt -a make -j8
   "$time_cmd" -f '%e' -o time_remake.txt -a make -j8
 done
+
+###############################################################################
+
 echo
 echo '********************'
 echo
@@ -27,5 +33,3 @@ awk '{s+=$1; ssq+=$1^2}END{print "Total runs:", NR, "\nAverage wall time:", s/NR
 echo
 echo 'Remake time:'
 awk '{s+=$1; ssq+=$1^2}END{print "Total runs:", NR, "\nAverage wall time:", s/NR, "seconds\nStandard deviation:", sqrt((ssq-s^2/NR)/(NR-1))}' time_remake.txt
-
-cd "${curr_dir}"
--- config/examples/tests/time_depgen_make.sh
+++ config/examples/tests/time_depgen_make.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -eu
 set -o pipefail
 
@@ -7,14 +6,23 @@ root_dir=$(cd "$(dirname "$0")"; pwd)
 work_dir="${root_dir}/$(basename "$0" '.sh')"
 rm -rf "${work_dir}" && mkdir -p "${work_dir}" && cd "${work_dir}"
 
-cfg='mpim-mpipc45-spack/gcc.no_jsbach'
+###############################################################################
+
+config='mpim-mpipc45-spack/gcc.no_jsbach'
+
 time_cmd=$(which time)
 
-"${root_dir}/../${cfg}"
+eval "${root_dir}/../${config}"
+
+###############################################################################
+
 for i in {1..5}; do
   "$time_cmd" -f '%e' -o time.txt -a make "$@" depend
   touch icon.mk
 done
+
+###############################################################################
+
 echo
 echo '********************'
 echo
