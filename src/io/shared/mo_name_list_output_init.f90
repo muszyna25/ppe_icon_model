af9504e3918fc6a95e9b552b257e06bdf1aafd29
Thomas Jahns <jahns@dkrz.de>
2020-10-16 03:27:30 +0200

    Add support for profiles and zonal reductions to async namelist output. HB: do not execute init of "derived filelds" on io-procs * It still makes sense to have these fields all written by rank 0,   instead of the async output facilities, because the data is already   present on rank 0 and no other task would be involved at the output   stage. But doing that requires the following prerequisites:   1. the file in question must not contain any variables not of      profile/zonal mean type   2. the output function to gather on rank 0 needs significant      extensions to handle these variables more efficiently   3. the async facilities need to handle events that involve rank 0
    * The above is a lot of work for the serial async case and moderately
      difficult for the CDI-PIO case.
    * For the above reasons this commit only introduces a simple handling
      of zonal/profile reduction variables in line with other variables.

--- src/io/shared/mo_name_list_output_init.f90
+++ src/io/shared/mo_name_list_output_init.f90
@@ -33,6 +33,7 @@ MODULE mo_name_list_output_init
                                                 & gridDefProj, GRID_PROJECTION, GRID_CURVILINEAR, institutDef
   USE mo_cdi_constants,                     ONLY: GRID_UNSTRUCTURED_CELL, GRID_UNSTRUCTURED_VERT, GRID_UNSTRUCTURED_EDGE, &
                                                 & GRID_REGULAR_LONLAT, GRID_VERTEX, GRID_EDGE, GRID_CELL, GRID_ZONAL
+  USE mo_dynamics_config, ONLY: nnow, nnew, nold
   USE mo_kind,                              ONLY: wp, i8, dp, sp
   USE mo_impl_constants,                    ONLY: max_phys_dom, max_dom, SUCCESS, vname_len,      &
     &                                             max_var_ml, max_var_pl, max_var_hl, max_var_il,   &
@@ -64,7 +65,7 @@ MODULE mo_name_list_output_init
   USE mo_cf_convention,                     ONLY: t_cf_var, cf_global_info
   USE mo_restart_nml_and_att,               ONLY: getAttributesForRestarting
   USE mo_key_value_store,                   ONLY: t_key_value_store
-  USE mo_model_domain,                      ONLY: p_patch, p_phys_patch
+  USE mo_model_domain,                      ONLY: p_patch, p_phys_patch, t_patch
   USE mo_math_utilities,                    ONLY: merge_values_into_set, t_value_set
   USE mo_math_constants,                    ONLY: rad2deg
   ! config modules
@@ -182,13 +183,12 @@ MODULE mo_name_list_output_init
   USE mo_master_control,                    ONLY: get_my_process_name
 #endif
 #ifdef HAVE_CDI_PIO
-  USE yaxt, ONLY: xt_idxlist
   USE ppm_extents,                          ONLY: extent
   USE mo_decomposition_tools,               ONLY: uniform_partition_start
   USE mo_name_list_output_gridinfo,         ONLY: distribute_all_grid_info
   USE yaxt,                                 ONLY: xt_idxlist, &
        xt_idxvec_new, xt_idxlist_delete, xt_idxstripes_from_idxlist_new, &
-       xt_int_kind
+       xt_int_kind, xt_idxstripes_new, xt_idxempty_new, xt_stripe
 #endif
   IMPLICIT NONE
 
@@ -203,6 +203,7 @@ MODULE mo_name_list_output_init
   PUBLIC :: output_file
   PUBLIC :: patch_info
   PUBLIC :: lonlat_info
+  PUBLIC :: zonal_ri, profile_ri
   ! subroutines
   PUBLIC :: read_name_list_output_namelists
   PUBLIC :: parse_variable_groups
@@ -221,6 +222,7 @@ MODULE mo_name_list_output_init
   TYPE(t_patch_info),    ALLOCATABLE, TARGET :: patch_info (:)
   TYPE(t_patch_info_ll), ALLOCATABLE, TARGET :: lonlat_info(:,:)
   TYPE(t_key_value_store) :: outputRegister
+  TYPE(t_reorder_info), TARGET :: zonal_ri, profile_ri
 
   ! Number of output domains. This depends on l_output_phys_patch and is either the number
   ! of physical or the number of logical domains.
@@ -1138,17 +1140,6 @@ CONTAINS
     ENDDO
     CALL deallocateTimeDelta(mtime_day)
 
-    ! Init temporal accumulation fields etc.
-    IF (my_process_is_work()) THEN
-      p_onl => first_output_name_list
-      DO WHILE(ASSOCIATED(p_onl))
-        idom = p_onl%dom
-        IF (idom .LE. n_dom_out) &
-          & CALL init_statistics(p_onl, p_patch(patch_info(p_onl%dom)%log_patch_id))
-        p_onl => p_onl%next
-      END DO
-    END IF
-
     ! Get the number of output files needed (by counting the domains per name list)
     p_onl => first_output_name_list
     nfiles = 0
@@ -1174,10 +1165,19 @@ CONTAINS
 
     ALLOCATE(output_file(nfiles))
 
+    ! Init temporal accumulation fields etc.
+    IF (my_process_is_work()) THEN
+      p_onl => first_output_name_list
+      DO WHILE(ASSOCIATED(p_onl))
+        idom = p_onl%dom
+        IF (idom .LE. n_dom_out) &
+          & CALL init_statistics(p_onl, p_patch(patch_info(p_onl%dom)%log_patch_id))
+        p_onl => p_onl%next
+      END DO
+    END IF
+
     ! ---------------------------------------------------------------------------
-
     ! If async IO is used, replicate data (mainly the variable lists) on IO procs
-
 #ifndef NOMPI
     IF (use_async_name_list_io .AND. .NOT. is_mpi_test) &
          CALL replicate_data_on_io_procs
@@ -1808,19 +1808,18 @@ CONTAINS
     TYPE(t_level_selection), POINTER, INTENT(IN) :: level_selection
     LOGICAL, OPTIONAL :: var_ignore_level_selection
     INTEGER :: nlevs, info_nlevs, jk
-    LOGICAL :: var_ignore_level_selection_
+    LOGICAL :: var_ignore_level_selection_, is_reduction_var
 
     var_ignore_level_selection_ = .FALSE.
-    IF (info%hgrid .EQ. GRID_ZONAL) THEN
-      ! zonal grids are 2-dim but with a vertical axis
-      nlevs = info%used_dimensions(1)
-    ELSE IF(info%ndims < 3) THEN
+    ! zonal grids are 2-dim but with a vertical axis
+    is_reduction_var = info%hgrid == grid_zonal .OR. info%hgrid == grid_lonlat
+    IF (info%ndims < 3 .AND. .NOT. is_reduction_var) THEN
       ! other 2-dim. var are supposed to be horizontal only
       nlevs = 1
     ELSE
       ! handle the case that a few levels have been selected out of
       ! the total number of levels:
-      info_nlevs = info%used_dimensions(2)
+      info_nlevs = info%used_dimensions(MERGE(1, 2, is_reduction_var))
       IF (ASSOCIATED(level_selection)) THEN
         nlevs = 0
         ! Sometimes the user mixes level-selected variables with
@@ -2137,8 +2136,59 @@ CONTAINS
     ENDDO ! jl
 #endif
 ! #ifndef __NO_ICON_ATMO__
+    IF (.NOT. is_mpi_test) THEN
+      CALL create_rank0only_ri(zonal_ri, nlat_moc, is_io)
+      CALL create_rank0only_ri(profile_ri, 1, is_io)
+    END IF
   END SUBROUTINE set_patch_info
 
+  SUBROUTINE create_rank0only_ri(ri, n_pnt, is_io)
+    TYPE(t_reorder_info), INTENT(out) :: ri
+    INTEGER, INTENT(in) :: n_pnt
+    LOGICAL, INTENT(in) :: is_io
+    INTEGER :: jl
+    IF (.NOT. is_io) THEN
+      ri%n_glb = n_pnt
+      ri%n_own = MERGE(n_pnt, 0, p_pe_work == 0)
+      ALLOCATE(ri%own_idx(ri%n_own), &
+           ri%own_blk(ri%n_own), &
+           ri%reorder_index_own(ri%n_own), &
+           ri%pe_own(0:p_n_work-1), &
+           ri%pe_off(0:p_n_work-1))
+      IF (p_pe_work == 0) THEN
+        ! hack ahead: note that zonal data is not blocked, we add an
+        ! artificial blocking of nproma=1 in
+        ! mo_name_list_output::get_ptr_to_var_data!
+        DO jl = 1, n_pnt
+          ri%own_idx(jl) = 1
+          ri%own_blk(jl) = jl
+          ri%reorder_index_own(jl) = jl
+        END DO
+      END IF
+      ri%pe_off(0) = 0
+      ri%pe_own(0) = n_pnt
+      DO jl = 1, p_n_work-1
+        ri%pe_off(jl) = n_pnt
+        ri%pe_own(jl) = 0
+      END DO
+#ifdef HAVE_CDI_PIO
+      IF (pio_type == pio_type_cdipio) THEN
+        ALLOCATE(ri%reorder_idxlst_xt(1))
+        IF (p_pe_work == 0) THEN
+          ri%reorder_idxlst_xt(1) = xt_idxstripes_new(xt_stripe(0_xt_int_kind,&
+               1,INT(n_pnt, xt_int_kind)))
+        ELSE
+          ri%reorder_idxlst_xt(1) = xt_idxempty_new()
+        END IF
+      END IF
+#endif
+    END IF
+    IF (use_async_name_list_io) THEN
+      CALL transfer_reorder_info(ri, &
+           &    is_io, bcast_root, p_comm_work_2_io)
+      ! CALL transfer_grid_info(lonlat_info(jl,jg)%grid_info, n_pnt, lonlat_info(jl,jg)%grid_info_mode)
+    END IF
+  END SUBROUTINE create_rank0only_ri
 
   !------------------------------------------------------------------------------------------------
   !> Sets the reorder_info for cells/edges/verts
@@ -2867,7 +2917,7 @@ CONTAINS
   !
   SUBROUTINE replicate_data_on_io_procs()
     CHARACTER(*), PARAMETER :: routine = modname//"::replicate_data_on_io_procs"
-    INTEGER :: ivct_len, nvgrid, ivgrid, size_var_groups_dyn
+    INTEGER :: ivct_len, nvgrid, ivgrid, size_var_groups_dyn, temp(4)
     LOGICAL :: is_io
 
     is_io = my_process_is_io()
@@ -2880,9 +2930,19 @@ CONTAINS
       ELSE
         ivct_len = -1
       END IF
+      temp(1) = ivct_len
+      temp(2) = nold(1)
+      temp(3) = nnow(1)
+      temp(4) = nnew(1)
     END IF
-    CALL p_bcast(ivct_len, bcast_root, p_comm_work_2_io)
+    CALL p_bcast(temp, bcast_root, p_comm_work_2_io)
 
+    IF (is_io) THEN
+      ivct_len = temp(1)
+      nold(1) = temp(2)
+      nnow(1) = temp(3)
+      nnew(1) = temp(4)
+    END IF
     IF (ivct_len > 0) THEN
       IF (is_io) ALLOCATE(vct(ivct_len))
       CALL p_bcast(vct, bcast_root, p_comm_work_2_io)
@@ -2906,7 +2966,7 @@ CONTAINS
 
     ! Map the variable groups given in the output namelist onto the
     ! corresponding variable subsets:
-    CALL parse_variable_groups()
+    IF (is_io) CALL parse_variable_groups()
 
 
 #ifndef __NO_ICON_ATMO__
@@ -3066,6 +3126,10 @@ CONTAINS
           i_log_dom = output_file(i)%log_patch_id
           n_own     = lonlat_info(lonlat_id, i_log_dom)%ri%n_own
 #endif
+        CASE (grid_zonal)
+          n_own = zonal_ri%n_own
+        CASE (grid_lonlat)
+          n_own = profile_ri%n_own
         CASE DEFAULT
           CALL finish(routine,'unknown grid type')
         END SELECT
