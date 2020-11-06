dd17d18b9fc7f99db33c781fba35c6e998770cd7
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2020-11-06 20:36:06 +0100

    GPU-related fixup for be0a9ac7c

--- src/shared/mo_var_list_gpu.f90
+++ src/shared/mo_var_list_gpu.f90
@@ -9,7 +9,8 @@ MODULE mo_var_list_gpu
 
   USE mo_impl_constants,      ONLY: REAL_T, SINGLE_T, INT_T, BOOL_T
   USE mo_var_metadata_types,  ONLY: t_var_metadata
-  USE mo_var_list,            ONLY: t_var_list_ptr, t_list_element
+  USE mo_var_list,            ONLY: t_var_list_ptr
+  USE mo_var,                 ONLY: t_var
   USE mo_var_list_register,   ONLY: vlr_get
   USE mo_util_string,         ONLY: int2string
 
@@ -33,8 +34,9 @@ CONTAINS
 
     TYPE(t_var_list_ptr) :: list
     TYPE(t_var_metadata), POINTER :: info
-    TYPE(t_list_element), POINTER :: element
+    TYPE(t_var), POINTER :: element
     CHARACTER(:), ALLOCATABLE :: listname
+    INTEGER :: ii
 
     ! generate listname
     listname = TRIM(name) // &
@@ -43,20 +45,19 @@ CONTAINS
       & MERGE(int2string(domain, "(i2.2)"), '', PRESENT(timelev))
     CALL vlr_get(list, listname)
 
-    element => list%p%first_list_element
-    for_all_list_elements: DO WHILE (ASSOCIATED(element))
-      info    => element%field%info
+    for_all_list_elements: DO ii = 1, list%p%nvars
+      element => list%p%vl(ii)%p
+      info    => element%info
       SELECT CASE(info%data_type)
       CASE (REAL_T)
-        !$ACC UPDATE DEVICE(element%field%r_ptr) IF(info%lopenacc)
+        !$ACC UPDATE DEVICE(element%r_ptr) IF(info%lopenacc)
       CASE (SINGLE_T)
-        !$ACC UPDATE DEVICE(element%field%s_ptr) IF(info%lopenacc)
+        !$ACC UPDATE DEVICE(element%s_ptr) IF(info%lopenacc)
       CASE (INT_T)
-        !$ACC UPDATE DEVICE(element%field%i_ptr) IF(info%lopenacc)
+        !$ACC UPDATE DEVICE(element%i_ptr) IF(info%lopenacc)
       CASE (BOOL_T)
-        !$ACC UPDATE DEVICE(element%field%l_ptr) IF(info%lopenacc)
+        !$ACC UPDATE DEVICE(element%l_ptr) IF(info%lopenacc)
       END SELECT
-      element => element%next_list_element
     END DO for_all_list_elements
 
   END SUBROUTINE gpu_h2d_var_list
@@ -71,8 +72,9 @@ CONTAINS
 
     TYPE(t_var_list_ptr) :: list
     TYPE(t_var_metadata), POINTER :: info
-    TYPE(t_list_element), POINTER :: element
+    TYPE(t_var), POINTER :: element
     CHARACTER(:), ALLOCATABLE :: listname
+    INTEGER :: ii
 
     ! generate listname
     listname = TRIM(name) // &
@@ -81,20 +83,19 @@ CONTAINS
       & MERGE(int2string(domain, "(i2.2)"), '', PRESENT(timelev))
     CALL vlr_get(list, listname)
 
-    element => list%p%first_list_element
-    for_all_list_elements: DO WHILE (ASSOCIATED(element))
-      info    => element%field%info
+    for_all_list_elements: DO ii = 1, list%p%nvars
+      element => list%p%vl(ii)%p
+      info    => element%info
       SELECT CASE(info%data_type)
       CASE (REAL_T)
-        !$ACC UPDATE HOST(element%field%r_ptr) IF(info%lopenacc)
+        !$ACC UPDATE HOST(element%r_ptr) IF(info%lopenacc)
       CASE (SINGLE_T)
-        !$ACC UPDATE HOST(element%field%s_ptr) IF(info%lopenacc)
+        !$ACC UPDATE HOST(element%s_ptr) IF(info%lopenacc)
       CASE (INT_T)
-        !$ACC UPDATE HOST(element%field%i_ptr) IF(info%lopenacc)
+        !$ACC UPDATE HOST(element%i_ptr) IF(info%lopenacc)
       CASE (BOOL_T)
-        !$ACC UPDATE HOST(element%field%l_ptr) IF(info%lopenacc)
+        !$ACC UPDATE HOST(element%l_ptr) IF(info%lopenacc)
       END SELECT
-      element => element%next_list_element
     END DO for_all_list_elements
 
   END SUBROUTINE gpu_d2h_var_list
