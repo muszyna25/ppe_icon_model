37496bee2324f2874da01f12c90ad6499c47dc2b
harald.braun <harald.braun@atos.net>
2021-02-05 15:19:20 +0100

    [var list register utils] kind of re-do 2657979da5765bc24326495707f6a3cf775a5e66 "Extract function" plus some further cleanup in the routine printing var-group details

--- src/shared/mo_var_list_register_utils.f90
+++ src/shared/mo_var_list_register_utils.f90
@@ -13,7 +13,7 @@ MODULE mo_var_list_register_utils
   USE mo_var_list,         ONLY: find_list_element, t_var_list_ptr
   USE mo_exception,        ONLY: finish
   USE mo_util_string,      ONLY: remove_duplicates, pretty_print_string_list, &
-    &                            tolower, difference, find_trailing_number
+    &                            lowcase, difference, find_trailing_number
   USE mo_impl_constants,   ONLY: vname_len, SUCCESS
   USE mo_cdi_constants,    ONLY: GRID_UNSTRUCTURED_CELL, GRID_REGULAR_LONLAT
   USE mo_util_sort,        ONLY: quicksort
@@ -136,27 +136,24 @@ CONTAINS
     LOGICAL, INTENT(IN), OPTIONAL :: opt_latex_fmt !< Flag: .TRUE., if output shall be formatted for LaTeX
     LOGICAL, INTENT(IN), OPTIONAL :: opt_reduce_trailing_num !< Flag: replace trailing numbers by "*"
     LOGICAL, INTENT(IN), OPTIONAL :: opt_skip_trivial        !< Flag: skip empty of single-entry groups
-    ! local variables
     CHARACTER(*), PARAMETER :: routine = modname//"::print_group_details"
     CHARACTER(LEN=vname_len), ALLOCATABLE :: grp_names(:), grp_vars(:), grp_vars_out(:)
-    INTEGER, ALLOCATABLE :: slen(:)
-    INTEGER :: ngrp_vars, ngrp_vars_out, ierrstat, i, j, k, t
-    LOGICAL :: latex_fmt, reduce_trailing_num, skip_trivial, lfound
-    CHARACTER(:), ALLOCATABLE :: lx_pre
+    INTEGER :: ngrp_vars, ngrp_vars_out, ierrstat, i, j, lx_l
+    LOGICAL :: latex_fmt, reduce_trailing_num, skip_trivial
+    CHARACTER(LEN=2), PARAMETER :: lx = ' %'
     TYPE(t_vl_register_iter) :: iter
 
     latex_fmt = .FALSE.
     IF (PRESENT(opt_latex_fmt)) latex_fmt = opt_latex_fmt
-    lx_pre = ''
-    IF (latex_fmt) lx_pre = ' %'
-    reduce_trailing_num = .FALSE.
-    IF (PRESENT(opt_reduce_trailing_num)) reduce_trailing_num = opt_reduce_trailing_num 
-    skip_trivial = .FALSE.
+    lx_l = MERGE(0, 2, latex_fmt)
+    reduce_trailing_num = .TRUE.
+    IF (PRESENT(opt_reduce_trailing_num)) reduce_trailing_num = opt_reduce_trailing_num
+    skip_trivial = .TRUE.
     IF (PRESENT(opt_skip_trivial)) skip_trivial = opt_skip_trivial
     WRITE (0,*) " "
-    WRITE (0,*) lx_pre//"---------------------------------------"
-    WRITE (0,'(a,i0,a)') lx_pre//" Variable group info (for domain #", idom, "):"
-    WRITE (0,*) lx_pre//"---------------------------------------"
+    WRITE (0,*) lx(1:lx_l)//"---------------------------------------"
+    WRITE (0,'(a,i0,a)') lx(1:lx_l)//" Variable group info (for domain #", idom, "):"
+    WRITE (0,*) lx(1:lx_l)//"---------------------------------------"
     IF (latex_fmt) THEN
       WRITE (0,*) "% "
       WRITE (0,*) "% LaTeX formatted output, requires suitable environment 'varlist' and"
@@ -164,97 +161,32 @@ CONTAINS
     END IF
     WRITE (0,*) " "
     grp_names = var_groups_dyn%alphabetical_list()
-    WRITE (0,*) lx_pre//"List of groups:"
-    CALL pretty_print_string_list(grp_names, opt_prefix=lx_pre//"    ")
+    WRITE (0,*) lx(1:lx_l)//"List of groups:"
+    CALL pretty_print_string_list(grp_names, opt_prefix=lx(1:lx_l)//"    ")
     ! temporary variables needed for variable group parsing
     i = 0
     DO WHILE(iter%next())
       DO j = 1, iter%cur%p%nvars
-        IF (.NOT.iter%cur%p%vl(j)%p%info%lcontainer) &
-          i = i + 1
+        i = i + MERGE(0, 1, iter%cur%p%vl(j)%p%info%lcontainer)
       END DO
     END DO
-    ALLOCATE(grp_vars_out(i), grp_vars(i), STAT=ierrstat)
+    ALLOCATE(grp_vars(i), grp_vars_out(i), STAT=ierrstat)
     IF (ierrstat /= SUCCESS) CALL finish (routine, 'ALLOCATE failed.')
     DO i = 1, SIZE(grp_names)
       ! for each group we collect the contained variables two times
       ! - the first time we collect *all* model level variables on
       ! the triangular grid, and the second time we collect only
       ! those variables which are available for output.
-      CALL vlr_group(grp_names(i), grp_vars, ngrp_vars, opt_dom_id=idom, &
-        & loutputvars_only=.FALSE., lremap_lonlat=.FALSE., opt_vlevel_type=level_type_ml)
+      CALL compose_grp_varlist(grp_names(i), .FALSE., grp_vars, ngrp_vars)
+      IF ((skip_trivial) .AND. (ngrp_vars <= 1))  CYCLE
+      WRITE (0,*) ' '
+      IF (latex_fmt) THEN
+        WRITE (0,"(3a)") "\begin{varlist}{\grpname{", TRIM(grp_names(i)), "}}"
+      ELSE
+        WRITE (0,"(3a)") 'GROUP "', TRIM(grp_names(i)), '":'
+      END IF
       IF (ngrp_vars > 0) THEN
-        DO j=1,ngrp_vars
-          grp_vars(j) = tolower(grp_vars(j))
-        END DO
-        ! (optionally) replace trailing numbers by "*"
-        IF (reduce_trailing_num) THEN
-          ALLOCATE(slen(ngrp_vars))
-          DO j=1,ngrp_vars
-            slen(j) = find_trailing_number(grp_vars(j))
-          END DO
-          DO j=1,ngrp_vars
-            t = slen(j)
-            IF (t /= -1) THEN
-              ! replace trailing number if any other variable of "this
-              ! kind" exists:
-              lfound = .FALSE.
-              INNER_LOOP1 : DO k=1,ngrp_vars
-                IF (j==k)  CYCLE INNER_LOOP1
-                IF (grp_vars(j)(1:(t-1)) == grp_vars(k)(1:(slen(k)-1))) lfound = .TRUE.
-              END DO INNER_LOOP1
-              IF (lfound) THEN
-                grp_vars(j) = grp_vars(j)(1:(t-1))//"*"
-              END IF
-            END IF
-          END DO
-          CALL remove_duplicates(grp_vars(1:ngrp_vars), ngrp_vars)
-          DEALLOCATE(slen)
-        END IF
-        CALL quicksort(grp_vars(1:ngrp_vars))
-        IF ((skip_trivial) .AND. (ngrp_vars <= 1))  CYCLE
-        CALL vlr_group(grp_names(i), grp_vars_out, ngrp_vars_out, opt_dom_id=idom, &
-          & loutputvars_only=.TRUE., lremap_lonlat=.FALSE., opt_vlevel_type=level_type_ml)
-        DO j=1,ngrp_vars_out
-          grp_vars_out(j) = tolower(grp_vars_out(j))
-        END DO
-        ! (optionally) replace trailing numbers by "*"
-        IF (reduce_trailing_num) THEN
-          ALLOCATE(slen(ngrp_vars_out))
-          DO j=1,ngrp_vars_out
-            slen(j) = find_trailing_number(grp_vars_out(j))
-          END DO
-          DO j=1,ngrp_vars_out
-            t = slen(j)
-            IF (t /= -1) THEN
-              ! replace trailing number if any other variable of "this
-              ! kind" exists:
-              lfound = .FALSE.
-              INNER_LOOP2 : DO k=1,ngrp_vars_out
-                IF (j==k)  CYCLE INNER_LOOP2
-                IF (grp_vars_out(j)(1:(t-1)) == grp_vars_out(k)(1:(slen(k)-1))) lfound = .TRUE.
-              END DO INNER_LOOP2
-              IF (lfound) grp_vars_out(j) = grp_vars_out(j)(1:(t-1))//"*"
-            END IF
-          END DO
-          CALL remove_duplicates(grp_vars_out(1:ngrp_vars_out), ngrp_vars_out)
-          DEALLOCATE(slen)
-        END IF
-        CALL quicksort(grp_vars_out(1:ngrp_vars_out))
-        IF (latex_fmt) THEN
-          DO j=1,ngrp_vars
-            grp_vars(j) = "\varname{"//TRIM(grp_vars(j))//"}"
-          END DO
-          DO j=1,ngrp_vars_out
-            grp_vars_out(j) = "\varname{"//TRIM(grp_vars_out(j))//"}"
-          END DO
-        END IF
-        WRITE (0,*) ' '
-        IF (latex_fmt) THEN
-          WRITE (0,*) "\begin{varlist}{\grpname{"//TRIM(grp_names(i))//"}}"
-        ELSE
-          WRITE (0,*) 'GROUP "', TRIM(grp_names(i)), '":'
-        END IF
+        CALL compose_grp_varlist(grp_names(i), .TRUE., grp_vars_out, ngrp_vars_out)
         CALL pretty_print_string_list(grp_vars_out(1:ngrp_vars_out), opt_prefix="    ")
         CALL difference(grp_vars, ngrp_vars, grp_vars_out, ngrp_vars_out)
         IF (ngrp_vars > 0) THEN
@@ -264,20 +196,49 @@ CONTAINS
           CALL pretty_print_string_list(grp_vars(1:ngrp_vars), opt_prefix="    ")
         END IF
       ELSE
-        IF ((skip_trivial) .AND. (ngrp_vars <= 1))  CYCLE
-        WRITE (0,*) ' '
-        IF (latex_fmt) THEN
-          WRITE (0,*) "\begin{varlist}{\grpname{"//TRIM(grp_names(i))//"}}"
-        ELSE
-          WRITE (0,*) 'GROUP "', TRIM(grp_names(i)), '":'
-        END IF
         WRITE (0,*) "   -- empty --"
       END IF
       IF (latex_fmt) WRITE (0,*) "\end{varlist}"
     END DO
     WRITE (0,*) " "
-    DEALLOCATE(grp_names, grp_vars, grp_vars_out, STAT=ierrstat)
-    IF (ierrstat /= SUCCESS) CALL finish (routine, 'DEALLOCATE failed.')
+  CONTAINS
+
+    SUBROUTINE compose_grp_varlist(grpname, lout, vargrp, nvargrp)
+      CHARACTER(*), INTENT(IN) :: grpname
+      LOGICAL, INTENT(IN) :: lout
+      CHARACTER(LEN=vname_len), INTENT(INOUT) :: vargrp(:)
+      INTEGER, INTENT(INOUT) :: nvargrp
+      INTEGER, ALLOCATABLE :: slen(:)
+      INTEGER :: jj, kk
+      LOGICAL :: lfound
+
+      CALL vlr_group(grpname, vargrp, nvargrp, opt_dom_id=idom, &
+        & loutputvars_only=lout, lremap_lonlat=.FALSE., opt_vlevel_type=level_type_ml)
+      IF (nvargrp .LE. 0) RETURN
+      CALL lowcase(vargrp(1:nvargrp))
+      IF (reduce_trailing_num) THEN
+        ALLOCATE(slen(nvargrp))
+        slen = [(find_trailing_number(vargrp(jj))-1, jj = 1, nvargrp)]
+        DO jj = 1, nvargrp
+          IF (slen(jj) .EQ. -2) CYCLE
+          ! replace trailing number if any other variable of "this kind" exists:
+          lfound = .FALSE.
+          DO kk = 1, nvargrp
+            IF (lfound) EXIT
+            IF (jj .EQ. kk) CYCLE
+            lfound = (vargrp(jj)(1:slen(jj)) == vargrp(kk)(1:slen(kk)))
+          END DO
+          IF (lfound) vargrp(jj) = vargrp(jj)(1:slen(jj))//"*"
+        END DO
+        CALL remove_duplicates(vargrp(1:nvargrp), nvargrp)
+      END IF
+      CALL quicksort(vargrp(1:nvargrp))
+      IF (latex_fmt) THEN
+        DO jj = 1, nvargrp
+          vargrp(jj) = "\varname{" // TRIM(vargrp(jj)) // "}"
+        END DO
+      END IF
+    END SUBROUTINE compose_grp_varlist
   END SUBROUTINE vlr_print_groups
 
 END MODULE mo_var_list_register_utils
